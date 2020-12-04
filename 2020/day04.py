15d7807364565d4febbf4ce7ac8eb221962164b1
Gid <gsmith.jemba2000@gmail.com>
2020-12-04 12:49:56 +0000

    Tidied up code

--- 2020/day04.py
+++ 2020/day04.py
@@ -19,73 +19,43 @@ for i, line in enumerate(lines):
     if line == "":
         passport = None
         continue
-    if passport is None:
-        passport = passports[i]
-    tokens = line.split(" ")
-    for token in tokens:
-        k, v = token.split(":")
-        passport[k] = v
+    passport = passport or passports[i]
+    passport.update([token.split(":") for token in line.split()])
 
-required = {"byr", "iyr", "eyr", "hgt", "hcl", "ecl", "pid"}
+
+def is_valid_number(x, min_value, max_value):
+    return re.match(r"^\d+$", x) and min_value <= int(x) <= max_value
+
+
+def is_valid_height(height, valid_heights):
+    try:
+        h, measure = re.search(rf"^(\d+)({'|'.join(valid_heights)})$", height).groups()
+    except AttributeError:
+        return False
+    return is_valid_number(h, *valid_heights[measure])
+
+
+passport_checks = {
+    "byr": lambda x: is_valid_number(x, 1920, 2002),
+    "iyr": lambda x: is_valid_number(x, 2010, 2020),
+    "eyr": lambda x: is_valid_number(x, 2020, 2030),
+    "hgt": lambda x: is_valid_height(x, {"cm": (150, 193), "in": (59, 76)}),
+    "hcl": lambda x: re.match(r"^#[0-9a-f]{6}$", x),
+    "ecl": lambda x: re.match(r"^amb|blu|brn|gry|grn|hzl|oth$", x),
+    "pid": lambda x: re.match(r"^\d{9}$", x),
+    "cid": lambda x: True,
+}
+
+required_details = set(passport_checks.keys())
+required_details.discard("cid")
 
 part1 = 0
-for passport in passports.values():
-    if required.issubset(passport.keys()):
-        part1 += 1
-print(f"Part 1: {part1}")
-
-
-def match_value(value, min_value, max_value):
-    if not re.match(r"^\d+$", value):
-        return False
-    value = int(value)
-    return value >= min_value and value <= max_value
-
-
 part2 = 0
-for line, passport in passports.items():
-    if not required.issubset(passport.keys()):
-        continue
+for passport in passports.values():
+    if required_details.issubset(passport):
+        part1 += 1
+        if all(passport_checks[k](v) for k, v in passport.items()):
+            part2 += 1
 
-    # byr (Birth Year) - four digits; at least 1920 and at most 2002.
-    if not match_value(passport["byr"], 1920, 2002):
-        continue
-
-    # iyr (Issue Year) - four digits; at least 2010 and at most 2020.
-    if not match_value(passport["iyr"], 2010, 2020):
-        continue
-
-    # eyr (Expiration Year) - four digits; at least 2020 and at most 2030.
-    if not match_value(passport["eyr"], 2020, 2030):
-        continue
-
-    # hgt (Height) - a number followed by either cm or in:
-    # If cm, the number must be at least 150 and at most 193.
-    # If in, the number must be at least 59 and at most 76.
-    hgt = re.search(r"^(\d+)(\w{2})$", passport["hgt"])
-    if hgt is None:
-        continue
-    hgt, measure = hgt.groups()
-    valid_hgt = False
-    if measure == "cm":
-        valid_hgt = match_value(hgt, 150, 193)
-    elif measure == "in":
-        valid_hgt = match_value(hgt, 59, 76)
-    if not valid_hgt:
-        continue
-
-    # hcl (Hair Color) - a # followed by exactly six characters 0-9 or a-f.
-    if not re.match(r"^#[0-9a-f]{6}$", passport["hcl"]):
-        continue
-
-    # ecl (Eye Color) - exactly one of: amb blu brn gry grn hzl oth.
-    if passport["ecl"] not in ["amb", "blu", "brn", "gry", "grn", "hzl", "oth"]:
-        continue
-
-    # pid (Passport ID) - a nine-digit number, including leading zeroes.
-    if not re.match(r"^\d{9}$", passport["pid"]):
-        continue
-
-    part2 += 1
-
+print(f"Part 1: {part1}")
 print(f"Part 2: {part2}")
