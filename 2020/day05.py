c44e9ce4bdfa83e993fddd61d55d8add01306c2b
Gid <gsmith.jemba2000@gmail.com>
2020-12-05 07:39:39 +0000

    Day 5

---
+++ 2020/day05.py
@@ -0,0 +1,21 @@
+import logging
+import os
+
+script_dir = os.path.dirname(__file__)
+script_name = os.path.splitext(os.path.basename(__file__))[0]
+
+log_file = os.path.join(script_dir, f"logs/{script_name}.log")
+logging.basicConfig(level=logging.WARNING, filename=log_file, filemode="w")
+
+input_file = os.path.join(script_dir, f"inputs/{script_name}_input.txt")
+lines = [line.rstrip("\n") for line in open(input_file)]
+
+occupied = set()
+for line in lines:
+    occupied.add(int(line.translate(str.maketrans("FBLR", "0101")), 2))
+
+print(f"Part 1: {max(occupied)}")
+
+for i in range(max(occupied)):
+    if i - 1 in occupied and i + 1 in occupied and i not in occupied:
+        print(f"Part 2: {i}")
