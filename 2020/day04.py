54d770bce18c75158212fdb69cdbda599a5971e6
Gid <gsmith.jemba2000@gmail.com>
2020-12-07 08:19:20 +0000

    Remove unused logging

--- 2020/day04.py
+++ 2020/day04.py
@@ -1,15 +1,9 @@
-import logging
 import os
 import re
-
 from collections import defaultdict
 
 script_dir = os.path.dirname(__file__)
 script_name = os.path.splitext(os.path.basename(__file__))[0]
-
-log_file = os.path.join(script_dir, f"logs/{script_name}.log")
-logging.basicConfig(level=logging.WARNING, filename=log_file, filemode="w")
-
 input_file = os.path.join(script_dir, f"inputs/{script_name}_input.txt")
 lines = [line.rstrip("\n") for line in open(input_file)]
 
