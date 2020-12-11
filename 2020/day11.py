779b4a5725ca4c6aeb583fe81dcfec6c74700f28
Gid <gsmith.jemba2000@gmail.com>
2020-12-11 08:30:04 +0000

    Cache static lines of sight

--- 2020/day11.py
+++ 2020/day11.py
@@ -1,4 +1,5 @@
 import os
+from collections import defaultdict
 
 from grid import XY
 
@@ -24,29 +25,13 @@ class ChairSystem:
     EMPTY = "L"
     OCCUPIED = "#"
 
-    def __repr__(self):
-        s = ""
-        for y in range(self.max_y):
-            for x in range(self.max_x):
-                xy = XY(x, y)
-                c = self.FLOOR
-                if XY(x, y) in self.chairs:
-                    c = self.EMPTY
-                    if XY(x, y) in self.occupied:
-                        c = self.OCCUPIED
-                s += c
-            s += "\n"
-        s += "\n"
-        return s
-
-    def __init__(self, inputs, tolerance=4, immediate_neighbours_only=True):
+    def __init__(self, inputs, tolerance, immediate_neighbours_only=True):
         self.tolerance = tolerance
-        self.immediate_neighbours_only = immediate_neighbours_only
 
         self.chairs = set()
         self.occupied = set()
 
-        self.max_x, self.max_y = None, None
+        max_x, max_y = None, None
         for y, line in enumerate(inputs.split()):
             for x, c in enumerate(line):
                 if c != self.FLOOR:
@@ -54,33 +39,32 @@ class ChairSystem:
                     self.chairs.add(chair)
                     if c == self.OCCUPIED:
                         self.occupied.add(chair)
-                self.max_x = x + 1
-            self.max_y = y + 1
+                max_x = x + 1
+            max_y = y + 1
 
-    def occupied_chairs_in_sight(self, this_chair):
-        occupied_chairs_in_sight = 0
-        for direction in XY.all_directions():
-            xy = this_chair
-            while True:
-                xy += direction
-                if xy.x < 0 or xy.x >= self.max_x or xy.y < 0 or xy.y >= self.max_y:
-                    break
-                if xy in self.chairs:
-                    occupied_chairs_in_sight += xy in self.occupied
-                    break
-                if self.immediate_neighbours_only:
-                    break
-        return occupied_chairs_in_sight
+        self.chairs_in_sight = defaultdict(set)
+        for chair in self.chairs:
+            for direction in XY.all_directions():
+                xy = chair
+                while True:
+                    xy += direction
+                    if xy.x < 0 or xy.x >= max_x or xy.y < 0 or xy.y >= max_y:
+                        break
+                    if xy in self.chairs:
+                        self.chairs_in_sight[chair].add(xy)
+                        break
+                    if immediate_neighbours_only:
+                        break
 
     def iterate(self):
         new_occupied = set()
         for chair in self.chairs:
-            neighbour_count = self.occupied_chairs_in_sight(chair)
+            n_occupied = sum(c in self.occupied for c in self.chairs_in_sight[chair])
             if chair not in self.occupied:
-                if neighbour_count == 0:
+                if n_occupied == 0:
                     new_occupied.add(chair)
             else:
-                if neighbour_count < self.tolerance:
+                if n_occupied < self.tolerance:
                     new_occupied.add(chair)
         self.occupied = new_occupied
         return frozenset(self.occupied)
@@ -88,14 +72,14 @@ class ChairSystem:
     def find_stable_state(self):
         while True:
             prior_state = frozenset(self.occupied)
-            new_state = self.iterate()
-            if new_state == prior_state:
+            self.iterate()
+            if frozenset(self.occupied) == prior_state:
                 break
         return len(self.occupied)
 
 
 def solve(inputs):
-    chairs = ChairSystem(inputs, tolerance=4, immediate_neighbours_only=True)
+    chairs = ChairSystem(inputs, tolerance=4)
     print(f"Part 1: {chairs.find_stable_state()}")
     chairs = ChairSystem(inputs, tolerance=5, immediate_neighbours_only=False)
     print(f"Part 2: {chairs.find_stable_state()}\n")
