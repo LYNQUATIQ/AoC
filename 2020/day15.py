9d9bd45fc9b9a53b35e31474a77685732fc73822
Gid <gsmith.jemba2000@gmail.com>
2020-12-15 09:17:19 +0000

    Day 15

--- 2020/day15.py
+++ 2020/day15.py
@@ -3,13 +3,13 @@ actual_input = [18, 11, 9, 0, 5, 1]
 
 
 def play_game(seeds, max_rounds):
-    prior_turn_spoken = {n: t for t, n in enumerate(seeds, 1)}
-    last_number_said = seeds[-1]
+    last_seen = {n: t for t, n in enumerate(seeds, 1)}
+    previous_number = seeds[-1]
     for last_turn in range(len(seeds), max_rounds):
-        number = last_turn - prior_turn_spoken.get(last_number_said, last_turn)
-        prior_turn_spoken[last_number_said] = last_turn
-        last_number_said = number
-    return last_number_said
+        next_number = last_turn - last_seen.get(previous_number, last_turn)
+        last_seen[previous_number] = last_turn
+        previous_number = next_number
+    return previous_number
 
 
 def solve(inputs):
