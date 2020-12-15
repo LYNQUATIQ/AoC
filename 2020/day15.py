4f52888ac69c68ceb9eff393f81015c9baf83d55
Gid <gsmith.jemba2000@gmail.com>
2020-12-15 06:25:48 +0000

    Day 15

--- 2020/day15.py
+++ 2020/day15.py
@@ -1,57 +1,28 @@
-# import logging
-import math
-import os
-import re
-import string
+sample_input = [0, 3, 6]
+actual_input = [18, 11, 9, 0, 5, 1]
 
-from collections import defaultdict, Counter
-import itertools as it
 
-from grid import XY, ConnectedGrid
-from utils import *
+def play_game(seeds, max_rounds):
+    turn = len(seeds)
+    last_spoken = {n: t for t, n in enumerate(seeds, 1)}
+    last_number_said = seeds[-1]
+    del last_spoken[last_number_said]
 
-script_dir = os.path.dirname(__file__)
-script_name = os.path.splitext(os.path.basename(__file__))[0]
-# log_file = os.path.join(script_dir, f"logs/{script_name}.log")
-# logging.basicConfig(level=logging.WARNING, filename=log_file, filemode="w")
-with open(os.path.join(script_dir, f"inputs/{script_name}_input.txt")) as f:
-    actual_input = f.read()
-
-sample_input = """0,3,6"""
-actual_input = """18,11,9,0,5,1"""
+    while True:
+        try:
+            number = turn - last_spoken[last_number_said]
+        except KeyError:
+            number = 0
+        last_spoken[last_number_said] = turn
+        turn += 1
+        last_number_said = number
+        if turn == max_rounds:
+            return last_number_said
 
 
 def solve(inputs):
-    numbers = [int(line) for line in inputs.split(",")]
-
-    turn = 0
-    when_said = defaultdict(list)
-    prior_spoken = {}
-    last_spoken = {}
-    last_number_said = None
-    for number in numbers:
-        turn += 1
-        last_number_said = number
-        when_said[last_number_said].append(turn)
-
-    while True:
-        times_spoken = when_said[last_number_said]
-        try:
-            last_number_said = turn - times_spoken[-2]
-        except IndexError:
-            last_number_said = 0
-        turn += 1
-        when_said[last_number_said].append(turn)
-        # print(f"Turn {turn}: {last_number_said}")
-        if turn == 2020:
-            print(f"Part 1: {last_number_said}")
-            break
-        if turn == 30000000:
-            break
-
-    # for k in sorted(when_said.keys()):
-    #     print(k, when_said[k])
-    print(f"Part 2: {last_number_said}\n")
+    print(f"Part 1: {play_game(inputs, 2020)}")
+    print(f"Part 2: {play_game(inputs, 30000000)}\n")
 
 
 solve(sample_input)
