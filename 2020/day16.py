649b914c7dac4e800eff1d3c7110700eda65169a
Gid <gsmith.jemba2000@gmail.com>
2020-12-16 12:37:16 +0000

    Day 16

--- 2020/day16.py
+++ 2020/day16.py
@@ -14,42 +14,40 @@ with open(os.path.join(script_dir, f"inputs/{script_name}_input.txt")) as f:
 
 @print_time_taken
 def solve(inputs):
-    rules_list, ticket, nearby = inputs.split("\n\n")
+    rule_lines, ticket, nearby = inputs.split("\n\n")
 
-    rules_list = dict([tuple(line.split(":")) for line in rules_list.split("\n")])
-    ticket = [int(v) for v in ticket.split("\n")[1].split(",")]
-    nearby = [[int(v) for v in t.split(",")] for t in nearby.split("\n")[1:]]
-
-    rules = defaultdict(list)
-    for k, v in rules_list.items():
-        for valid in v.split(" or "):
-            lower, upper = valid.split("-")
-            rules[k].append((int(lower), int(upper)))
+    ticket = list(map(int, ticket.splitlines()[1].split(",")))
+    nearby = [list(map(int, line.split(","))) for line in nearby.splitlines()[1:]]
+    rules = {}
+    for field, bounds in (tuple(line.split(":")) for line in rule_lines.splitlines()):
+        rules[field] = set(
+            map(lambda x: tuple(map(int, x.split("-"))), bounds.split(" or "))
+        )
 
     possible_indices = {k: set(range(len(rules))) for k in rules}
     part1 = 0
     for values in nearby:
-        not_possible = defaultdict(list)
-        for (i, v), (k, valid_values) in product(enumerate(values), rules.items()):
-            if not any((lower <= v <= upper for lower, upper in valid_values)):
-                not_possible[i].append(k)
-        invalid_i = [i for i, v in not_possible.items() if len(v) == len(rules)]
-        if invalid_i:
-            part1 += sum(values[i] for i in invalid_i)
+        not_possible = defaultdict(set)
+        for index, value in enumerate(values):
+            for field, bounds in rules.items():
+                if not any((low <= value <= high for low, high in bounds)):
+                    not_possible[index].add(field)
+        invalid_indices = [i for i, v in not_possible.items() if len(v) == len(rules)]
+        if invalid_indices:
+            part1 += sum(values[i] for i in invalid_indices)
             continue
         for i, impossible_fields in not_possible.items():
             for field in impossible_fields:
                 possible_indices[field].discard(i)
     print(f"Part 1: {part1}")
 
-    field_index = {}
-    while len(field_index) < len(rules):
+    indices = {}
+    while indices.keys() != rules.keys():
         field, index = next((k, v) for k, v in possible_indices.items() if len(v) == 1)
-        (index,) = index
-        field_index[field] = index
-        for field in possible_indices:
-            possible_indices[field].discard(index)
-    part2 = (ticket[i] for k, i in field_index.items() if k.startswith("departure"))
+        indices[field] = index.pop()
+        for other_field in possible_indices:
+            possible_indices[other_field].discard(indices[field])
+    part2 = (ticket[i] for k, i in indices.items() if k.startswith("departure"))
     print(f"Part 2: {math.prod(part2)}\n")
 
 
