cb2bd7b2aec0b26f17675ecfb787266fd5cab88e
Gid <gsmith.jemba2000@gmail.com>
2020-12-17 07:13:37 +0000

    Day 17

--- 2020/day17.py
+++ 2020/day17.py
@@ -8,21 +8,19 @@ script_name = os.path.splitext(os.path.basename(__file__))[0]
 with open(os.path.join(script_dir, f"inputs/{script_name}_input.txt")) as f:
     actual_input = f.read()
 
-sample_input = """
-.#.
+sample_input = """.#.
 ..#
-###
-"""
+###"""
 
 
 class NDimensionsalPoint(tuple):
     @property
     def neighbours(self):
-        return [
+        return set(
             self + type(self)(direction)
-            for direction in product(*([[-1, 0, 1]] * len(self)))
+            for direction in product((-1, 0, 1), repeat=len(self))
             if not all(d == 0 for d in direction)
-        ]
+        )
 
     def __add__(self, other):
         return type(self)(a + b for a, b in zip(self, other))
@@ -34,38 +32,30 @@ class ConwayCubes:
         for y, line in enumerate(inputs.splitlines()):
             for x, c in enumerate(line):
                 if c == "#":
-                    pt = NDimensionsalPoint([x, y] + [0] * (dimensions - 2))
+                    pt = NDimensionsalPoint((x, y) + (0,) * (dimensions - 2))
                     self.active.add(pt)
 
-    def iterate(self):
-        new_active = set()
-
-        to_consider = set(self.active)
-        for pt in self.active:
-            to_consider.update(pt.neighbours)
-
-        for pt in to_consider:
-            n_count = sum(n in self.active for n in pt.neighbours)
-            if pt in self.active:
-                if n_count in [2, 3]:
-                    new_active.add(pt)
-            else:
-                if n_count == 3:
-                    new_active.add(pt)
-        self.active = new_active
-
-    def iterate_cycles(self):
+    def boot_up(self):
         for _ in range(6):
-            self.iterate()
+            to_consider = set(self.active)
+            for pt in self.active:
+                to_consider.update(pt.neighbours)
+            new_active = set()
+            for pt in to_consider:
+                n_count = sum(n in self.active for n in pt.neighbours)
+                if n_count not in [2, 3]:
+                    continue
+                if pt not in self.active and n_count != 3:
+                    continue
+                new_active.add(pt)
+            self.active = new_active
         return len(self.active)
 
 
 @print_time_taken
 def solve(inputs):
-    cubes = ConwayCubes(inputs, dimensions=3)
-    print(f"Part 1: {cubes.iterate_cycles()}")
-    cubes = ConwayCubes(inputs, dimensions=4)
-    print(f"Part 2: {cubes.iterate_cycles()}\n")
+    print(f"Part 1: {ConwayCubes(inputs, dimensions=3).boot_up()}")
+    print(f"Part 2: {ConwayCubes(inputs, dimensions=4).boot_up()}\n")
 
 
 solve(sample_input)
