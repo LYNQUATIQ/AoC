c34b9de75fba382f5a0a4c7c08cbb4e99d958c89
Gid <gsmith.jemba2000@gmail.com>
2020-12-18 14:24:39 +0000

    Day 18

--- 2020/day18.py
+++ 2020/day18.py
@@ -9,36 +9,34 @@ with open(os.path.join(script_dir, f"inputs/{script_name}_input.txt")) as f:
 
 sample_input = """((2 + 4 * 9) * (6 + 9 * 8 + 6) + 6) + 2 + 4 * 2"""
 
-BRACKETS, STAR, PLUS, EITHER = "()", "*", "+", "*+"
-REGEXES = {
-    BRACKETS: re.compile(r"^(?P<left>[\d+*()]*)\((?P<brackets>[\d+*]+)\)(?P<right>[\d+*()]*)$"),
-    STAR: re.compile(r"^(?P<left>[\d*+]+)(?P<oper>\*)(?P<right>[\d+]+)$"),
-    PLUS: re.compile(r"^(?P<left>[\d*+]+)(?P<oper>\+)(?P<right>[\d*]+)$"),
-    EITHER: re.compile(r"^(?P<left>[\d*+]+)(?P<oper>[*+])(?P<right>\d+)$"),
-}
+BRACKET= re.compile(r"^(?P<left>[\d+*()]*)\((?P<brackets>[\d+*]+)\)(?P<right>[\d+*()]*)$")
+STAR= re.compile(r"^(?P<left>[\d*+]+)(?P<oper>\*)(?P<right>[\d+]+)$")
+PLUS= re.compile(r"^(?P<left>[\d*+]+)(?P<oper>\+)(?P<right>[\d*]+)$")
+ANY= re.compile(r"^(?P<left>[\d*+]+)(?P<oper>[*+])(?P<right>\d+)$")
 
-def evaluate_expression(expression, reductions):
-    matches = (REGEXES[r].match(expression) for r in reductions)
+
+def evaluate_expression(expression, regexes):
+    matches = (regex.match(expression) for regex in regexes)
     try:
         match = next(m for m in matches if m is not None)
     except StopIteration:
         return expression
 
     if "brackets" in match.groupdict():
-        contents = evaluate_expression(match.group('brackets'), reductions)
+        contents = evaluate_expression(match.group('brackets'), regexes)
         expression = match['left'] + str(contents) + match['right'] 
-        return evaluate_expression(expression, reductions)
+        return evaluate_expression(expression, regexes)
 
-    a = evaluate_expression(match.group('left'), reductions)
-    b = evaluate_expression(match.group('right'), reductions)
+    a = evaluate_expression(match.group('left'), regexes)
+    b = evaluate_expression(match.group('right'), regexes)
     return { "*": math.prod, "+": sum }[match['oper']]((int(a), int(b)))
 
 
 def solve(inputs):
     part1, part2 = 0, 0
     for expression in[line.replace(" ","") for line in inputs.splitlines()]:
-        part1 += evaluate_expression(expression, [BRACKETS, EITHER])
-        part2 += evaluate_expression(expression, [BRACKETS, STAR, PLUS])
+        part1 += evaluate_expression(expression, [BRACKET, ANY])
+        part2 += evaluate_expression(expression, [BRACKET, STAR, PLUS])
 
     print(f"Part 1: {part1}")
     print(f"Part 2: {part2}\n")
