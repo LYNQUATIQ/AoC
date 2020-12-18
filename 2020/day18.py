23fce8b5e5dad4598b16d1e1f12b2ca4bcdc6abd
Gid <gsmith.jemba2000@gmail.com>
2020-12-18 14:38:28 +0000

    Day 18

--- 2020/day18.py
+++ 2020/day18.py
@@ -9,10 +9,10 @@ with open(os.path.join(script_dir, f"inputs/{script_name}_input.txt")) as f:
 
 sample_input = """((2 + 4 * 9) * (6 + 9 * 8 + 6) + 6) + 2 + 4 * 2"""
 
-BRACKET= re.compile(r"^(?P<left>.*)\((?P<brackets>[\d+*]+)\)(?P<right>.*)$")
-STAR= re.compile(r"^(?P<left>[\d*+]+)(?P<operator>\*)(?P<right>[\d+]+)$")
-PLUS= re.compile(r"^(?P<left>[\d*+]+)(?P<operator>\+)(?P<right>[\d*]+)$")
-ANY= re.compile(r"^(?P<left>[\d*+]+)(?P<operator>[*+])(?P<right>\d+)$")
+BRACKET = re.compile(r"^(?P<left>.*)\((?P<brackets>[\d+*]+)\)(?P<right>.*)$")
+STAR = re.compile(r"^(?P<left>[\d*+]+)(?P<operator>\*)(?P<right>[\d+]+)$")
+PLUS = re.compile(r"^(?P<left>[\d*+]+)(?P<operator>\+)(?P<right>[\d*]+)$")
+ANY = re.compile(r"^(?P<left>[\d*+]+)(?P<operator>[*+])(?P<right>\d+)$")
 
 
 def evaluate_expression(expression, regexes):
@@ -22,18 +22,18 @@ def evaluate_expression(expression, regexes):
         return expression
 
     if "brackets" in match.groupdict():
-        contents = evaluate_expression(match.group('brackets'), regexes)
-        expression = match['left'] + str(contents) + match['right'] 
+        contents = evaluate_expression(match.group("brackets"), regexes)
+        expression = match["left"] + str(contents) + match["right"]
         return evaluate_expression(expression, regexes)
 
-    a = int(evaluate_expression(match.group('left'), regexes))
-    b = int(evaluate_expression(match.group('right'), regexes))
-    return { "*": math.prod, "+": sum }[match['operator']]((a, b))
+    a = int(evaluate_expression(match.group("left"), regexes))
+    b = int(evaluate_expression(match.group("right"), regexes))
+    return {"*": math.prod, "+": sum}[match["operator"]]((a, b))
 
 
 def solve(inputs):
     part1, part2 = 0, 0
-    for expression in[line.replace(" ","") for line in inputs.splitlines()]:
+    for expression in (line.replace(" ", "") for line in inputs.splitlines()):
         part1 += evaluate_expression(expression, [BRACKET, ANY])
         part2 += evaluate_expression(expression, [BRACKET, STAR, PLUS])
 
