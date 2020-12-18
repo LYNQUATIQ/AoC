9aa5a3c7ada163ce2bffb33cdb5297f57916cdbb
Gid <gsmith.jemba2000@gmail.com>
2020-12-18 14:21:16 +0000

    Day 18

--- 2020/day18.py
+++ 2020/day18.py
@@ -1,3 +1,4 @@
+import math
 import os
 import re
 
@@ -6,46 +7,38 @@ script_name = os.path.splitext(os.path.basename(__file__))[0]
 with open(os.path.join(script_dir, f"inputs/{script_name}_input.txt")) as f:
     actual_input = f.read()
 
-sample_input = """1 + 2 * 3 + 4 * 5 + 6
-1 + (2 * 3) + (4 * (5 + 6))"""
+sample_input = """((2 + 4 * 9) * (6 + 9 * 8 + 6) + 6) + 2 + 4 * 2"""
 
+BRACKETS, STAR, PLUS, EITHER = "()", "*", "+", "*+"
+REGEXES = {
+    BRACKETS: re.compile(r"^(?P<left>[\d+*()]*)\((?P<brackets>[\d+*]+)\)(?P<right>[\d+*()]*)$"),
+    STAR: re.compile(r"^(?P<left>[\d*+]+)(?P<oper>\*)(?P<right>[\d+]+)$"),
+    PLUS: re.compile(r"^(?P<left>[\d*+]+)(?P<oper>\+)(?P<right>[\d*]+)$"),
+    EITHER: re.compile(r"^(?P<left>[\d*+]+)(?P<oper>[*+])(?P<right>\d+)$"),
+}
 
-RE_EXPRESSION = re.compile(r"^(?P<left>[\d+*()]*)\((?P<expression>[\d+*]+)\)(?P<right>[\d+*()]*)$")
-RE_OPERATOR = re.compile(r"^(?P<left>[\d*+]+)(?P<operator>[+*])(?P<value>\d+)$")
-RE_MULTIPLY = re.compile(r"^(?P<left>[\d*+]+)(?P<operator>[*])(?P<right>[\d+]+)$")
+def evaluate_expression(expression, reductions):
+    matches = (REGEXES[r].match(expression) for r in reductions)
+    try:
+        match = next(m for m in matches if m is not None)
+    except StopIteration:
+        return expression
 
-def evaluate_sum_pt2(expression):
-    match = RE_MULTIPLY.match(expression)
-    if match is None:
-        return evaluate_sum_pt1(expression)
-    left = evaluate_sum_pt2(match.group('left'))
-    right = evaluate_sum_pt2(match.group('right'))
-    return left * right
+    if "brackets" in match.groupdict():
+        contents = evaluate_expression(match.group('brackets'), reductions)
+        expression = match['left'] + str(contents) + match['right'] 
+        return evaluate_expression(expression, reductions)
 
-def evaluate_sum_pt1(expression):
-    match = RE_OPERATOR.match(expression)
-    if match is None:
-        return int(expression)
-    value = int(match.group('value'))
-    left = evaluate_sum_pt1(match.group('left'))
-    if match.group('operator') == "+":
-        return left + value
-    return left * value
-
-def evaluate_expression(expression, evaluate_fn):
-    match = RE_EXPRESSION.match(expression)
-    if match is None:
-        return evaluate_fn(expression)
-    bracketed_expression = str(evaluate_fn(match.group('expression')))
-    expression = match.group('left') + bracketed_expression + match.group('right')
-    return evaluate_expression(expression,evaluate_fn)
+    a = evaluate_expression(match.group('left'), reductions)
+    b = evaluate_expression(match.group('right'), reductions)
+    return { "*": math.prod, "+": sum }[match['oper']]((int(a), int(b)))
 
 
 def solve(inputs):
     part1, part2 = 0, 0
     for expression in[line.replace(" ","") for line in inputs.splitlines()]:
-        part1 += evaluate_expression(expression, evaluate_sum_pt1)
-        part2 += evaluate_expression(expression, evaluate_sum_pt2)
+        part1 += evaluate_expression(expression, [BRACKETS, EITHER])
+        part2 += evaluate_expression(expression, [BRACKETS, STAR, PLUS])
 
     print(f"Part 1: {part1}")
     print(f"Part 2: {part2}\n")
