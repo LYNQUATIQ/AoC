132bb61ac510d510697d7b974e5154b6d2253b37
Gid <gsmith.jemba2000@gmail.com>
2020-12-22 08:10:12 +0000

    Day 22

--- 2020/day22.py
+++ 2020/day22.py
@@ -24,7 +24,7 @@ Player 2:
 """
 
 
-def play_recursive(deck1, deck2):
+def play_combat(deck1, deck2, recursive=False):
     seen_before1, seen_before2 = set(), set()
     while deck1 and deck2:
         if tuple(deck1) in seen_before1 or tuple(deck2) in seen_before2:
@@ -33,8 +33,8 @@ def play_recursive(deck1, deck2):
 
         c1, c2 = deck1.popleft(), deck2.popleft()
         one_winner = c1 > c2
-        if len(deck1) >= c1 and len(deck2) >= c2:
-            r1, r2 = play_recursive(deque(list(deck1)[:c1]), deque(list(deck2)[:c2]))
+        if recursive and len(deck1) >= c1 and len(deck2) >= c2:
+            r1, r2 = play_combat(deque(list(deck1)[:c1]), deque(list(deck2)[:c2]))
             one_winner = len(r1) > len(r2)
 
         deck1.extend((c1, c2)) if one_winner else deck2.extend((c2, c1))
@@ -50,15 +50,10 @@ def calculate_score(deck1, deck2):
 @print_time_taken
 def solve(inputs):
     deck1, deck2 = (deque(map(int, x.splitlines()[1:])) for x in inputs.split("\n\n"))
-
-    while deck1 and deck2:
-        card1, card2 = deck1.popleft(), deck2.popleft()
-        deck1.extend((card1, card2)) if card1 > card2 else deck2.extend((card2, card1))
-    print(f"Part 1: {calculate_score(deck1, deck2)}")
+    print(f"Part 1: {calculate_score(*play_combat(deck1, deck2, False))}")
 
     deck1, deck2 = (deque(map(int, x.splitlines()[1:])) for x in inputs.split("\n\n"))
-    deck1, deck2 = play_recursive(deck1, deck2)
-    print(f"Part 2: {calculate_score(deck1, deck2)}\n")
+    print(f"Part 2: {calculate_score(*play_combat(deck1, deck2, True))}\n")
 
 
 solve(sample_input)
