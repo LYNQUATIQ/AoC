7f3df4ade1bb435560dac163c3f2c491604f2583
Gid <gsmith.jemba2000@gmail.com>
2021-01-04 08:54:36 +0000

    Tidy up

--- 2020/upping_the_ante.py
+++ 2020/upping_the_ante.py
@@ -11,39 +11,24 @@ with open(os.path.join(os.path.dirname(__file__), f"inputs/upping_the_ante.txt")
 
 @print_time_taken
 def solve(inputs):
-    # values = []
-    # for line in inputs.splitlines():
-    #     print(line)
-    #     values.append([int(x) for x in line.split(",")])
     values = (list(map(int, line.split(","))) for line in inputs.splitlines())
     bits = []
     for bit in values:
         wave = [b > 0 for b in bit]
         sequences, i = [], 0
-        while wave[i] == wave[i + 1]:
-            i += 1
-        while True:
+        while i < (len(wave) - 1):
             c = 0
             i += 1
-            if i >= (len(wave) - 1):
-                break
             while wave[i] == wave[i + 1]:
                 i += 1
                 c += 1
                 if i >= (len(wave) - 1):
                     break
             sequences.append(c)
-        x = sum(sequences) / len(sequences)
-        if x < 2.5:
-            bits.append("2")
-            continue
-        if x > 6.0:
-            bits.append("0")
-            continue
-        bits.append("1")
+        bits.append(str(int(2 - (sum(sequences) / len(sequences)) // 3)))
 
     for char in grouper(bits, 8):
-        print(chr(int("".join(char[1:6]), 3)), end="")
+        print(chr(int("".join(char[1:-2]), 3)), end="")
 
     print()
 
