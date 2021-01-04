4afc68fa6a2eac7e1b67f9b82cf2bd822c4e55dc
Gid <gsmith.jemba2000@gmail.com>
2021-01-04 09:18:03 +0000

    Tidying

--- 2020/upping_the_ante.py
+++ 2020/upping_the_ante.py
@@ -11,21 +11,13 @@ with open(os.path.join(os.path.dirname(__file__), f"inputs/upping_the_ante.txt")
 
 @print_time_taken
 def solve(inputs):
-    values = (list(map(int, line.split(","))) for line in inputs.splitlines())
+    waves = (list(map(int, line.split(","))) for line in inputs.splitlines())
     bits = []
-    for bit in values:
-        wave = [b > 0 for b in bit]
-        sequences, i = [], 0
-        while i < (len(wave) - 1):
-            c = 0
-            i += 1
-            while wave[i] == wave[i + 1]:
-                i += 1
-                c += 1
-                if i >= (len(wave) - 1):
-                    break
-            sequences.append(c)
-        bits.append(str(int(2 - (sum(sequences) / len(sequences)) // 3)))
+    for wave in waves:
+        boundary_crosses = []
+        for b1, b2 in zip(wave[:-1], wave[1:]):
+            boundary_crosses.append((b1 > 0) != (b2 > 0))
+        bits.append(str(int((sum(boundary_crosses) // 10))))
 
     for char in grouper(bits, 8):
         print(chr(int("".join(char[1:-2]), 3)), end="")
