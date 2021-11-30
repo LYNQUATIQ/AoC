be66e7a88b5fe49145179e4b8ffc8881aee303d0
Gid <gsmith.jemba2000@gmail.com>
2021-11-30 21:15:02 +0000

    Update templates

--- 2021/day24.py
+++ 2021/day24.py
@@ -8,11 +8,11 @@ from collections import defaultdict, Counter
 from itertools import product
 
 from grid import XY, ConnectedGrid
-from utils import *
+from utils import flatten, grouper, powerset, print_time_taken
 
-# log_file = os.path.join(os.path.dirname(__file__), f"logs/dayxx.log")
+# log_file = os.path.join(os.path.dirname(__file__), f"logs/day24.log")
 # logging.basicConfig(level=logging.WARNING, filename=log_file, filemode="w")
-with open(os.path.join(os.path.dirname(__file__), f"inputs/dayxx_input.txt")) as f:
+with open(os.path.join(os.path.dirname(__file__), f"inputs/day24_input.txt")) as f:
     actual_input = f.read()
 
 sample_input = """sample"""
