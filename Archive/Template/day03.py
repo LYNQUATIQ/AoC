1ed95c4543a3df4584743028b786f07c0c3a9528
Gid <gsmith.jemba2000@gmail.com>
2021-11-30 21:16:45 +0000

    Add template folder

---
+++ Archive/Template/day03.py
@@ -0,0 +1,32 @@
+# import logging
+import math
+import os
+import re
+import string
+
+from collections import defaultdict, Counter
+from itertools import product
+
+from grid import XY, ConnectedGrid
+from utils import flatten, grouper, powerset, print_time_taken
+
+# log_file = os.path.join(os.path.dirname(__file__), f"logs/day03.log")
+# logging.basicConfig(level=logging.WARNING, filename=log_file, filemode="w")
+with open(os.path.join(os.path.dirname(__file__), f"inputs/day03_input.txt")) as f:
+    actual_input = f.read()
+
+sample_input = """sample"""
+
+
+@print_time_taken
+def solve(inputs):
+    # lines = inputs.splitlines()
+    # values = list(map(int, inputs.splitlines()))
+
+    print(f"Part 1: {False}")
+
+    print(f"Part 2: {False}\n")
+
+
+solve(sample_input)
+# solve(actual_input)
