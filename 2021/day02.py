ee3754770eab6792acdf836b5bb3e3d8fcae2356
Gid <gsmith.jemba2000@gmail.com>
2021-12-02 06:20:05 +0000

    Day 2

--- 2021/day02.py
+++ 2021/day02.py
@@ -1,32 +1,36 @@
-# import logging
-import math
 import os
-import re
-import string
 
-from collections import defaultdict, Counter
-from itertools import product
+from utils import print_time_taken
 
-from grid import XY, ConnectedGrid
-from utils import flatten, grouper, powerset, print_time_taken
-
-# log_file = os.path.join(os.path.dirname(__file__), f"logs/day02.log")
-# logging.basicConfig(level=logging.WARNING, filename=log_file, filemode="w")
 with open(os.path.join(os.path.dirname(__file__), f"inputs/day02_input.txt")) as f:
     actual_input = f.read()
 
-sample_input = """sample"""
+sample_input = """forward 5
+down 5
+forward 8
+up 3
+down 8
+forward 2"""
 
 
 @print_time_taken
 def solve(inputs):
-    # lines = inputs.splitlines()
-    # values = list(map(int, inputs.splitlines()))
 
-    print(f"Part 1: {False}")
+    position, depth, aim = 0, 0, 0
 
-    print(f"Part 2: {False}\n")
+    for line in inputs.splitlines():
+        match line.split():
+            case ('forward', x):
+                position += int(x)
+                depth += aim *int(x)
+            case ('up', x):
+                aim -= int(x)
+            case ('down', x):
+                aim += int(x)
+
+    print(f"Part 1: {position * aim}")
+    print(f"Part 2: {position * depth}\n")
 
 
 solve(sample_input)
-# solve(actual_input)
+solve(actual_input)
