fbecb770cd65be54c928aa34782c2fef3f023fc1
Gid <gsmith.jemba2000@gmail.com>
2021-12-10 07:02:40 +0000

    Improved Day 10

--- 2021/day10.py
+++ 2021/day10.py
@@ -21,26 +21,25 @@ sample_input = """[({(<(())[]>[[{[]{<()<>>
 
 
 CLOSING = {")": "(", "]": "[", "}": "{", ">": "<"}
-POINTS1 = {")": 3, "]": 57, "}": 1197, ">": 25137}
-POINTS2 = {"(": 1, "[": 2, "{": 3, "<": 4}
+POINTS = {")": 3, "]": 57, "}": 1197, ">": 25137, "(": 1, "[": 2, "{": 3, "<": 4}
 
 
 @print_time_taken
 def solve(inputs):
     part1, part2 = 0, []
     for line in inputs.splitlines():
-        opening = []
+        openings, incorrect = [], False
         for bracket in line:
             if bracket in CLOSING:
-                incorrect = opening.pop() != CLOSING[bracket]
-                if incorrect:
+                if CLOSING[bracket] != openings.pop():
+                    incorrect = True
                     break
             else:
-                opening.append(bracket)
+                openings.append(bracket)
         if incorrect:
-            part1 += POINTS1[bracket]
+            part1 += POINTS[bracket]
         else:
-            part2.append(reduce(lambda a, b: a * 5 + POINTS2[b], opening[::-1], 0))
+            part2.append(reduce(lambda a, b: a * 5 + POINTS[b], openings[::-1], 0))
 
     print(f"Part 1: {part1}")
     print(f"Part 2: {median(part2)}\n")
