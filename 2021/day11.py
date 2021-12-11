5ad5fa496aae398fcd12fee08b3e5065c19720f4
Gid <gsmith.jemba2000@gmail.com>
2021-12-11 08:17:28 +0000

    Improved Day 11

--- 2021/day11.py
+++ 2021/day11.py
@@ -1,9 +1,8 @@
-# import logging
-import math
 import os
 
-from collections import defaultdict
+from itertools import product
 
+from grid import XY
 from utils import print_time_taken
 
 with open(os.path.join(os.path.dirname(__file__), f"inputs/day11_input.txt")) as f:
@@ -21,55 +20,52 @@ sample_input = """5483143223
 5283751526"""
 
 
+class Octopi:
+    def __init__(self, inputs):
+        self.octopi = {}
+        for y, line in enumerate(inputs.splitlines()):
+            for x, level in enumerate(line):
+                self.octopi[XY(x, y)] = int(level)
+        self.bounds = XY(x, y)
+
+    def flashing_octopus(self):
+        for octopus, level in self.octopi.items():
+            if level > 9:
+                return octopus
+        return None
+
+    def octopus_neighbours(self, octopus):
+        return (n for n in octopus.all_neighbours if n.in_bounds(self.bounds))
+
+    def take_step(self):
+        for octopus, level in self.octopi.items():
+            self.octopi[octopus] = level + 1
+
+        flashed = set()
+        while flasher := self.flashing_octopus():
+            self.octopi[flasher] = 0
+            flashed.add(flasher)
+            for neighbour in self.octopus_neighbours(flasher):
+                if neighbour not in flashed:
+                    self.octopi[neighbour] += 1
+
+        return len(flashed)
+
+
 @print_time_taken
 def solve(inputs):
-    octopi_grid = {
-        (x, y): int(octopus)
-        for y, line in enumerate(inputs.splitlines())
-        for x, octopus in enumerate(line)
-    }
-    octopi = {i: set() for i in range(11)}
-    for xy, level in octopi_grid.items():
-        octopi[level].add(xy)
-
-    # print_octopi(octopi)
-    get_neighbours = lambda x, y: (
-        (x + 1, y),
-        (x + 1, y + 1),
-        (x, y + 1),
-        (x - 1, y + 1),
-        (x - 1, y),
-        (x - 1, y - 1),
-        (x, y - 1),
-        (x + 1, y - 1),
-    )
+    octopi = Octopi(inputs)
 
     flashes, step = 0, 0
     while True:
         step += 1
-        octopi = {(level + 1) % 11: xys for level, xys in octopi.items()}
-        flashed = set()
-        while octopi[10]:
-            octopus = octopi[10].pop()
-            octopi[0].add(octopus)
-            if octopus in flashed:
-                continue
-            flashes += 1
-            flashed.add(octopus)
-            for neighbour in get_neighbours(*octopus):
-                current_level = None
-                for neighbour_level, xys in octopi.items():
-                    if neighbour in xys:
-                        current_level = neighbour_level
-                        break
-                if current_level is not None and current_level > 0:
-                    octopi[current_level].remove(neighbour)
-                    octopi[min(current_level + 1, 10)].add(neighbour)
+        flashed = octopi.take_step()
+        flashes += flashed
         if step == 100:
             print(f"Part 1: {flashes}")
-        if len(flashed) == 100:
+        if flashed == 100:
+            print(f"Part 2: {step}\n")
             break
-    print(f"Part 2: {step}\n")
 
 
 solve(sample_input)
