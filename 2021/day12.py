104c87ce42d7d11cc66acb8c7e055b344dcda45b
Gid <gsmith.jemba2000@gmail.com>
2021-12-12 12:09:57 +0000

    Day 12

--- 2021/day12.py
+++ 2021/day12.py
@@ -1,32 +1,58 @@
-# import logging
-import math
 import os
-import re
-import string
 
-from collections import defaultdict, Counter
-from itertools import product
+from collections import defaultdict
 
-from grid import XY, ConnectedGrid
-from utils import flatten, grouper, powerset, print_time_taken
+from utils import print_time_taken
 
-# log_file = os.path.join(os.path.dirname(__file__), f"logs/day12.log")
-# logging.basicConfig(level=logging.WARNING, filename=log_file, filemode="w")
 with open(os.path.join(os.path.dirname(__file__), f"inputs/day12_input.txt")) as f:
     actual_input = f.read()
 
-sample_input = """sample"""
+
+sample_input = """dc-end
+HN-start
+start-kj
+dc-start
+dc-HN
+LN-dc
+HN-end
+kj-sa
+kj-HN
+kj-dc"""
+
+
+def find_paths(edges, allow_double_visit: bool = False):
+    number_of_paths, path_allows_double = 0, defaultdict(lambda: allow_double_visit)
+    to_explore = [("start",)]
+    while to_explore:
+        current_path = to_explore.pop()
+        for next_step in edges[current_path[-1]]:
+            if next_step == "end":
+                number_of_paths += 1
+                continue
+            allow_double = path_allows_double[current_path]
+            if next_step.islower() and next_step in current_path:
+                if not allow_double:
+                    continue
+                allow_double = False
+            to_explore.append((*current_path, next_step))
+            path_allows_double[to_explore[-1]] = allow_double
+
+    return number_of_paths
 
 
 @print_time_taken
 def solve(inputs):
-    # lines = inputs.splitlines()
-    # values = list(map(int, inputs.splitlines()))
+    edges = defaultdict(set)
+    for line in inputs.splitlines():
+        a, b = line.split("-")
+        if a != "end" and b != "start":
+            edges[a].add(b)
+        if b != "end" and a != "start":
+            edges[b].add(a)
 
-    print(f"Part 1: {False}")
-
-    print(f"Part 2: {False}\n")
+    print(f"Part 1: {find_paths(edges)}")
+    print(f"Part 2: {find_paths(edges, True)}\n")
 
 
 solve(sample_input)
-# solve(actual_input)
+solve(actual_input)
