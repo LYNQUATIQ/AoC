ece1971628c2632775e31889853fb928803221bf
Gid <gsmith.jemba2000@gmail.com>
2021-12-13 09:26:40 +0000

    Improved Day 13

--- 2021/day13.py
+++ 2021/day13.py
@@ -1,17 +1,7 @@
-# import logging
-import math
 import os
-import re
-import string
 
-from collections import defaultdict, Counter
-from itertools import product
+from utils import print_time_taken
 
-from grid import XY, ConnectedGrid
-from utils import flatten, grouper, powerset, print_time_taken
-
-# log_file = os.path.join(os.path.dirname(__file__), f"logs/day13.log")
-# logging.basicConfig(level=logging.WARNING, filename=log_file, filemode="w")
 with open(os.path.join(os.path.dirname(__file__), f"inputs/day13_input.txt")) as f:
     actual_input = f.read()
 
@@ -40,33 +30,21 @@ fold along x=5"""
 
 @print_time_taken
 def solve(inputs):
-    dot_inputs, fold_inputs = inputs.split("\n\n")
+    dots, folds = inputs.split("\n\n")
+    dots = {map(int, xy.split(",")) for xy in dots.splitlines()}
 
-    dots = set()
-    for xy in dot_inputs.splitlines():
-        x, y = xy.split(",")
-        dots.add((int(x), int(y)))
-
-    folds = []
-    for line in fold_inputs.splitlines():
-        fold_xy, value = line.split()[-1].split("=")
-        folds.append((fold_xy, int(value)))
-
-    def do_fold(dots, xy, value):
+    for step, fold in enumerate(folds.splitlines()):
+        xy, value = fold.split()[-1].split("=")
+        fold_line = int(value)
         new_dots = set()
         for x, y in dots:
-            if xy == "x" and x >= value:
-                x = 2 * value - x
-            if xy == "y" and y >= value:
-                y = 2 * value - y
+            x = 2 * fold_line - x if xy == "x" and x >= fold_line else x
+            y = 2 * fold_line - y if xy == "y" and y >= fold_line else y
             new_dots.add((x, y))
-        return new_dots
+        dots = new_dots
+        if step == 0:
+            print(f"Part 1: {len(dots)}")
 
-    dots = do_fold(dots, *folds[0])
-    print(f"Part 1: {len(dots)}")
-
-    for fold in folds[1:]:
-        dots = do_fold(dots, *fold)
     print(f"Part 2:\n")
     max_x, max_y = max(xy[0] for xy in dots), max(xy[1] for xy in dots)
     for y in range(max_y + 1):
