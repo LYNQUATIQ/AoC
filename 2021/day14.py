80844035eeb8a8840d293b08e49a8737957ba5be
Gid <gsmith.jemba2000@gmail.com>
2021-12-14 09:16:11 +0000

    Improved Day 14

--- 2021/day14.py
+++ 2021/day14.py
@@ -25,24 +25,25 @@ CN -> C"""
 
 
 def solve(inputs):
-    template_polymer, replacements = inputs.split('\n\n')
+    template_polymer, replacements = inputs.split("\n\n")
     replacements = dict(tuple(r.split(" -> ")) for r in replacements.splitlines())
-    
+
     def get_answer(steps):
         elements = Counter(template_polymer)
-        pair_counts = { ab: template_polymer.count(ab) for ab in replacements }
+        pair_counts = {ab: template_polymer.count(ab) for ab in replacements}
         for _ in range(steps):
             new_pair_counts = Counter()
             for (a, b), x in replacements.items():
-                elements[x] += pair_counts[a + b]
-                new_pair_counts[a + x] += pair_counts[a + b]
-                new_pair_counts[x + b] += pair_counts[a + b]
+                occurences = pair_counts[a + b]
+                new_pair_counts[a + x] += occurences
+                new_pair_counts[x + b] += occurences
+                elements[x] += occurences
             pair_counts = new_pair_counts
 
         return max(elements.values()) - min(elements.values())
 
-    print(f'Part 1: {get_answer(10)}')
-    print(f'Part 2: {get_answer(40)}\n')
+    print(f"Part 1: {get_answer(10)}")
+    print(f"Part 2: {get_answer(40)}\n")
 
 
 solve(sample_input)
