1ddc20d4d450c307ca58bf8d760ac308320cf844
Gid <gsmith.jemba2000@gmail.com>
2021-12-15 22:03:22 +0000

    Day 15

--- 2021/day15.py
+++ 2021/day15.py
@@ -1,17 +1,10 @@
-# import logging
-import math
 import os
-import re
-import string
+import sys
 
-from collections import defaultdict, Counter
 from itertools import product
 
-from grid import XY, ConnectedGrid
-from utils import flatten, grouper, powerset, print_time_taken
+from utils import print_time_taken
 
-# log_file = os.path.join(os.path.dirname(__file__), f"logs/day15.log")
-# logging.basicConfig(level=logging.WARNING, filename=log_file, filemode="w")
 with open(os.path.join(os.path.dirname(__file__), f"inputs/day15_input.txt")) as f:
     actual_input = f.read()
 
@@ -29,68 +22,41 @@ sample_input = """1163751742
 
 @print_time_taken
 def solve(inputs):
-    grid = {
+    small_grid = {
         (x, y): int(height)
         for y, line in enumerate(inputs.splitlines())
         for x, height in enumerate(line)
     }
+
+    w, h = (max(xy[0] for xy in small_grid) + 1, max(xy[1] for xy in small_grid) + 1)
+    big_grid = {}
+    for dx, dy in product(range(5), range(5)):
+        for xy, risk in small_grid.items():
+            big_grid[(xy[0] + dx * w, xy[1] + dy * h)] = (risk - 1 + dx + dy) % 9 + 1
+
     neighbours = lambda x, y: ((x + 1, y), (x, y + 1), (x - 1, y), (x, y - 1))
 
-    start = (0, 0)
-    target = (max(xy[0] for xy in grid), max(xy[1] for xy in grid))
+    def find_shortest_path(grid):
+        start, target = (0, 0), (max(xy[0] for xy in grid), max(xy[1] for xy in grid))
+        to_visit = {start: 0}
+        total_risk_levels = dict()
+        while to_visit:
+            current_node = min(to_visit, key=to_visit.get)
+            grid.pop(current_node)
+            risk_to_here = to_visit.pop(current_node)
+            total_risk_levels[current_node] = risk_to_here
+            if current_node == target:
+                break
+            for next_node in (n for n in neighbours(*current_node) if n in grid):
+                total_risk_to_next_node = grid[next_node] + risk_to_here
+                if total_risk_to_next_node < to_visit.get(next_node, sys.maxsize):
+                    to_visit[next_node] = total_risk_to_next_node
 
-    def find_shortest_path(grid, start, target):
-        # best_paths is a dict of nodes mapped to a tuple of (previous node, risk_level)
-        best_paths = {start: (None, 0)}
-        current_node = start
-        visited = set()
-        while current_node != target:
-            visited.add(current_node)
-            next_nodes = tuple(xy for xy in neighbours(*current_node) if xy in grid)
-            risk_level_to_here = best_paths[current_node][1]
-            for next_node in next_nodes:
-                risk_level = grid[next_node] + risk_level_to_here
-                if next_node not in best_paths:
-                    best_paths[next_node] = (current_node, risk_level)
-                else:
-                    current_lowest_risk_level = best_paths[next_node][1]
-                    if current_lowest_risk_level > risk_level:
-                        best_paths[next_node] = (current_node, risk_level)
+        return total_risk_levels[target]
 
-            next_steps = {
-                node: best_paths[node] for node in best_paths if node not in visited
-            }
-            if not next_steps:
-                raise ValueError("No path possible")
-            # next node is the destination with the lowest risk_level
-            current_node = min(next_steps, key=lambda k: next_steps[k][1])
-
-        # Work back through destinations in shortest path
-        path = []
-        while current_node is not None:
-            path.append(current_node)
-            next_node = best_paths[current_node][0]
-            current_node = next_node
-        return path[:-1]
-
-    total_risk_level = sum(grid[xy] for xy in find_shortest_path(grid, start, target))
-    print(f"Part 1: {total_risk_level}")
-
-    big_grid = {}
-    for my in range(5):
-        for mx in range(5):
-            for xy, risk_level in grid.items():
-                big_grid[
-                    (xy[0] + mx * (target[0] + 1), xy[1] + my * (target[1] + 1))
-                ] = (risk_level - 1 + mx + my) % 9 + 1
-
-    target = (target[0] * 5, target[1] * 5)
-
-    total_risk_level = sum(
-        big_grid[xy] for xy in find_shortest_path(big_grid, start, target)
-    )
-    print(f"Part 2: {total_risk_level}\n")
+    print(f"Part 1: {find_shortest_path(small_grid)}")
+    print(f"Part 2: {find_shortest_path(big_grid)}\n")
 
 
 solve(sample_input)
-# solve(actual_input)
+solve(actual_input)
