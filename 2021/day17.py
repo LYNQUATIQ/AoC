c84592c36fc37321a26515e9375f0d190f53b292
Gid <gsmith.jemba2000@gmail.com>
2021-12-17 16:49:56 +0000

    Improved Day 17

--- 2021/day17.py
+++ 2021/day17.py
@@ -9,16 +9,14 @@ def solve(inputs):
 
     left, right, bottom, top = map(int, re.findall(r"-?\d+", inputs))
 
-    # The highest shot will be the one that hits the water and in the next step hits the
-    # bottom edge of the target. It hits the water at the same velocity it rose at, so
+    # The highest shot will be the one that hits the water and in the *next* step hits
+    # the bottom of the target. It hits the water at the same velocity it rose at, so
     # the velocity needs to equal the (absolute) depth (will also be the max velocity)
 
     print(f"Part 1: {sum(range(abs(bottom)))}")
 
     valid_velocities = 0
-    min_vx, max_vx = 1, right
-    min_vy, max_vy = bottom, abs(bottom)
-    for vx, vy in product(range(min_vx, max_vx + 1), range(min_vy, max_vy + 1)):
+    for vx, vy in product(range(1, right + 1), range(bottom, abs(bottom) + 1)):
         x, y = 0, 0
         while y >= bottom:
             x, y = x + vx, y + vy
