f527068c0285a94315d7acfef9d82166134a478e
Gid <gsmith.jemba2000@gmail.com>
2021-12-17 16:48:05 +0000

    Improved Day 17

--- 2021/day17.py
+++ 2021/day17.py
@@ -1,7 +1,5 @@
-from collections import defaultdict
-from itertools import product
-from enum import Enum
 import re
+from itertools import product
 
 sample_input = """target area: x=20..30, y=-10..-5"""
 actual_input = """target area: x=96..125, y=-144..-98"""
@@ -12,36 +10,24 @@ def solve(inputs):
     left, right, bottom, top = map(int, re.findall(r"-?\d+", inputs))
 
     # The highest shot will be the one that hits the water and in the next step hits the
-    # bottom. It hits the water at the same velocity it rose at so the velocity needs to
-    # equal the (absolute) depth (and will also be the maximum velocity)
+    # bottom edge of the target. It hits the water at the same velocity it rose at, so
+    # the velocity needs to equal the (absolute) depth (will also be the max velocity)
 
+    print(f"Part 1: {sum(range(abs(bottom)))}")
+
+    valid_velocities = 0
     min_vx, max_vx = 1, right
     min_vy, max_vy = bottom, abs(bottom)
-
-    print(f"Part 1: {sum(range(max_vy))}")
-
-    def test_shot(self, vx, vy):
+    for vx, vy in product(range(min_vx, max_vx + 1), range(min_vy, max_vy + 1)):
         x, y = 0, 0
-        while y >= self.bottom:
-            x, y = x + vx, y + vy
-            vx = max(vx + (1 if x < 0 else -1 if x > 0 else 0), 0)
-            vy -= 1
-            if self.hit(x, y):
-                return self.HIT
-        if x < self.left:
-            return self.SHORT
-        return self.LONG
-
-    valid_velocities = set()
-    for vx0, vy0 in product(range(min_vx, max_vx + 1), range(min_vy, max_vy + 1)):
-        x, y, vx, vy = 0, 0, vx0, vy0
         while y >= bottom:
             x, y = x + vx, y + vy
-            if left <= x <= right and bottom <= y <= top:
-                valid_velocities.add((vx0, vy0))
             vx, vy = max(vx - 1, 0), vy - 1
+            if left <= x <= right and bottom <= y <= top:
+                valid_velocities += 1
+                break
 
-    print(f"Part 2: {len(valid_velocities)}\n")
+    print(f"Part 2: {valid_velocities}\n")
 
 
 solve(sample_input)
