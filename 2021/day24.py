5ba5f59370d457ce30aa73cdecc07972c361d468
Gid <gsmith.jemba2000@gmail.com>
2021-12-24 17:04:02 +0000

    Day 24

--- 2021/day24.py
+++ 2021/day24.py
@@ -1,27 +1,13 @@
 """https://adventofcode.com/2021/day/24"""
-import logging
-import math
-import os
-import re
-import string
+from functools import lru_cache
 
-from collections import defaultdict, Counter, deque
-from dataclasses import dataclass, field
-from itertools import product
-from functools import cache, lru_cache
+from utils import print_time_taken
 
-from grid import XY, ConnectedGrid
-from utils import flatten, grouper, powerset, print_time_taken
-
-log_file = os.path.join(os.path.dirname(__file__), f"logs/day24.log")
-logging.basicConfig(level=logging.INFO, filename=log_file, filemode="w")
-with open(os.path.join(os.path.dirname(__file__), f"inputs/day24_input.txt")) as f:
-    actual_input = f.read()
 
 PARAMS = [
     (1, 14, 14),
     (1, 14, 2),
-    (1, 14, 11),
+    (1, 14, 1),
     (1, 12, 13),
     (1, 15, 5),
     (26, -12, 5),
@@ -36,47 +22,50 @@ PARAMS = [
 ]
 
 
-@lru_cache
-def get_next_z(digit: int, z: int, z_div, constant1, constant2) -> int:
-    x = z % 26
+@lru_cache(maxsize=None)
+def get_next_z(w: int, z: int, z_div: int, xc: int, yc: int) -> int | None:
+
+    # N.B. When z_div is 1, xc is *always* > 10, therefore flag is 1 in all cases
+    # and code reduces to:
+    #     new_z = z * 26 + (digit + yc)   ...essentially pushing a digit in base 26
+    #
+    # In cases where z_div is 26 we pop a digit *unless* flag is 1 in which case we
+    # remultiply by 26. We therefore *need* flag too be 0 to ensure we pop digit,
+    # (as well as needing constants to tie up) or no chance of being zero at end!
+    flag = int(((z % 26) + xc) != w)
+    if z_div == 26 and flag:
+        return None
+
     z = z // z_div
-    x += constant1
-    x = int(x != digit)
-    y = 25 * x + 1
-    z *= y
-    y = digit + constant2
-    y *= x
-    return z + y
+    y = 25 * flag + 1
+    z *= 25 * flag + 1
+    z += (w + yc) * flag
+    return z
+
+
+def find_result(digits, so_far=[], z=0):
+    num_digits = len(so_far)
+
+    if num_digits == 14:
+        if z == 0:
+            return "".join(map(str, so_far))
+        return None
+
+    z_div, xc, yc = PARAMS[num_digits]
+    for w in digits:
+        next_z = get_next_z(w, z, z_div, xc, yc)
+        if next_z is None:
+            continue
+        result = find_result(digits, so_far + [w], next_z)
+        if result:
+            return result
 
 
 @print_time_taken
-def solve(inputs):
+def solve():
 
-    model_number, delta = 99999999999999, -1
-    while True:
-        model_number += delta
-        while "0" in str(model_number):
-            model_number += delta
-        z = 0
-        for digit, params in zip(str(model_number), PARAMS):
-            z = get_next_z(int(digit), z, *params)
-        if z == 0:
-            break
-
-    print(f"Part 1: {model_number}")
-
-    model_number, delta = 11111111111111, +1
-    while True:
-        model_number += delta
-        while "0" in str(model_number):
-            model_number += delta
-        z = 0
-        for digit, params in zip(str(model_number), PARAMS):
-            z = get_next_z(int(digit), z, *params)
-        if z == 0:
-            break
-
-    print(f"Part 2: {False}\n")
+    print(f"Part 1: {find_result(tuple(range(9,0,-1)))}")
+    print(f"Part 2: {find_result(tuple(range(1,10)))}\n")
 
 
-solve(actual_input)
+solve()
