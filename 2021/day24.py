20155c95825ff8d8b9ac86988924677cb478b79b
Gid <gsmith.jemba2000@gmail.com>
2021-12-27 15:37:21 +0000

    Read actual inputs - Day 24

--- 2021/day24.py
+++ 2021/day24.py
@@ -1,71 +1,64 @@
 """https://adventofcode.com/2021/day/24"""
+import os
 from functools import lru_cache
 
-from utils import print_time_taken
+with open(os.path.join(os.path.dirname(__file__), f"inputs/day24_input.txt")) as f:
+    actual_input = f.read()
 
-
-PARAMS = [
-    (1, 14, 14),
-    (1, 14, 2),
-    (1, 14, 1),
-    (1, 12, 13),
-    (1, 15, 5),
-    (26, -12, 5),
-    (26, -12, 5),
-    (1, 12, 9),
-    (26, -7, 3),
-    (1, 13, 13),
-    (26, -8, 2),
-    (26, -5, 1),
-    (26, -10, 11),
-    (26, -7, 8),
-]
+Z_BASE, FLAG_DELTA, Z_DELTA = 0, 1, 2
+PARAMS = tuple(
+    tuple(
+        int(instruction.split(" ")[-1])
+        for i, instruction in enumerate(routine.splitlines())
+        if i in (3, 4, 14)  # Lines where we find Z_BASE, FLAG_DELTA, and Z_DELTA
+    )
+    for routine in actual_input.split("inp w\n")[1:]
+)
+DIGITS_1_TO_9 = tuple(range(1, 10))
+DIGITS_9_TO_1 = tuple(reversed(DIGITS_1_TO_9))
 
 
 @lru_cache(maxsize=None)
-def get_next_z(w: int, z: int, z_div: int, xc: int, yc: int) -> int | None:
+def get_next_z(w: int, z: int, args: tuple[int, int, int]) -> int | None:
+    """Return the next value of z based on input w and arguments.
 
-    # N.B. When z_div is 1, xc is *always* > 10, therefore flag is 1 in all cases
-    # and code reduces to:
-    #     new_z = z * 26 + (digit + yc)   ...essentially pushing a digit in base 26
-    #
-    # In cases where z_div is 26 we pop a digit *unless* flag is 1 in which case we
-    # remultiply by 26. We therefore *need* flag too be 0 to ensure we pop digit,
-    # (as well as needing constants to tie up) or no chance of being zero at end!
-    flag = int(((z % 26) + xc) != w)
-    if z_div == 26 and flag:
+    N.B. When Z_BASE is 1, FLAG_DELTA is *always* > 10, therefore flag will be 1 in
+    *all* cases and code reduces to:  new_z = z * 26 + (w + Z_DELTA)
+    ...essentially pushing a digit in base 26
+
+    In cases where Z_BASE is 26 we pop a digit *unless* flag is 1 in which case we
+    remultiply by 26. We therefore *need* flag too be 0 to ensure we pop digit,
+    (as well as needing constants to tie up) or no chance of being zero at end!
+    If flag is not 0 and Z_BASE is 26, we can return None and prune that path.
+    """
+    flag = int(((z % 26) + args[FLAG_DELTA]) != w)
+    if args[Z_BASE] == 26 and flag:
         return None
-
-    z = z // z_div
-    y = 25 * flag + 1
+    z = z // args[Z_BASE]
     z *= 25 * flag + 1
-    z += (w + yc) * flag
+    z += (w + args[Z_DELTA]) * flag
     return z
 
 
-def find_result(digits, so_far=[], z=0):
-    num_digits = len(so_far)
-
-    if num_digits == 14:
-        if z == 0:
-            return "".join(map(str, so_far))
+def find_result(z, ordered_inputs, digits=[]):
+    digit_count = len(digits)
+    if digit_count == 14:
+        if z == 0:  # Success!
+            return "".join(map(str, digits))
         return None
 
-    z_div, xc, yc = PARAMS[num_digits]
-    for w in digits:
-        next_z = get_next_z(w, z, z_div, xc, yc)
+    for w in ordered_inputs:
+        next_z = get_next_z(w, z, PARAMS[digit_count])
         if next_z is None:
-            continue
-        result = find_result(digits, so_far + [w], next_z)
+            continue  # Pruning (when flag is 1 and Z_BASE is 26)
+        result = find_result(next_z, ordered_inputs, digits + [w])
         if result:
             return result
 
 
-@print_time_taken
 def solve():
-
-    print(f"Part 1: {find_result(tuple(range(9,0,-1)))}")
-    print(f"Part 2: {find_result(tuple(range(1,10)))}\n")
+    print(f"Part 1: {find_result(0, DIGITS_9_TO_1)}")
+    print(f"Part 2: {find_result(0, DIGITS_1_TO_9)}\n")
 
 
 solve()
