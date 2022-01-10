da988937f993ba20e5f5ae69882a8a04378267e5
Gid <gsmith.jemba2000@gmail.com>
2022-01-10 11:19:19 +0000

    2018 Day 14

--- Archive/2018/day14.py
+++ Archive/2018/day14.py
@@ -3,33 +3,30 @@
 from utils import print_time_taken
 
 
-actual_input = 864801
+INITIAL_RECIPES = 37
 
 
 @print_time_taken
-def solve(target_length, target_number):
-    recipes = {0: 3, 1: 7}
-    elf1, elf2 = 0, 1
-    total_recipes = 2
-    num_digits = 10 ** (len(str(target_number)))
-    most_recent = 37
+def solve(part1_length, target):
+    elf1, elf2, recipes = 0, 1, list(divmod(INITIAL_RECIPES, 10))
+    most_recent, target_modulo = INITIAL_RECIPES, 10 ** (len(str(target)))
     done = False
     while not done:
-        total = recipes[elf1] + recipes[elf2]
+        elf1_quality, elf2_quality = recipes[elf1], recipes[elf2]
+        total = elf1_quality + elf2_quality
         for recipe in divmod(total, 10) if total > 9 else (total,):
-            recipes[total_recipes] = recipe
-            total_recipes += 1
-            most_recent = (most_recent * 10 + recipe) % num_digits
-            if most_recent == target_number:
+            recipes.append(recipe)
+            most_recent = (most_recent * 10 + recipe) % target_modulo
+            if most_recent == target:
                 done = True
                 break
-        elf1 = (elf1 + recipes[elf1] + 1) % total_recipes
-        elf2 = (elf2 + recipes[elf2] + 1) % total_recipes
+        elf1 = (elf1 + elf1_quality + 1) % len(recipes)
+        elf2 = (elf2 + elf2_quality + 1) % len(recipes)
 
-    next10 = "".join(str(recipes[i]) for i in range(target_length, target_length + 10))
+    next10 = "".join(str(recipes[i]) for i in range(part1_length, part1_length + 10))
     print(f"Part 1: {next10}")
-    print(f"Part 2: {total_recipes - len(str(target_number))}\n")
+    print(f"Part 2: {len(recipes) - len(str(target))}\n")
 
 
 solve(9, 92510)
-solve(actual_input, actual_input)
+solve(864801, 864801)
