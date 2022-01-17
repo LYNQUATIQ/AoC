531f7ed7543c4528d2a36183a4f36949175b8e5d
Gid <gsmith.jemba2000@gmail.com>
2022-01-17 06:40:02 +0000

    Day 25

--- Archive/2018/day25.py
+++ Archive/2018/day25.py
@@ -2,9 +2,6 @@
 import os
 import re
 
-from collections import defaultdict
-from itertools import combinations
-
 from utils import print_time_taken
 
 with open(os.path.join(os.path.dirname(__file__), f"inputs/day25_input.txt")) as f:
@@ -22,35 +19,22 @@ sample_input = """1,-1,-1,-2
 -1,-2,0,-2"""
 
 
-class Point(tuple):
-    def __new__(cls, *_tuple):
-        return tuple.__new__(cls, _tuple)
-
-    def __add__(self, other):
-        return type(self)(*(a + b for a, b in zip(self, other)))
-
-    def __sub__(self, other):
-        return type(self)(*(a - b for a, b in zip(self, other)))
-
-
 @print_time_taken
 def solve(inputs):
-    points = [Point(*map(int, (re.findall(r"-?\d+", l)))) for l in inputs.splitlines()]
+    points = [tuple(map(int, (re.findall(r"-?\d+", l)))) for l in inputs.splitlines()]
     constellations = {p: p for p in points}
 
     for i, point in enumerate(points[:-1]):
         my_constellation = constellations[point]
         for other in points[i + 1 :]:
-            if sum(map(abs, point - other)) <= 3:
+            if sum(abs(a - b) for a, b in zip(point, other)) <= 3:
                 neighbours_constellation = constellations[other]
-                if neighbours_constellation == my_constellation:
-                    continue
-                for x in (
+                for merge in (
                     p
                     for p, c in constellations.items()
                     if c == neighbours_constellation
                 ):
-                    constellations[x] = my_constellation
+                    constellations[merge] = my_constellation
 
     print(f"Part 1: {len(set(constellations.values()))}")
 
