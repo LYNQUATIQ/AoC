38ef3adbe4d4145f39c0a48acf7e59454466a62a
Gid <gsmith.jemba2000@gmail.com>
2022-12-01 05:21:53 +0000

    Add newlines to template answers

--- 2022/day06.py
+++ 2022/day06.py
@@ -11,8 +11,8 @@ SAMPLE_INPUT = """xxx"""
 def solve(inputs: str) -> None:
     values = tuple(map(int, inputs.splitlines()))
 
-    print(f"Part 1: {False}")
-    print(f"Part 2: {False}")
+    print(f"\nPart 1: {False}")
+    print(f"Part 2: {False}\n")
 
 
 solve(SAMPLE_INPUT)
