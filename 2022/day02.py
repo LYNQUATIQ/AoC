3d5e9113aa80c6347a4ffd0aa35f766d26262f16
Gid <gsmith.jemba2000@gmail.com>
2022-12-02 05:49:58 +0000

    Day2

--- 2022/day02.py
+++ 2022/day02.py
@@ -1,19 +1,55 @@
 """https://adventofcode.com/2022/day/2"""
 import os
 
+from enum import IntEnum
+
+
 with open(os.path.join(os.path.dirname(__file__), f"inputs/day02_input.txt")) as f:
     actual_input = f.read()
 
 
-SAMPLE_INPUT = """xxx"""
+SAMPLE_INPUT = """A Y
+B X
+C Z
+"""
+
+
+ROCK, PAPER, SCISSORS = 1, 2, 3
+SHAPES = {"A": ROCK, "B": PAPER, "C": SCISSORS, "X": ROCK, "Y": PAPER, "Z": SCISSORS}
+
+WIN, DRAW, LOSE = 6, 3, 0
+RESULTS = {"X": LOSE, "Y": DRAW, "Z": WIN}
+
+OUTCOMES = {
+    ROCK: {ROCK: DRAW, PAPER: LOSE, SCISSORS: WIN},
+    PAPER: {ROCK: WIN, PAPER: DRAW, SCISSORS: LOSE},
+    SCISSORS: {ROCK: LOSE, PAPER: WIN, SCISSORS: DRAW},
+}
+
+STRATEGIES = {
+    WIN: {ROCK: PAPER, PAPER: SCISSORS, SCISSORS: ROCK},
+    DRAW: {ROCK: ROCK, PAPER: PAPER, SCISSORS: SCISSORS},
+    LOSE: {ROCK: SCISSORS, PAPER: ROCK, SCISSORS: PAPER},
+}
 
 
 def solve(inputs: str) -> None:
-    values = tuple(map(int, inputs.splitlines()))
+    total_score = 0
+    for round in inputs.splitlines():
+        token1, token2 = round.split()
+        opponent, you = SHAPES[token1], SHAPES[token2]
+        result = OUTCOMES[you][opponent]
+        total_score += result + you
+    print(f"\nPart 1: {total_score}")
 
-    print(f"\nPart 1: {False}")
-    print(f"Part 2: {False}\n")
+    total_score = 0
+    for round in inputs.splitlines():
+        token1, token2 = round.split()
+        opponent, result = SHAPES[token1], RESULTS[token2]
+        you = STRATEGIES[result][opponent]
+        total_score += result + you
+    print(f"Part 2: {total_score}\n")
 
 
 solve(SAMPLE_INPUT)
-# solve(actual_input)
+solve(actual_input)
