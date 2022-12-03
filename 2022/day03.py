ebb9eda6a8781e6502072dd7629e49179751fb6f
Gid <gsmith.jemba2000@gmail.com>
2022-12-03 06:18:32 +0000

    Simpler chunking

--- 2022/day03.py
+++ 2022/day03.py
@@ -14,11 +14,6 @@ ttgJtRGJQctTZtZT
 CrZsJsPPZsGzwwsLwLmpwMDw"""
 
 
-def grouper(iterable, n):
-    args = [iter(iterable)] * n
-    return zip(*args)
-
-
 def solve(inputs: str) -> None:
     rucksacks = inputs.splitlines()
 
@@ -30,9 +25,9 @@ def solve(inputs: str) -> None:
     print(f"\nPart 1: {sum(string.ascii_letters.index(c) + 1 for c in dupes)}")
 
     badges = []
-    for packs in grouper(rucksacks, 3):
+    for i in range(0, len(rucksacks), 3):
         for c in string.ascii_letters:
-            if all(c in pack for pack in packs):
+            if all(c in pack for pack in rucksacks[i : i + 3]):
                 badges.append(c)
     print(f"Part 2: {sum(string.ascii_letters.index(c) + 1 for c in badges)}\n")
 
