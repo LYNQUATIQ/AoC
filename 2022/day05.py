6ca7d6e6c69b3e4938b9079cb9fc10525903a20f
Gid <gsmith.jemba2000@gmail.com>
2022-12-05 08:08:53 +0000

    Add comments

--- 2022/day05.py
+++ 2022/day05.py
@@ -24,25 +24,27 @@ def move_crates(arrangement: str, procedures: str, is_9001: bool = False) -> str
 
     stacks: dict[int, deque[str]] = defaultdict(deque)
 
+    # Read arrangement into stacks data structure (dict of deques)
     rows = arrangement.splitlines()
-    columns = int(rows.pop().split()[-1])
-    for row, column in product(rows, range(columns)):
+    n_columns = int(rows.pop().split()[-1])
+    for row, column in product(rows, range(n_columns)):
         crate = row[column * 4 + 1]
         if crate != " ":
             stacks[column + 1].append(crate)
 
+    # Perform each move popping crates off one by one (re-reversing if crane is 9001)
     for move in procedures.splitlines():
         n, move_from, move_to = map(int, re.findall(r"\d+", move))
         moving = [stacks[move_from].popleft() for _ in range(n)]
         for crate in moving[::-1] if is_9001 else moving:
             stacks[move_to].appendleft(crate)
 
-    return "".join([stacks[c + 1][0] for c in range(columns)])
+    # Return top (first) crate in each stack
+    return "".join([stacks[c + 1][0] for c in range(n_columns)])
 
 
 def solve(inputs: str) -> None:
     arrangement, procedures = inputs.split("\n\n")
-
     print(f"Part 1: {move_crates(arrangement, procedures)}")
     print(f"Part 2: {move_crates(arrangement, procedures, is_9001=True)}\n")
 
