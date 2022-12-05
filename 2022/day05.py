dab84412c6718ae1ab01f26ff762e08ea087cd2e
Gid <gsmith.jemba2000@gmail.com>
2022-12-05 05:48:23 +0000

    Improved Day 5

--- 2022/day05.py
+++ 2022/day05.py
@@ -24,44 +24,30 @@ def solve(inputs: str) -> None:
     rows = arrangement.splitlines()
     moves = procedures.splitlines()
 
-    stacks = defaultdict(deque)
     columns = int((rows[-1][-2]))
+
+    stacks_9000: dict[int, deque[str]] = defaultdict(deque)
+    stacks_9001: dict[int, deque[str]] = defaultdict(deque)
+
     for row in rows[:-1]:
         for column in range(1, columns + 1):
             crate = row[column * 4 - 3]
             if crate != " ":
-                stacks[column].append(crate)
+                stacks_9000[column].append(crate)
+                stacks_9001[column].append(crate)
 
     for move in moves:
         n, column_1, column_2 = map(int, re.findall(r"\d+", move))
+
         for _ in range(n):
-            stacks[column_2].appendleft(stacks[column_1].popleft())
+            stacks_9000[column_2].appendleft(stacks_9000[column_1].popleft())
 
-    top_row = ""
-    for column in range(1, columns + 1):
-        top_row += stacks[column][0]
-
-    print(f"\nPart 1: {top_row}")
-
-    stacks = defaultdict(deque)
-    columns = int((rows[-1][-2]))
-    for row in rows[:-1]:
-        for column in range(1, columns + 1):
-            crate = row[column * 4 - 3]
-            if crate != " ":
-                stacks[column].append(crate)
-
-    for move in moves:
-        n, column_1, column_2 = map(int, re.findall(r"\d+", move))
-        moving = [stacks[column_1].popleft() for _ in range(n)]
+        moving = [stacks_9001[column_1].popleft() for _ in range(n)]
         for crate in moving[::-1]:
-            stacks[column_2].appendleft(crate)
+            stacks_9001[column_2].appendleft(crate)
 
-    top_row = ""
-    for column in range(1, columns + 1):
-        top_row += stacks[column][0]
-
-    print(f"Part 2: {top_row}\n")
+    print(f"\nPart 1: {''.join([stacks_9000[c][0] for c in range(1, columns + 1)])}")
+    print(f"Part 2: {''.join([stacks_9001[c][0] for c in range(1, columns + 1)])}\n")
 
 
 solve(SAMPLE_INPUT)
