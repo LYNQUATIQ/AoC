83d1f30b088e50dfa0cad4783468e4f0c245379c
Gid <gsmith.jemba2000@gmail.com>
2022-12-05 08:01:41 +0000

    Use product to itertate over arrangement

--- 2022/day05.py
+++ 2022/day05.py
@@ -3,6 +3,7 @@ import os
 import re
 
 from collections import defaultdict, deque
+from itertools import product
 
 with open(os.path.join(os.path.dirname(__file__), f"inputs/day05_input.txt")) as f:
     actual_input = f.read()
@@ -20,21 +21,21 @@ move 1 from 1 to 2"""
 
 
 def move_crates(arrangement: str, procedures: str, is_9001: bool = False) -> str:
-    rows = arrangement.splitlines()
-    columns = int(rows.pop().split()[-1])
 
     stacks: dict[int, deque[str]] = defaultdict(deque)
-    for row in rows:
-        for column in range(columns):
-            crate = row[column * 4 + 1]
-            if crate != " ":
-                stacks[column + 1].append(crate)
+
+    rows = arrangement.splitlines()
+    columns = int(rows.pop().split()[-1])
+    for row, column in product(rows, range(columns)):
+        crate = row[column * 4 + 1]
+        if crate != " ":
+            stacks[column + 1].append(crate)
 
     for move in procedures.splitlines():
-        n, column_1, column_2 = map(int, re.findall(r"\d+", move))
-        moving = [stacks[column_1].popleft() for _ in range(n)]
+        n, move_from, move_to = map(int, re.findall(r"\d+", move))
+        moving = [stacks[move_from].popleft() for _ in range(n)]
         for crate in moving[::-1] if is_9001 else moving:
-            stacks[column_2].appendleft(crate)
+            stacks[move_to].appendleft(crate)
 
     return "".join([stacks[c + 1][0] for c in range(columns)])
 
@@ -42,7 +43,7 @@ def move_crates(arrangement: str, procedures: str, is_9001: bool = False) -> str
 def solve(inputs: str) -> None:
     arrangement, procedures = inputs.split("\n\n")
 
-    print(f"\nPart 1: {move_crates(arrangement, procedures)}")
+    print(f"Part 1: {move_crates(arrangement, procedures)}")
     print(f"Part 2: {move_crates(arrangement, procedures, is_9001=True)}\n")
 
 
