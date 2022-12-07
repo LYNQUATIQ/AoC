8336fcdbf0777dd8eee4785a18d8ec044752a4d7
Gid <gsmith.jemba2000@gmail.com>
2022-12-07 06:33:05 +0000

    Use abstract base FileSystemObject class

--- 2022/day07.py
+++ 2022/day07.py
@@ -1,9 +1,10 @@
 """https://adventofcode.com/2022/day/7"""
-
 from __future__ import annotations
+
 import os
 
-from dataclasses import dataclass, field
+from abc import ABC, abstractmethod
+from typing import cast
 
 with open(os.path.join(os.path.dirname(__file__), f"inputs/day07_input.txt")) as f:
     actual_input = f.read()
@@ -34,77 +35,72 @@ $ ls
 7214296 k"""
 
 
-@dataclass
-class File:
-    size: int
-    name: str
+class FileSystemObject(ABC):
+    def __init__(self, container: Directory | None, name: str) -> None:
+        self.container = container
+        self.name = name
+
+    @property
+    @abstractmethod
+    def size(self) -> int:
+        """Returns the size of the file system object"""
 
 
-@dataclass
-class Directory:
-    name: str
-    parent_dir: Directory | None = None
-    files: dict[str, File] = field(default_factory=dict)
-    directories: dict[str, Directory] = field(default_factory=dict)
+class Directory(FileSystemObject):
+    def __init__(self, container: Directory | None, name: str) -> None:
+        super().__init__(container, name)
+        self.contents: dict[str, FileSystemObject] = {}
 
     @property
     def size(self) -> int:
-        return sum(x.size for x in self.files.values()) + sum(
-            x.size for x in self.directories.values()
-        )
+        return sum(x.size for x in self.contents.values())
+
+
+class File(FileSystemObject):
+    def __init__(self, container: Directory, name: str, size: int) -> None:
+        super().__init__(container, name)
+        self._size = size
 
     @property
-    def path(self) -> str:
-        if self.parent_dir is None:
-            return ""
-        return f"{self.parent_dir.path}/{self.name}"
+    def size(self) -> int:
+        return self._size
 
 
 def solve(inputs: str) -> None:
-    commands = inputs.split("$ ")
 
-    root_dir = Directory("/")
-    current_dir = root_dir
-    all_directories = {"/": root_dir}
+    root = Directory(container=None, name="/")
+    current = root
+    all_directories = {root}
 
-    for command in commands:
-        if not command:
-            continue
-        lines = command.splitlines()
-        instruction, output = lines[0], lines[1:]
+    for command in inputs.split("$ "):
 
-        if instruction.startswith("cd"):
-            _, target = instruction.split()
+        if command.startswith("cd"):
+            _, target = command.split()
             if target == "/":
-                current_dir = root_dir
+                current = root
             elif target == "..":
-                assert current_dir.parent_dir is not None
-                current_dir = current_dir.parent_dir
+                assert current.container is not None
+                current = current.container
             else:
-                current_dir = current_dir.directories[target]
+                assert isinstance(current.contents[target], Directory)
+                current = cast(Directory, current.contents[target])
 
-        if instruction.startswith("ls"):
-            for item in output:
-                token_1, name = item.split()
-                if token_1 == "dir":
-                    if name not in current_dir.directories:
-                        new_dir = Directory(name, current_dir)
-                        current_dir.directories[name] = new_dir
-                        all_directories[new_dir.path] = new_dir
-                else:
-                    if name not in current_dir.files:
-                        current_dir.files[name] = File(size=int(token_1), name=name)
+        if command.startswith("ls"):
+            for item in command.splitlines()[1:]:
+                header, name = item.split()
+                if name not in current.contents:
+                    if header == "dir":
+                        directory = Directory(current, name)
+                        current.contents[name] = directory
+                        all_directories.add(directory)
+                    else:
+                        current.contents[name] = File(current, name, int(header))
 
-    print(
-        f"Part 1: {sum(d.size for d in all_directories.values() if d.size < 100_000)}"
-    )
+    print(f"Part 1: {sum(d.size for d in all_directories if d.size < 100_000)}")
 
-    current_dir = root_dir
-    available = 70_000_000 - root_dir.size
+    available = 70_000_000 - root.size
     required = 30_000_000 - available
-
-    candidates = [d.size for d in all_directories.values() if d.size > required]
-
+    candidates = [d.size for d in all_directories if d.size > required]
     print(f"Part 2: {min(candidates)}\n")
 
 
