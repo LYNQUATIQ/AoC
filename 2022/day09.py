6262cc2200b260ed9361a987e24778735fe2dbfe
Gid <gsmith.jemba2000@gmail.com>
2022-12-09 10:29:35 +0000

    Do in a single  pass

--- 2022/day09.py
+++ 2022/day09.py
@@ -16,35 +16,27 @@ R 2"""
 
 # fmt:off
 TAIL_MOVE = {
-    (-2, -2): (-1, -1), (-2, -1): (-1, -1), (-1, -2): (-1, -1), # Above and to the left
-    (2, -2): (1, -1), (1, -2): (1, -1), (2, -1): (1, -1),       # Above and to the right
-    (-2, 2): (-1, 1), (-2, 1): (-1, 1), (-1, 2): (-1, 1),       # Below and to the left
-    (2, 2): (1, 1), (1, 2): (1, 1), (2, 1): (1, 1),             # Below and to the right
-    (-2, 0): (-1, 0), (2, 0): (1, 0),                           # Left and right
-    (0, -2): (0, -1), (0, 2): (0, 1),                           # Above and below
+    -2-2j: -1-1j, -2-1j: -1-1j, -1-2j: -1-1j,  # Above and to the left
+    2-2j: 1-1j, 1-2j: 1-1j, 2-1j: 1-1j,        # Above and to the right
+    -2+2j: -1+1j, -2+1j :-1+1j, -1+2j: -1+1j,  # Below and to the left
+    2+2j: 1+1j, 1+2j: 1+1j, 2+1j: 1+1j,        # Below and to the right
+    -2: -1, 2: 1, -2j: -1j, 2j: 1j,            # Above, below, left or right
 }
 # fmt:on
 
 
-def move_rope(number_of_knots: int, moves: list[tuple[str, int]]) -> int:
-    knots = [[0, 0] for _ in range(number_of_knots)]
-    head, tail = knots[0], knots[-1]
-    tail_positions = {tuple(tail)}
-    for direction, distance in moves:
-        for _ in range(distance):
-            head[0] += {"L": -1, "R": 1}.get(direction, 0)
-            head[1] += {"U": -1, "D": 1}.get(direction, 0)
-            for h, t in zip(knots[:-1], knots[1:]):
-                dx, dy = TAIL_MOVE.get((h[0] - t[0], h[1] - t[1]), (0, 0))
-                t[0], t[1] = t[0] + dx, t[1] + dy
-            tail_positions.add(tuple(tail))
-    return len(tail_positions)
-
-
 def solve(inputs: str) -> None:
-    moves = [(d, int(n)) for d, n in (l.split() for l in inputs.splitlines())]
-    print(f"Part 1: {move_rope(2,moves)}")
-    print(f"Part 2: {move_rope(10,moves)}\n")
+    knots = [0 + 0j] * 10
+    visited = [{knot} for knot in knots]
+    for direction, distance in [l.split() for l in inputs.splitlines()]:
+        for _ in range(int(distance)):
+            knots[0] += {"L": -1, "R": 1, "U": -1j, "D": 1j}[direction]
+            for i in range(1, 10):
+                knots[i] += TAIL_MOVE.get(knots[i - 1] - knots[i], 0)
+                visited[i].add(knots[i])
+
+    print(f"Part 1: {len(visited[1])}")
+    print(f"Part 2: {len(visited[9])}\n")
 
 
 solve(sample_input)
