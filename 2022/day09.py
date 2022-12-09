eb3ae8a5973dd2ee289760506ae1d9e9e729b79a
Gid <gsmith.jemba2000@gmail.com>
2022-12-09 08:50:19 +0000

    Simplify day 9 with lists and a dict

--- 2022/day09.py
+++ 2022/day09.py
@@ -1,8 +1,6 @@
 """https://adventofcode.com/2022/day/9"""
 import os
 
-from grid import XY
-
 with open(os.path.join(os.path.dirname(__file__), f"inputs/day09_input.txt")) as f:
     actual_input = f.read()
 
@@ -16,28 +14,38 @@ D 1
 L 5
 R 2"""
 
-
-def tail_shift(head: XY, tail: XY) -> XY:
-    x_delta, y_delta = head.x - tail.x, head.y - tail.y
-    abs_x_delta, abs_y_delta = abs(x_delta), abs(y_delta)
-    if abs_x_delta + abs_y_delta > 2:
-        return XY(x_delta // abs_x_delta, y_delta // abs_y_delta)  # Move diagonally
-    if abs_x_delta == 2:
-        return XY(x_delta // abs_x_delta, 0)  # Move horizontally
-    if abs_y_delta == 2:
-        return XY(0, y_delta // abs_y_delta)  # Move vertically
-    return XY(0, 0)  # Touching - don't move
+TAIL_MOVE = {
+    (-2, -2): (-1, -1),
+    (-2, -1): (-1, -1),
+    (-2, 0): (-1, 0),
+    (-2, 1): (-1, 1),
+    (-2, 2): (-1, 1),
+    (-1, -2): (-1, -1),
+    (-1, 2): (-1, 1),
+    (0, -2): (0, -1),
+    (0, 2): (0, 1),
+    (1, -2): (1, -1),
+    (1, 2): (1, 1),
+    (2, -2): (1, -1),
+    (2, -1): (1, -1),
+    (2, 0): (1, 0),
+    (2, 1): (1, 1),
+    (2, 2): (1, 1),
+}
 
 
 def move_rope(number_of_knots: int, moves: list[tuple[str, int]]) -> int:
-    knots = [XY(0, 0) for _ in range(number_of_knots)]
-    tail_positions = {knots[-1]}
+    knots = [[0, 0] for _ in range(number_of_knots)]
+    head, tail = knots[0], knots[-1]
+    tail_positions = {tuple(tail)}
     for direction, steps in moves:
         for _ in range(steps):
-            knots[0] += XY.direction(direction)
-            for i in range(1, number_of_knots):
-                knots[i] += tail_shift(knots[i - 1], knots[i])
-            tail_positions.add(knots[-1])
+            head[0] += {"L": -1, "R": 1}.get(direction, 0)
+            head[1] += {"U": -1, "D": 1}.get(direction, 0)
+            for h, t in zip(knots[:-1], knots[1:]):
+                dx, dy = TAIL_MOVE.get((h[0] - t[0], h[1] - t[1]), (0, 0))
+                t[0], t[1] = t[0] + dx, t[1] + dy
+            tail_positions.add(tuple(tail))
     return len(tail_positions)
 
 
