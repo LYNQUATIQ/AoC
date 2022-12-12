c234c4a43ae57783589a5305c6287f429e10c9c5
Gid <gsmith.jemba2000@gmail.com>
2022-12-12 11:53:16 +0000

    Use NamedTuple

--- 2022/day12.py
+++ 2022/day12.py
@@ -1,8 +1,10 @@
 """https://adventofcode.com/2022/day/12"""
+from __future__ import annotations
 import os
 
 from heapq import heappop, heappush
-from utils import print_time_taken
+from typing import NamedTuple
+
 
 with open(os.path.join(os.path.dirname(__file__), f"inputs/day12_input.txt")) as f:
     actual_input = f.read()
@@ -14,20 +16,30 @@ accszExk
 acctuvwj
 abdefghi"""
 
-XY = tuple[int, int]
+
+class XY(NamedTuple):
+    x: int
+    y: int
+
+    @property
+    def neighbours(self) -> tuple[XY, XY, XY, XY]:
+        return (
+            XY(self.x + 1, self.y),
+            XY(self.x, self.y + 1),
+            XY(self.x - 1, self.y),
+            XY(self.x, self.y - 1),
+        )
+
+    def distance(self, other: XY) -> int:
+        return abs(self.x - other.x) + abs(self.x - other.x)
 
 
-def manhattan_distance(a: XY, b: XY):
-    return abs(a[0] - b[0]) + abs(a[1] - b[1])
-
-
-@print_time_taken
 def solve(inputs: str) -> None:
-    start, target = (0, 0), (0, 0)
+    start, target = XY(0, 0), XY(0, 0)
     grid: dict[XY, int] = {}
     for y, line in enumerate(inputs.splitlines()):
         for x, c in enumerate(line):
-            xy, elevation = (x, y), ord(c) - ord("a")
+            xy, elevation = XY(x, y), ord(c) - ord("a")
             if c == "S":
                 start, elevation = xy, 0
             elif c == "E":
@@ -36,11 +48,7 @@ def solve(inputs: str) -> None:
 
     possible_steps = {}
     for xy in grid:
-        steps = []
-        for dx, dy in ((1, 0), (0, 1), (-1, 0), (0, -1)):
-            step = (xy[0] + dx, xy[1] + dy)
-            if grid.get(step, 999) - grid[xy] <= 1:
-                steps.append(step)
+        steps = [n for n in xy.neighbours if grid.get(n, 999) - grid[xy] <= 1]
         possible_steps[xy] = tuple(steps)
 
     def shortest_path(starts: list[XY]) -> int:
@@ -48,36 +56,35 @@ def solve(inputs: str) -> None:
         distance_to = {start: 0 for start in starts}
         to_visit: list[tuple[float, XY]] = []
         for start in starts:
-            heappush(to_visit, (manhattan_distance(target, start), start))
+            heappush(to_visit, (start.distance(target), start))
         while to_visit:
             _, this_node = heappop(to_visit)
             if this_node == target:
                 return distance_to[this_node]
             visited.add(this_node)
-            for next_node in possible_steps[this_node]:
+            for next_step in possible_steps[this_node]:
                 distance_to_here = distance_to[this_node] + 1
-                prior_distance_to_here = distance_to.get(next_node, 0)
-                if next_node in visited and distance_to_here >= prior_distance_to_here:
+                prior_distance_to_here = distance_to.get(next_step, 0)
+                if next_step in visited and distance_to_here >= prior_distance_to_here:
                     continue
-                if distance_to_here < prior_distance_to_here or next_node not in [
+                if distance_to_here < prior_distance_to_here or next_step not in [
                     i[1] for i in to_visit
                 ]:
-                    distance_to[next_node] = distance_to_here
-                    h_score = manhattan_distance(target, next_node) - grid[next_node]
-                    f_score = distance_to_here + h_score
-                    heappush(to_visit, (f_score, next_node))
+                    distance_to[next_step] = distance_to_here
+                    f_score = distance_to_here + next_step.distance(target)
+                    heappush(to_visit, (f_score, next_step))
         raise ValueError("No path found")
 
-    print(f"Part 1: {shortest_path([start])}")
-
-    first_steps = list(
+    possible_starts = list(
         {
             s
             for s, e in grid.items()
             if e == 1 and any(grid[n] == 0 for n in possible_steps[s])
         }
     )
-    print(f"Part 2: {shortest_path(first_steps)+1}\n")
+
+    print(f"Part 1: {shortest_path([start])}")
+    print(f"Part 2: {shortest_path(possible_starts)+1}\n")
 
 
 solve(sample_input)
