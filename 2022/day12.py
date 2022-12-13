2eb2fb428694b62f7576dddfdb5f9be5e0545e96
Gid <gsmith.jemba2000@gmail.com>
2022-12-13 13:31:55 +0000

    Use complex numbers

--- 2022/day12.py
+++ 2022/day12.py
@@ -3,7 +3,6 @@ from __future__ import annotations
 import os
 
 from collections import deque
-from typing import NamedTuple
 
 from utils import print_time_taken
 
@@ -18,49 +17,34 @@ acctuvwj
 abdefghi"""
 
 
-class XY(NamedTuple):
-    x: int
-    y: int
-
-    @property
-    def neighbours(self) -> tuple[XY, XY, XY, XY]:
-        return (
-            XY(self.x + 1, self.y),
-            XY(self.x, self.y + 1),
-            XY(self.x - 1, self.y),
-            XY(self.x, self.y - 1),
-        )
-
-
 @print_time_taken
 def solve(inputs: str) -> None:
-    start, target = XY(0, 0), XY(0, 0)
-    grid: dict[XY, int] = {}
+    start, target = complex(0, 0), complex(0, 0)
+    grid: dict[complex, int] = {}
     for y, line in enumerate(inputs.splitlines()):
         for x, c in enumerate(line):
-            xy, elevation = XY(x, y), ord(c) - ord("a")
+            xy, elevation = complex(x, y), ord(c) - ord("a")
             if c == "S":
                 start, elevation = xy, 0
             elif c == "E":
                 target, elevation = xy, 26
             grid[xy] = elevation
 
-    possible_steps = {
-        xy: [n for n in xy.neighbours if grid.get(n, 999) - grid[xy] <= 1]
-        for xy in grid
-    }
-
-    def shortest_path(starts: list[XY]) -> int:
+    def shortest_path(starts: list[complex]) -> int:
         queue = deque(starts)
-        visited = {s: 0 for s in starts}
+        distance_to = {s: 0 for s in starts}
         while queue:
             node = queue.popleft()
             if node == target:
-                return visited[node]
-            for next_node in possible_steps[node]:
-                if next_node not in visited:
+                return distance_to[node]
+            for next_node in (
+                node + d
+                for d in (1, -1, 1j, -1j)
+                if grid.get(node + d, 999) - grid[node] <= 1
+            ):
+                if next_node not in distance_to:
                     queue.append(next_node)
-                    visited[next_node] = visited[node] + 1
+                    distance_to[next_node] = distance_to[node] + 1
         raise ValueError("No path found")
 
     print(f"Part 1: {shortest_path([start])}")
@@ -69,7 +53,7 @@ def solve(inputs: str) -> None:
         {
             s
             for s, e in grid.items()
-            if e == 1 and any(grid[n] == 0 for n in possible_steps[s])
+            if e == 1 and any(grid.get(s + d, 999) == 0 for d in (1, -1, 1j, -1j))
         }
     )
     print(f"Part 2: {shortest_path(possible_starts)+1}\n")
