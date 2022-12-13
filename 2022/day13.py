5d54dbff4aed6c60e28282f78b07d39968f43de8
Gid <gsmith.jemba2000@gmail.com>
2022-12-13 17:40:58 +0000

    Subclass str

--- 2022/day13.py
+++ 2022/day13.py
@@ -5,9 +5,6 @@ import json
 import math
 import os
 
-from dataclasses import dataclass
-
-
 with open(os.path.join(os.path.dirname(__file__), f"inputs/day13_input.txt")) as f:
     actual_input = f.read()
 
@@ -37,40 +34,34 @@ sample_input = """[1,1,3,1,1]
 [1,[2,[3,[4,[5,6,0]]]],8,9]"""
 
 
-@dataclass
-class PacketData:
-
-    _data: str
-
-    def __repr__(self) -> str:
-        return str(self._data)
-
+class PacketData(str):
     def __lt__(self, other: object) -> bool:
         def _split_packet(packet: PacketData) -> tuple[PacketData, PacketData]:
-            items = json.loads(packet._data)
+            items = json.loads(packet)
             return (
                 PacketData(str(items[0])),
                 PacketData(str(list(i for i in items[1:]))),
             )
 
         assert isinstance(other, PacketData)
-        if self._data == "[]" or other._data == "[]":
-            return self._data == "[]"
+        if self == "[]" or other == "[]":
+            return self == "[]"
         l_item, l_remaining = _split_packet(self)
         r_item, r_remaining = _split_packet(other)
-        if l_item._data == r_item._data:
+        if l_item == r_item:
             return l_remaining < r_remaining
-        if l_item._data.isdigit() and r_item._data.isdigit():
-            return int(l_item._data) < int(r_item._data)
-        if l_item._data.isdigit():
+        if l_item.isdigit() and r_item.isdigit():
+            return int(l_item) < int(r_item)
+        if l_item.isdigit():
             return PacketData(f"[[{l_item}],{l_remaining}]") < other
-        if r_item._data.isdigit():
+        if r_item.isdigit():
             return self < PacketData(f"[[{r_item}],{r_remaining}]")
         return l_item < r_item
 
 
 def solve(inputs: str) -> None:
     pairs = inputs.split("\n\n")
+    packets = ("\n".join(pairs)).splitlines()
 
     correct_indices: list[int] = []
     for i, (left, right) in enumerate(map(str.splitlines, pairs), start=1):
@@ -78,9 +69,8 @@ def solve(inputs: str) -> None:
             correct_indices.append(i)
     print(f"Part 1: {sum(correct_indices)}")
 
-    DIVIDERS = ["[2]", "[6]"]
-    packets = ("\n".join(pairs)).splitlines() + DIVIDERS
-    sorted_packets = [str(p) for p in sorted(list(map(PacketData, packets)))]
+    DIVIDERS = [PacketData("[2]"), PacketData("[6]")]
+    sorted_packets = sorted(list(map(PacketData, packets)) + DIVIDERS)
     divider_indices = (sorted_packets.index(d) + 1 for d in DIVIDERS)
     print(f"Part 2: {math.prod(divider_indices)}\n")
 
