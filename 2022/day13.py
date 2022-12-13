a00e9f9669be21a276680d1179dd62c9fb690134
Gid <gsmith.jemba2000@gmail.com>
2022-12-13 16:52:18 +0000

    Streamline PacketData class

--- 2022/day13.py
+++ 2022/day13.py
@@ -10,8 +10,6 @@ from functools import total_ordering
 with open(os.path.join(os.path.dirname(__file__), f"inputs/day13_input.txt")) as f:
     actual_input = f.read()
 
-from utils import print_time_taken
-
 
 sample_input = """[1,1,3,1,1]
 [1,1,5,1,1]
@@ -40,9 +38,6 @@ sample_input = """[1,1,3,1,1]
 
 @total_ordering
 class PacketData:
-    class PacketsEqual(Exception):
-        """Raised when packets are equal (same value or empty lists)"""
-
     def __init__(self, data: str) -> None:
         self._data = data
 
@@ -51,30 +46,30 @@ class PacketData:
 
     def __eq__(self, other: object) -> bool:
         assert isinstance(other, PacketData)
-        if self.is_integer and other.is_integer:
-            return self._data == other._data
-        elif self.is_list and other.is_list:
-            return self.empty and other.empty
-        return False
+        return self._data == other._data
 
     def __lt__(self, other: object) -> bool:
         assert isinstance(other, PacketData)
-        try:
-            return PacketData._compare(self, other)
-        except PacketData.PacketsEqual:
-            return False
+        if self.is_empty or other.is_empty:
+            return self.is_empty
+        l_item, l_remaining = PacketData._split_packet(self)
+        r_item, r_remaining = PacketData._split_packet(other)
+        if l_item == r_item:
+            return l_remaining < r_remaining
+        if l_item.is_integer and r_item.is_integer:
+            return int(l_item._data) < int(r_item._data)
+        if l_item.is_integer:
+            return PacketData(f"[[{l_item}],{l_remaining}]") < other
+        if r_item.is_integer:
+            return self < PacketData(f"[[{r_item}],{r_remaining}]")
+        return l_item < r_item
 
     @property
     def is_integer(self) -> bool:
         return self._data.isdigit()
 
     @property
-    def is_list(self) -> bool:
-        return not self.is_integer
-
-    @property
-    def empty(self) -> bool:
-        assert not self.is_integer
+    def is_empty(self) -> bool:
         return self._data == "[]"
 
     @staticmethod
@@ -85,61 +80,20 @@ class PacketData:
             PacketData(str(list(i for i in items[1:]))),
         )
 
-    @staticmethod
-    def _combine_packet(item: PacketData, other: PacketData) -> PacketData:
-        item_str = str(f"[{item}]") if item.is_integer else str(item)
-        return PacketData(f"[{item_str},{other._data}]")
 
-    @staticmethod
-    def _compare(left_packet: PacketData, right_packet: PacketData) -> bool:
-        if left_packet == right_packet:
-            raise PacketData.PacketsEqual
-
-        if left_packet.empty or right_packet.empty:
-            return left_packet.empty
-
-        l_item, l_remaining = PacketData._split_packet(left_packet)
-        r_item, r_remaining = PacketData._split_packet(right_packet)
-
-        if l_item == r_item:
-            return PacketData._compare(l_remaining, r_remaining)
-        if l_item.is_integer and r_item.is_integer:
-            return int(l_item._data) < int(r_item._data)
-        if l_item.is_integer or r_item.is_integer:
-            return PacketData._compare(
-                PacketData._combine_packet(l_item, l_remaining),
-                PacketData._combine_packet(r_item, r_remaining),
-            )
-        # Both items are lists
-        if l_item.empty and not r_item.empty:
-            return True
-        if r_item.empty and not l_item.empty:
-            return False
-        try:
-            return PacketData._compare(l_item, r_item)
-        except PacketData.PacketsEqual:
-            return PacketData._compare(l_remaining, r_remaining)
-
-
-DIVIDERS = ("[2]", "[6]")
-
-
-@print_time_taken
 def solve(inputs: str) -> None:
     pairs = inputs.split("\n\n")
 
-    correct_indices: list[int] = [
-        i
-        for i, (left, right) in enumerate(map(str.splitlines, pairs), start=1)
-        if PacketData(left) < PacketData(right)
-    ]
+    correct_indices: list[int] = []
+    for i, (left, right) in enumerate(map(str.splitlines, pairs), start=1):
+        if PacketData(left) < PacketData(right):
+            correct_indices.append(i)
     print(f"Part 1: {sum(correct_indices)}")
 
-    packet_data = "\n".join(pairs) + "\n" + "\n".join(DIVIDERS)
-    packets = list(map(PacketData, packet_data.splitlines()))
-    packet_strings = [str(p) for p in sorted(packets)]
-    divider_indices = (packet_strings.index(d) + 1 for d in DIVIDERS)
-
+    DIVIDERS = ("[2]", "[6]")
+    packets = ("\n".join(pairs)).splitlines() + list(DIVIDERS)
+    sorted_packets = [str(p) for p in sorted(list(map(PacketData, packets)))]
+    divider_indices = (sorted_packets.index(d) + 1 for d in DIVIDERS)
     print(f"Part 2: {math.prod(divider_indices)}\n")
 
 
