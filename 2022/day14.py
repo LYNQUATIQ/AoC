76b84aad580b9f670a32fef93c182f4beb60167e
Gid <gsmith.jemba2000@gmail.com>
2022-12-14 07:41:09 +0000

    Use set instead of dict

--- 2022/day14.py
+++ 2022/day14.py
@@ -1,6 +1,5 @@
 """https://adventofcode.com/2022/day/14"""
 import os
-from grid import Grid, XY
 
 with open(os.path.join(os.path.dirname(__file__), f"inputs/day14_input.txt")) as f:
     actual_input = f.read()
@@ -10,59 +9,52 @@ sample_input = """498,4 -> 498,6 -> 496,6
 503,4 -> 502,4 -> 502,9 -> 494,9"""
 
 
-def build_grid(inputs: str) -> Grid:
-    rocks = {}
-    for line in inputs.splitlines():
-        points: list[XY] = []
-        for p in line.split(" -> "):
-            x, y = map(int, p.split(","))
-            points.append(XY(x, y))
-        for p_0, p_1 in zip(points[:-1], points[1:]):
-            if p_1.x == p_0.x:
-                s = 1 if p_1.y > p_0.y else -1
-                for y in range(p_0.y, p_1.y + s, s):
-                    rocks[XY(p_1.x, y)] = "#"
-            else:
-                s = 1 if p_1.x > p_0.x else -1
-                for x in range(p_0.x, p_1.x + s, s):
-                    rocks[XY(x, p_1.y)] = "#"
-            p_0 = p_1
-    grid = Grid()
-    grid._grid = rocks
-    return grid
-
-
 def solve(inputs: str) -> None:
 
-    grid = build_grid(inputs)
+    blockages = set()
+    for line in inputs.splitlines():
+        points: list[tuple[int, int]] = []
+        for p in line.split(" -> "):
+            x, y = map(int, p.split(","))
+            points.append((x, y))
+        for p_0, p_1 in zip(points[:-1], points[1:]):
+            if p_1[0] == p_0[0]:
+                s = 1 if p_1[1] > p_0[1] else -1
+                for y in range(p_0[1], p_1[1] + s, s):
+                    blockages.add((p_1[0], y))
+            else:
+                s = 1 if p_1[0] > p_0[0] else -1
+                for x in range(p_0[0], p_1[0] + s, s):
+                    blockages.add((x, p_1[1]))
+            p_0 = p_1
 
     sand_units = 0
-    sand_source = XY(500, 0)
-    part1_abyss = grid.limits[3] + 1
-    floor = part1_abyss + 1
+    sand_source = (500, 0)
+    max_y = max(xy[1] for xy in blockages)
+    part1_abyss = max_y + 1
+    floor = max_y + 2
 
     x, y = sand_source
     part_1_sand_units = 1
     while True:
 
-        if y > part1_abyss:
+        if y > max_y + 1:
             part_1_sand_units = y
         if y < floor:
             y += 1
-            if XY(x, y) not in grid._grid:
+            if (x, y) not in blockages:
                 continue
-            if XY(x - 1, y) not in grid._grid:
+            if (x - 1, y) not in blockages:
                 x -= 1
                 continue
-            if XY(x + 1, y) not in grid._grid:
+            if (x + 1, y) not in blockages:
                 x += 1
                 continue
-        grid[XY(x, y - 1)] = "o"
+        blockages.add((x, y - 1))
         sand_units += 1
-        if XY(x, y - 1) == sand_source:
+        if (x, y - 1) == sand_source:
             break
         x, y = sand_source
-        # grid.print_grid(show_headers=False)
 
     print(f"Part 1: {part_1_sand_units}")
     print(f"Part 2: {sand_units}\n")
