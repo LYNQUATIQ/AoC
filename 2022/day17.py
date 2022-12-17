9be3cdb7f607ee6fa3acbbc6523de4175574e44f
Gid <gsmith.jemba2000@gmail.com>
2022-12-17 19:36:35 +0000

    Finished Day 17

--- 2022/day17.py
+++ 2022/day17.py
@@ -1,8 +1,6 @@
 """https://adventofcode.com/2022/day/17"""
 import os
 
-from itertools import cycle
-
 with open(os.path.join(os.path.dirname(__file__), f"inputs/day17_input.txt")) as f:
     actual_input = f.read()
 
@@ -20,9 +18,9 @@ SHAPES = (
 
 def play_tetris(puffs: str, number_of_rocks: int) -> int:
     puff_cycle, puff_index = len(puffs), 0
-    shape_cycle = cycle(range(5))
+    shape_cycle, shape_index = len(SHAPES), 0
 
-    # Chamber always has seven blank rows for shape to start in
+    # Chamber has seven blank rows for shape to start in - row 3 plus 4 for shape itself
     chamber = bytearray([0] * 7 + [0b11111111])
 
     prior_states: dict[tuple[tuple[int, ...], int, int], int] = {}
@@ -30,23 +28,22 @@ def play_tetris(puffs: str, number_of_rocks: int) -> int:
 
     rocks, current_height = 0, 0
     while True:
-
-        shape_index = next(shape_cycle)
-        shape = list(SHAPES[shape_index])
         rocks += 1
-
+        shape = list(SHAPES[shape_index])
+        shape_index = (shape_index + 1) % shape_cycle
+        
         i = 4 - len(shape)
         while True:
+            
             # Try moving left/right making sure we don't move into an edge
             puff = puffs[puff_index]
             puff_index = (puff_index + 1) % puff_cycle
-            if puff == ">":
-                new_shape, edge = [x // 2 for x in shape], 1
-            else:
-                new_shape, edge = [x * 2 for x in shape], 256
-            if not any((s & edge) or (s & c) for s, c in zip(new_shape, chamber[i:])):
-                shape = new_shape
-
+            match puff:
+                case '>': shifted, edge = [x // 2 for x in shape], 1
+                case '<': shifted, edge = [x * 2 for x in shape], 256
+            if not any((s & edge) or (s & c) for s, c in zip(shifted, chamber[i:])):
+                shape = shifted
+                
             # Try moving down - break out if we hit any rocks in the cavern
             if any((s & c) for s, c in zip(shape, chamber[i + 1 :])):
                 break
@@ -57,24 +54,21 @@ def play_tetris(puffs: str, number_of_rocks: int) -> int:
             chamber[chamber_index] |= shape_row
 
         current_height += 7 - min(i, 7)
-        new_chamber = bytearray([0] * 7) + chamber[min(i, 7) :]
-        chamber = new_chamber
-
+        chamber = bytearray([0] * 7) + chamber[min(i, 7) :]
         state = (tuple(chamber[:20]), shape_index, puff_index)
         if state in prior_states:
             prior_rocks = prior_states[state]
             prior_height = prior_heights[prior_rocks]
-            height_change = current_height - prior_height
             cycle_length = rocks - prior_rocks
             remaining_rocks = number_of_rocks - prior_rocks
             number_of_cycles = remaining_rocks // cycle_length
             remaining_rocks = remaining_rocks % cycle_length
             total_height = prior_height
-            total_height += number_of_cycles * height_change
+            total_height += number_of_cycles * (current_height - prior_height)
             total_height += prior_heights[remaining_rocks + prior_rocks] - prior_height
             return total_height
-        else:
-            prior_states[state] = rocks
+
+        prior_states[state] = rocks
         prior_heights[rocks] = current_height
 
 
