14fa14d59e505a43d0d8a258ece0f80c3e2c94ff
Gid <gsmith.jemba2000@gmail.com>
2022-12-18 09:12:01 +0000

    Tidied code

--- 2022/day18.py
+++ 2022/day18.py
@@ -25,23 +25,21 @@ sample_input = """2,2,2
 2,1,5
 2,3,5"""
 
-Cube = tuple[int, int, int]
 
-
-def neighbours(cube: Cube, extent=999) -> set[Cube]:
-    x, y, z = cube
+def neighbours(xyz: tuple[int, int, int], span: int = 999) -> set[tuple[int, int, int]]:
+    x, y, z = xyz
     neighbours = set()
     if x >= 0:
         neighbours.add((x - 1, y, z))
-    if x < extent:
+    if x < span:
         neighbours.add((x + 1, y, z))
     if y >= 0:
         neighbours.add((x, y - 1, z))
-    if y < extent:
+    if y < span:
         neighbours.add((x, y + 1, z))
     if z >= 0:
         neighbours.add((x, y, z - 1))
-    if z < extent:
+    if z < span:
         neighbours.add((x, y, z + 1))
     return neighbours
 
@@ -59,26 +57,21 @@ def solve(inputs: str) -> None:
             sides += neighbour not in cubes
     print(f"Part 1: {sides}")
 
-    extent = max(*(max(cube) for cube in cubes)) + 1
-
-    open_air: set[Cube] = {(0, 0, 0)}
-    queue: deque[Cube] = deque([(0, 0, 0)])
+    max_span = max(*(max(cube) for cube in cubes)) + 1
+    open_air: set[tuple[int, int, int]] = {(0, 0, 0)}
+    queue: deque[tuple[int, int, int]] = deque([(0, 0, 0)])
     while queue:
         xyz = queue.popleft()
-        for next_xyz in neighbours(xyz, extent):
-            if next_xyz not in cubes and next_xyz not in open_air:
-                queue.append(next_xyz)
-                open_air.add(next_xyz)
+        for next_xyz in neighbours(xyz, max_span):
+            if next_xyz in cubes or next_xyz in open_air:
+                continue
+            open_air.add(next_xyz)
+            queue.append(next_xyz)
 
-    gaps = set()
-    for xyz in product(range(extent), range(extent), range(extent)):
+    for xyz in product(range(max_span), range(max_span), range(max_span)):
         if not (xyz in cubes or xyz in open_air):
-            gaps.add(xyz)
-
-    for gap in gaps:
-        for neighbour in neighbours(gap):
-            sides -= neighbour in cubes
-
+            for neighbour in neighbours(xyz):
+                sides -= neighbour in cubes
     print(f"Part 2: {sides}\n")
 
 
