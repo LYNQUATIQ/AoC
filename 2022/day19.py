63ec4ac062aadf110263de159a5f4d52a1c28175
Gid <gsmith.jemba2000@gmail.com>
2022-12-21 19:28:02 +0000

    *Very* fast part 1

--- 2022/day19.py
+++ 2022/day19.py
@@ -3,6 +3,8 @@ import math
 import os
 import re
 
+from contextlib import redirect_stdout
+
 from utils import print_time_taken
 
 with open(os.path.join(os.path.dirname(__file__), f"inputs/day19_input.txt")) as f:
@@ -31,6 +33,43 @@ State = tuple[int, tuple[int, ...], tuple[int, ...], int]
 
 from collections import deque
 
+SAMPLE_ANSWERS = {
+    1: 9,
+    2: 12,
+}
+ACTUAL_ANSWERS = {
+    1: 0,
+    2: 0,
+    3: 1,
+    4: 5,
+    5: 3,
+    6: 2,
+    7: 0,
+    8: 1,
+    9: 14,
+    10: 0,
+    11: 1,
+    12: 0,
+    13: 5,
+    14: 6,
+    15: 0,
+    16: 0,
+    17: 1,
+    18: 0,
+    19: 15,
+    20: 6,
+    21: 0,
+    22: 5,
+    23: 0,
+    24: 0,
+    25: 6,
+    26: 4,
+    27: 1,
+    28: 0,
+    29: 8,
+    30: 0,
+}
+
 
 def print_path(blueprint, g_scores, prior_states) -> None:
     ore_costs, clay_cost, obsidian_cost = blueprint
@@ -46,8 +85,9 @@ def print_path(blueprint, g_scores, prior_states) -> None:
             break
         path.append(s)
     elem = {ORE: "ore", CLAY: "clay", OBSIDIAN: "obsidian", GEODE: "geode"}
-    for time, resources, bots, bot_to_build in sorted(path):  # type: ignore
-        print(f"\n== Minute {time} ==")
+    for state in sorted(path):  # type: ignore
+        time, resources, bots, bot_to_build = state
+        print(f"\n== Minute {time} ==", state)
         res = list(resources)
         if bot_to_build != DO_NOTHING:
             res[ORE] -= ore_costs[bot_to_build]
@@ -75,7 +115,9 @@ def print_path(blueprint, g_scores, prior_states) -> None:
 
 
 @print_time_taken
-def find_max_geodes(blueprint: BluePrint, max_minutes: int, debug: bool = False) -> int:
+def find_max_geodes(
+    blueprint: BluePrint, max_minutes: int, bp_id: int, sample: bool
+) -> int:
     ore_costs, clay_cost, obsidian_cost = blueprint
     initial_bots = (1, 0, 0, 0)
     initial_state = (1, (0, 0, 0, 0), initial_bots, DO_NOTHING)
@@ -84,6 +126,9 @@ def find_max_geodes(blueprint: BluePrint, max_minutes: int, debug: bool = False)
     g_scores: dict[State, int] = {initial_state: 0}
 
     queue = deque([initial_state])
+
+    # if bp_id != 3:
+    #     return ACTUAL_ANSWERS[bp_id]
     while queue:
         state = queue.popleft()
         time, resources, bots, bot_to_build = state
@@ -104,27 +149,9 @@ def find_max_geodes(blueprint: BluePrint, max_minutes: int, debug: bool = False)
             if bot_to_build == GEODE:
                 next_resources[OBSIDIAN] -= obsidian_cost
 
-        # Determine what bots we should build next
-        ore, clay, obsidian, _ = next_resources
-        need_ore = bots[ORE] < max(ore_costs)
-        need_clay = bots[CLAY] < clay_cost
-        need_obsidian = bots[OBSIDIAN] < obsidian_cost
-        can_make_ore = ore >= ore_costs[ORE]
-        can_make_clay = ore >= ore_costs[CLAY]
-        can_make_obsidian = ore >= ore_costs[OBSIDIAN] and clay >= clay_cost
-        can_make_geode = ore >= ore_costs[GEODE] and obsidian >= obsidian_cost
-
-        if can_make_geode:
-            bot_options = {GEODE}
-        else:
-            if need_obsidian and can_make_obsidian:
-                bot_options = {OBSIDIAN, DO_NOTHING}
-            else:
-                bot_options = {DO_NOTHING}
-                if need_clay and can_make_clay:
-                    bot_options.add(CLAY)
-                if need_ore and can_make_ore:
-                    bot_options.add(ORE)
+        # if state == ((12, (2, 12, 0, 0), (1, 3, 0, 0), DO_NOTHING)):
+        #     breakpoint()
+        bot_options = get_bot_options(blueprint, bots, next_resources, next_bots)
 
         for bot_to_build in bot_options:
             next_state = (
@@ -140,14 +167,68 @@ def find_max_geodes(blueprint: BluePrint, max_minutes: int, debug: bool = False)
             queue.append(next_state)  # type: ignore
             prior_states[next_state] = state
 
-    if debug:
-        print_path(blueprint, g_scores, prior_states)
+    correct_answer = SAMPLE_ANSWERS[bp_id] if sample else ACTUAL_ANSWERS[bp_id]
+    if max(g_scores.values()) != correct_answer:
+        s = "sample" if sample else "case"
+        with open(
+            os.path.join(os.path.dirname(__file__), f"fails/{s}_{bp_id}_FAIL.txt"), "w"
+        ) as f:
+            with redirect_stdout(f):
+                print_path(blueprint, g_scores, prior_states)
+        assert False
 
     return max(g_scores.values())
 
 
+def get_bot_options(blueprint: BluePrint, bots, next_resources, next_bots) -> set[int]:
+    ore_costs, clay_cost, obsidian_cost = blueprint
+
+    # Determine what bots we should build next
+    ore, clay, obsidian, _ = next_resources
+    need_ore = bots[ORE] < max(ore_costs)
+    need_clay = bots[CLAY] < clay_cost
+    need_obsidian = bots[OBSIDIAN] < obsidian_cost
+    can_make_ore = ore >= ore_costs[ORE]
+    can_make_clay = ore >= ore_costs[CLAY]
+    can_make_obsidian = ore >= ore_costs[OBSIDIAN] and clay >= clay_cost
+    can_make_geode = ore >= ore_costs[GEODE] and obsidian >= obsidian_cost
+
+    if can_make_geode:
+        return {GEODE}
+
+    if need_obsidian and can_make_obsidian:
+        if (
+            (obsidian + next_bots[OBSIDIAN]) >= obsidian_cost
+            and (ore + next_bots[ORE]) >= ore_costs[GEODE]
+            and not ((ore + next_bots[ORE]) >= (ore_costs[GEODE] + ore_costs[OBSIDIAN]))
+        ):
+            return {DO_NOTHING}
+        return {OBSIDIAN}
+
+    if need_clay and can_make_clay and bots[CLAY]:
+        time_to_obsidian_if_do_nothing = max(
+            math.ceil((ore_costs[OBSIDIAN] - ore) / bots[ORE]),
+            math.ceil((clay_cost - clay) / bots[CLAY]),
+        )
+        time_to_obsidian_if_extra_clay = max(
+            math.ceil((ore_costs[OBSIDIAN] - ore + ore_costs[CLAY]) / bots[ORE]),
+            math.ceil((clay_cost - clay) / (bots[CLAY] + 1)),
+        )
+        if time_to_obsidian_if_do_nothing < time_to_obsidian_if_extra_clay:
+            return {DO_NOTHING}
+
+    bot_options = set()
+    if need_clay and can_make_clay:
+        bot_options.add(CLAY)
+    if need_ore and can_make_ore:
+        bot_options.add(ORE)
+    if not bot_options:
+        bot_options.add(DO_NOTHING)
+    return bot_options
+
+
 @print_time_taken
-def solve(inputs: str) -> None:
+def solve(inputs: str, sample: bool = False) -> None:
     blueprints: dict[int, BluePrint] = {}
     for bp in inputs.splitlines():
         match = re.match(REGEX, bp)
@@ -157,18 +238,22 @@ def solve(inputs: str) -> None:
         )
         blueprints[bp_id] = ((ore1, ore2, ore3, ore4), clay_cost, obsidian_cost)
 
-    # quality_levels = []
-    # for bp_id, blueprint in blueprints.items():
-    #     max_geode = find_max_geodes(blueprint, 24)
-    #     quality_levels.append(bp_id * max_geode)
-    #     print(f"{bp_id}: {max_geode}")
-    # print(f"Part 1: {sum(quality_levels)}")
+    quality_levels = []
+    for bp_id, blueprint in blueprints.items():
+        try:
+            max_geode = find_max_geodes(blueprint, 24, bp_id, sample)
+            quality_levels.append(bp_id * max_geode)
+            print(f"{bp_id}: {max_geode}")
+        except AssertionError:
+            pass
 
-    first_3_blueprints = list(blueprints.values())[:3]
-    max_geodes = [find_max_geodes(blueprint, 32) for blueprint in first_3_blueprints]
-    print(f"Part 2: {max_geodes}\n")
-    print(f"Part 2: {math.prod(max_geodes)}\n")
+    print(f"Part 1: {sum(quality_levels)}")
+
+    # first_3_blueprints = list(blueprints.values())[:3]
+    # max_geodes = [find_max_geodes(blueprint, 32) for blueprint in first_3_blueprints]
+    # print(f"Part 2: {max_geodes}\n")
+    # print(f"Part 2: {math.prod(max_geodes)}\n")
 
 
-# solve(sample_input)
+solve(sample_input, sample=True)
 solve(actual_input)
