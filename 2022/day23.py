b9bc71e021010ab901d49caffdc212399443e4cf
Gid <gsmith.jemba2000@gmail.com>
2022-12-23 17:39:34 +0000

    Yet more tidying

--- 2022/day23.py
+++ 2022/day23.py
@@ -51,18 +51,17 @@ def solve(inputs: str) -> None:
                     break
 
         elves_moved = False
-        for new_elf, (old_elf, *other_elves) in proposals.items():
+        for new_position, (old_position, *other_elves) in proposals.items():
             if not other_elves:
-                elves.remove(old_elf)
-                elves.add(new_elf)
+                elves.remove(old_position)
+                elves.add(new_position)
                 elves_moved = True
 
         if round == 10:
-            x0, x1, y0, y1 = float("inf"), float("-inf"), float("inf"), float("-inf")
-            for elf in elves:
-                x, y = int(elf.real), int(elf.imag)
-                x0, x1, y0, y1 = min(x0, x), max(x1, x), min(y0, y), max(y1, y)
-            print(f"Part 1: {(x1 - x0 + 1) * (y1 - y0 + 1) - len(elves)}")
+            x_pos, y_pos = [int(e.real) for e in elves], [int(e.imag) for e in elves]
+            y_pos = [int(elf.imag) for elf in elves]
+            width, height = max(x_pos) - min(x_pos) + 1, max(y_pos) - min(y_pos) + 1
+            print(f"Part 1: {width * height - len(elves)}")
 
         if not elves_moved:
             break
