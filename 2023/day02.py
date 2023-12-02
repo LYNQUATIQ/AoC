65c7e0a5fcc1be0ba32fea986d1b7c753b2608d5
Gid <gsmith.jemba2000@gmail.com>
2023-12-02 10:14:01 +0000

    Day 2

--- 2023/day02.py
+++ 2023/day02.py
@@ -1,19 +1,39 @@
 """https://adventofcode.com/2023/day/2"""
 import os
+import math
+import re
 
 with open(os.path.join(os.path.dirname(__file__), "inputs/day02_input.txt")) as f:
     actual_input = f.read()
 
 
-sample_input = """xxx"""
+sample_input = """Game 1: 3 blue, 4 red; 1 red, 2 green, 6 blue; 2 green
+Game 2: 1 blue, 2 green; 3 green, 4 blue, 1 red; 1 green, 1 blue
+Game 3: 8 green, 6 blue, 20 red; 5 blue, 4 red, 13 green; 5 green, 1 red
+Game 4: 1 green, 3 red, 6 blue; 3 green, 6 red; 3 green, 15 blue, 14 red
+Game 5: 6 red, 1 blue, 3 green; 2 blue, 1 red, 2 green"""
+
+RED, GREEN, BLUE = "red", "green", "blue"
+MAX_COUNT = {RED: 12, GREEN: 13, BLUE: 14}
 
 
 def solve(inputs):
-    values = tuple(map(int, inputs.splitlines()))
+    total = 0
+    total_power = 0
+    for game in inputs.splitlines():
+        game_id = int(re.search(r"\d+", game).group())
+        max_count = {RED: 0, GREEN: 0, BLUE: 0}
+        for cube_set in game.split(": ")[1].split("; "):
+            for cubes in cube_set.split(", "):
+                count, color = cubes.split(" ")
+                max_count[color] = max(int(count), max_count[color])
+        if all(max_count[c] <= MAX_COUNT[c] for c in MAX_COUNT):
+            total += game_id
+        total_power += math.prod(max_count.values())
 
-    print(f"Part 1: {False}")
-    print(f"Part 2: {False}")
+    print(f"Part 1: {total}")
+    print(f"Part 2: {total_power}\n")
 
 
 solve(sample_input)
-# solve(actual_input)
+solve(actual_input)
