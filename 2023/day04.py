01a334aedf6bb41d1eb7f83b4129fc74cee283a1
Gid <gsmith.jemba2000@gmail.com>
2023-12-04 06:49:07 +0000

    Simplified Day 4

--- 2023/day04.py
+++ 2023/day04.py
@@ -1,7 +1,6 @@
 """https://adventofcode.com/2023/day/4"""
 import os
 import re
-from collections import deque
 
 with open(os.path.join(os.path.dirname(__file__), "inputs/day04_input.txt")) as f:
     actual_input = f.read()
@@ -16,33 +15,24 @@ Card 6: 31 18 13 56 72 | 74 77 10 23 35 67 36 11"""
 
 
 def solve(inputs):
-    scratchcards = {}
+    matches = []
     for card in inputs.splitlines():
-        header, values = card.split(":")
-        card_id = int(re.search(r"\d+", header).group())
+        _, values = card.split(":")
         left, right = values.split("|")
         winning_values = set(map(int, re.findall(r"\d+", left)))
         your_values = set(map(int, re.findall(r"\d+", right)))
-        scratchcards[card_id] = len(winning_values & your_values)
+        matches.append(len(winning_values & your_values))
 
-    total = 0
-    for matches in scratchcards.values():
-        if matches:
-            total += 2 ** (matches - 1)
-    print(f"Part 1: {total}")
+    print(f"Part 1: {sum(map(lambda x : 2 ** (x - 1) if x else 0, matches))}")
 
-    total_scratchcards = {card_id: 0 for card_id in scratchcards}
-    to_play = deque(scratchcards.keys())
-    while to_play:
-        card_id = to_play.popleft()
-        try:
-            total_scratchcards[card_id] += 1
-        except KeyError:
-            pass
-        for delta in range(1, scratchcards[card_id] + 1):
-            to_play.append(card_id + delta)
-
-    print(f"Part 2: {sum(total_scratchcards.values())}\n")
+    # Play every original scratchcard once then lookback and add in any copies
+    scratchcards = {card_id: 1 for card_id, _ in enumerate(matches)}
+    max_lookback = len(winning_values)
+    for card_id in range(len(matches)):
+        for prior_id in range(max(card_id - max_lookback, 0), card_id):
+            if prior_id + matches[prior_id] >= card_id:
+                scratchcards[card_id] += scratchcards[prior_id]
+    print(f"Part 2: {sum(scratchcards.values())}\n")
 
 
 solve(sample_input)
