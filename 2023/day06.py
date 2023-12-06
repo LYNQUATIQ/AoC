1393917cc824530d87a6de4b5bd2dbb7b5886bb1
Gid <gsmith.jemba2000@gmail.com>
2023-12-06 08:32:02 +0000

    Day 6

--- 2023/day06.py
+++ 2023/day06.py
@@ -1,19 +1,35 @@
 """https://adventofcode.com/2023/day/6"""
 import os
+import re
+import math
 
 with open(os.path.join(os.path.dirname(__file__), "inputs/day06_input.txt")) as f:
     actual_input = f.read()
 
 
-sample_input = """xxx"""
+sample_input = """Time:      7  15   30
+Distance:  9  40  200"""
+
+
+def ways_to_win(time: int, distance: int) -> int:
+    root1 = (-time + (time * time - 4 * distance) ** 0.5) / -2
+    root2 = (-time - (time * time - 4 * distance) ** 0.5) / -2
+    root2 = root2 - 1 if root2 == int(root2) else root2
+    return math.floor(root2) - math.floor(root1)
 
 
 def solve(inputs):
-    values = tuple(map(int, inputs.splitlines()))
+    lines = inputs.splitlines()
+    times = [int(n) for n in re.findall(r"\d+", lines[0])]
+    distances = [int(n) for n in re.findall(r"\d+", lines[1])]
 
-    print(f"Part 1: {False}")
-    print(f"Part 2: {False}")
+    wins = [ways_to_win(time, distance) for time, distance in zip(times, distances)]
+    print(f"Part 1: {math.prod(wins)}")
+
+    time = int("".join([str(n) for n in times]))
+    distance = int("".join([str(n) for n in distances]))
+    print(f"Part 2: {ways_to_win(time, distance)}\n")
 
 
 solve(sample_input)
-# solve(actual_input)
+solve(actual_input)
