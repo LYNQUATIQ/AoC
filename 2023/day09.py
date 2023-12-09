88c5e243bf208c534b192ff52b5a6078efe7c2e1
Gid <gsmith.jemba2000@gmail.com>
2023-12-09 06:09:13 +0000

    Tidied day 9

--- 2023/day09.py
+++ 2023/day09.py
@@ -1,6 +1,5 @@
 """https://adventofcode.com/2023/day/9"""
 import os
-import re
 
 with open(os.path.join(os.path.dirname(__file__), "inputs/day09_input.txt")) as f:
     actual_input = f.read()
@@ -11,23 +10,18 @@ sample_input = """0 3 6 9 12 15
 10 13 16 21 30 45"""
 
 
-def extrapolate(values: list[int], backwards: bool = False) -> int:
+def extrapolate(values: list[int], back: bool = False) -> int:
     if all(n == 0 for n in values):
         return 0
     differences = [b - a for a, b in zip(values[:-1], values[1:])]
-    x = extrapolate(differences, backwards)
-    return values[0] - x if backwards else values[-1] + x
+    x = extrapolate(differences, back)
+    return values[0] - x if back else values[-1] + x
 
 
 def solve(inputs):
-    total1, total2 = 0, 0
-    for line in inputs.splitlines():
-        values = [int(n) for n in line.split()]
-        total1 += extrapolate(values)
-        total2 += extrapolate(values, backwards=True)
-
-    print(f"Part 1: {total1}")
-    print(f"Part 2: {total2}\n")
+    histories = [[int(n) for n in line.split()] for line in inputs.splitlines()]
+    print(f"Part 1: {sum(extrapolate(values) for values in histories)}")
+    print(f"Part 2: {sum(extrapolate(values, back=True) for values in histories)}\n")
 
 
 solve(sample_input)
