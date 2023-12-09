49dec3234c12310d1d145573c32c92b70e331a9c
Gid <gsmith.jemba2000@gmail.com>
2023-12-09 06:05:31 +0000

    Day 9

--- 2023/day09.py
+++ 2023/day09.py
@@ -11,28 +11,20 @@ sample_input = """0 3 6 9 12 15
 10 13 16 21 30 45"""
 
 
-def extrapolate_forwards(values: list[int]) -> int:
+def extrapolate(values: list[int], backwards: bool = False) -> int:
     if all(n == 0 for n in values):
         return 0
     differences = [b - a for a, b in zip(values[:-1], values[1:])]
-    x = extrapolate_forwards(differences)
-    return values[-1] + x
-
-
-def extrapolate_backwards(values: list[int]) -> int:
-    if all(n == 0 for n in values):
-        return 0
-    differences = [b - a for a, b in zip(values[:-1], values[1:])]
-    x = extrapolate_backwards(differences)
-    return values[0] - x
+    x = extrapolate(differences, backwards)
+    return values[0] - x if backwards else values[-1] + x
 
 
 def solve(inputs):
     total1, total2 = 0, 0
     for line in inputs.splitlines():
         values = [int(n) for n in line.split()]
-        total1 += extrapolate_forwards(values)
-        total2 += extrapolate_backwards(values)
+        total1 += extrapolate(values)
+        total2 += extrapolate(values, backwards=True)
 
     print(f"Part 1: {total1}")
     print(f"Part 2: {total2}\n")
