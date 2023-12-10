c4c27390f043d1ddcd4521c0c606b90ac58c696b
Gid <gsmith.jemba2000@gmail.com>
2023-12-10 07:19:08 +0000

    Day 10 - part1

--- 2023/day10.py
+++ 2023/day10.py
@@ -5,15 +5,57 @@ with open(os.path.join(os.path.dirname(__file__), "inputs/day10_input.txt")) as 
     actual_input = f.read()
 
 
-sample_input = """xxx"""
+sample_input = """..F7.
+.FJ|.
+SJ.L7
+|F--J
+LJ..."""
+
+
+START = "S"
+NORTH, SOUTH, EAST, WEST = -1j, +1j, 1, -1
+DIRECTIONS = {
+    (NORTH, "|"): NORTH,
+    (SOUTH, "|"): SOUTH,
+    (EAST, "-"): EAST,
+    (WEST, "-"): WEST,
+    (SOUTH, "L"): EAST,
+    (WEST, "L"): NORTH,
+    (SOUTH, "J"): WEST,
+    (EAST, "J"): NORTH,
+    (EAST, "7"): SOUTH,
+    (NORTH, "7"): WEST,
+    (WEST, "F"): SOUTH,
+    (NORTH, "F"): EAST,
+}
 
 
 def solve(inputs):
-    values = tuple(map(int, inputs.splitlines()))
+    grid: dict[complex, str] = {}
+    start: complex | None = None
+    for y, line in enumerate(inputs.splitlines()):
+        for x, c in enumerate(line):
+            grid[complex(x, y)] = c
+            if c == START:
+                start = complex(x, y)
+    assert start
 
-    print(f"Part 1: {False}")
-    print(f"Part 2: {False}")
+    valid_headings = {
+        heading
+        for heading in (NORTH, SOUTH, EAST, WEST)
+        if (heading, grid.get(start + heading)) in DIRECTIONS
+    }
+
+    direction = valid_headings.pop()
+    location, steps = start + direction, 1
+    while location != start:
+        direction = DIRECTIONS[direction, grid[location]]
+        location += direction
+        steps += 1
+
+    print(f"Part 1: {steps//2}")
+    print(f"Part 2: {False}\n")
 
 
 solve(sample_input)
-# solve(actual_input)
+solve(actual_input)
