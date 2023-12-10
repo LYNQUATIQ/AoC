a2f41ee52a1ecc548c5a421ad4890256c3ba67d0
Gid <gsmith.jemba2000@gmail.com>
2023-12-10 10:50:29 +0000

    Tidying

--- 2023/day10.py
+++ 2023/day10.py
@@ -11,27 +11,6 @@ SJ.L7
 |F--J
 LJ..."""
 
-# sample_input = """.F----7F7F7F7F-7....
-# .|F--7||||||||FJ....
-# .||.FJ||||||||L7....
-# FJL7L7LJLJ||LJ.L-7..
-# L--J.L7...LJS7F-7L7.
-# ....F-J..F7FJ|L7L7L7
-# ....L7.F7||L7|.L7L7|
-# .....|FJLJ|FJ|F7|.LJ
-# ....FJL-7.||.||||...
-# ....L---J.LJ.LJLJ..."""
-
-# sample_input = """FF7FSF7F7F7F7F7F---7
-# L|LJ||||||||||||F--J
-# FL-7LJLJ||||||LJL-77
-# F--JF--7||LJLJ7F7FJ-
-# L---JF-JLJ.||-FJLJJ7
-# |F|F-JF---7F7-L7L|7|
-# |FFJF7L7F-JF7|JL---7
-# 7-L-JL7||F7|L7F-7F7|
-# L.L7LFJ|||||FJL7||LJ
-# L7JLJL-JLJLJL--JLJ.L"""
 
 START = "S"
 NORTH, SOUTH, EAST, WEST = -1j, +1j, 1, -1
@@ -83,16 +62,16 @@ def solve(inputs):
         location += direction
     print(f"Part 1: {len(path)//2}")
 
-    enclosed = set()
+    enclosed = 0
     for y in range(height):
         within_loop, x = False, 0
         while x < width:
             if within_loop and complex(x, y) not in path:
-                enclosed.add(complex(x, y))
+                enclosed += 1
             while x < width and complex(x, y) not in path:
                 x += 1
                 if within_loop and complex(x, y) not in path:
-                    enclosed.add(complex(x, y))
+                    enclosed += 1
             if x >= width:
                 break
             pipe = grid[complex(x, y)]
@@ -112,15 +91,8 @@ def solve(inputs):
                     within_loop = not within_loop
             x += 1
 
-    # for y in range(height):
-    #     line = ""
-    #     for x in range(width):
-    #         line += "\u2589" if complex(x, y) in enclosed else grid[complex(x, y)]
-    #     print(line)
-
-    print(f"Part 2: {len(enclosed)}\n")
+    print(f"Part 2: {enclosed}\n")
 
 
 solve(sample_input)
 solve(actual_input)
-# 441 is too high
