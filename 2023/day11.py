2e8565b98a1c5a781f51df237d5877e0403a1231
Gid <gsmith.jemba2000@gmail.com>
2023-12-11 08:04:37 +0000

    Use tuples

--- 2023/day11.py
+++ 2023/day11.py
@@ -1,6 +1,8 @@
 """https://adventofcode.com/2023/day/11"""
 import os
 
+from itertools import combinations
+
 with open(os.path.join(os.path.dirname(__file__), "inputs/day11_input.txt")) as f:
     actual_input = f.read()
 
@@ -17,40 +19,37 @@ sample_input = """...#......
 #...#....."""
 
 
-def expand_galaxies(inputs: str, expansion: int = 2) -> tuple[list[int], list[int]]:
-    initial_x, initial_y = [], []
+def expand_galaxies(inputs: str, expansion: int = 2) -> set[tuple[int, int]]:
+    initial = set()
     for y, line in enumerate(inputs.splitlines()):
         for x, c in enumerate(line):
             if c == "#":
-                initial_x.append(x)
-                initial_y.append(y)
+                initial.add((x, y))
     width, height = x + 1, y + 1
+    empty_rows = set(y for y in range(height) if y not in {xy[1] for xy in initial})
+    empty_columns = set(x for x in range(width) if x not in {xy[0] for xy in initial})
 
-    empty_rows = set(y for y in range(height) if y not in initial_y)
-    empty_columns = set(x for x in range(width) if x not in initial_x)
-
-    expanded_x, expanded_y = [], []
-    for x, y in zip(initial_x, initial_y):
-        expanded_x.append(x + (sum([c < x for c in empty_columns]) * (expansion - 1)))
-        expanded_y.append(y + (sum([r < y for r in empty_rows]) * (expansion - 1)))
-
-    return expanded_x, expanded_y
+    return {
+        (
+            x + sum([column < x for column in empty_columns]) * (expansion - 1),
+            y + sum([row < y for row in empty_rows]) * (expansion - 1),
+        )
+        for x, y in initial
+    }
 
 
-def solve(inputs, expansion: int):
-    galaxy_x, galaxy_y = expand_galaxies(inputs)
-    total = 0
-    for g1 in range(len(galaxy_x)):
-        for g2 in range(g1 + 1, len(galaxy_x)):
-            total += abs(galaxy_x[g1] - galaxy_x[g2]) + abs(galaxy_y[g1] - galaxy_y[g2])
-    print(f"Part 1: {total}")
+def solve(inputs: str, expansion: int):
+    galaxies = expand_galaxies(inputs)
+    distances = [
+        abs(x1 - x2) + abs(y1 - y2) for (x1, y1), (x2, y2) in combinations(galaxies, 2)
+    ]
+    print(f"Part 1: {sum(distances)}")
 
-    galaxy_x, galaxy_y = expand_galaxies(inputs, expansion)
-    total = 0
-    for g1 in range(len(galaxy_x)):
-        for g2 in range(g1 + 1, len(galaxy_x)):
-            total += abs(galaxy_x[g1] - galaxy_x[g2]) + abs(galaxy_y[g1] - galaxy_y[g2])
-    print(f"Part 2: {total}\n")
+    galaxies = expand_galaxies(inputs, expansion)
+    distances = [
+        abs(x1 - x2) + abs(y1 - y2) for (x1, y1), (x2, y2) in combinations(galaxies, 2)
+    ]
+    print(f"Part 2: {sum(distances)}\n")
 
 
 solve(sample_input, 10)
