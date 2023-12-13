57bf65273e38c4c70df114ae83dc3aa45a8cbc6a
Gid <gsmith.jemba2000@gmail.com>
2023-12-13 14:55:40 +0000

    Tidied day 13

--- 2023/day13.py
+++ 2023/day13.py
@@ -24,54 +24,41 @@ sample_input = """#.##..##.
 #....#..#"""
 
 ASH, ROCK = ".", "#"
+UNSMUDGE = {ASH: ROCK, ROCK: ASH}
 
 
-def find_v_mirror(pattern: list[str], ignore: int = 0) -> int | None:
-    rows = ["".join(row[x] for row in pattern) for x in range(len(pattern[0]))]
-    return find_h_mirror(rows, ignore)
-
-
-def find_h_mirror(rows: list[str], ignore: int = 0) -> int | None:
-    n_rows = len(rows)
-    for y in range(1, n_rows):
-        if y == ignore:
-            continue
-        if all(rows[y + i] == rows[y - i - 1] for i in range(min(n_rows - y, y))):
+def find_horizontal_reflection_line(rows: list[str], ignore: int = 0) -> int | None:
+    for y in (y for y in range(1, len(rows)) if y != ignore):
+        if all(rows[y + i] == rows[y - i - 1] for i in range(min(len(rows) - y, y))):
             return y
+    return None
+
+
+def find_reflection_line(
+    pattern: list[str], ignore: tuple[int, int] = (0, 0)
+) -> tuple[int, int] | None:
+    if h_mirror := find_horizontal_reflection_line(pattern, ignore[1]):
+        return (0, h_mirror)
+    pattern = ["".join(row[x] for row in pattern) for x in range(len(pattern[0]))]
+    if v_mirror := find_horizontal_reflection_line(pattern, ignore[0]):
+        return (v_mirror, 0)
     return None
 
 
 def solve(inputs: str):
     patterns = [grid.splitlines() for grid in inputs.split("\n\n")]
-
-    v_mirrors, h_mirrors, mirrors = [], [], []
-    for pattern in patterns:
-        if (h_mirror := find_h_mirror(pattern)) is not None:
-            mirrors.append((0, h_mirror))
-            h_mirrors.append(h_mirror)
-        else:
-            assert (v_mirror := find_v_mirror(pattern)) is not None
-            mirrors.append((v_mirror, 0))
-            v_mirrors.append(v_mirror)
-    print(f"Part 1: {sum(v_mirrors) + 100 * sum(h_mirrors)}")
-
-    v_mirrors, h_mirrors = [], []
-    for pattern, (original_v_mirror, original_h_mirror) in zip(patterns, mirrors):
-        columns, rows = len(pattern[0]), len(pattern)
-        for x, y in product(range(columns), range(rows)):
-            new_pattern = pattern[:]
-            exchanged_symbol = {ASH: ROCK, ROCK: ASH}[pattern[y][x]]
-            new_pattern[y] = pattern[y][:x] + exchanged_symbol + pattern[y][x + 1 :]
-            h_mirror = find_h_mirror(new_pattern, original_h_mirror)
-            if (h_mirror := find_h_mirror(new_pattern, original_h_mirror)) is not None:
-                h_mirrors.append(h_mirror)
+    lines1 = [find_reflection_line(pattern) for pattern in patterns]
+    lines2 = []
+    for pattern, prior_reflection in zip(patterns, lines1):
+        for x, y in product(range(len(pattern[0])), range(len(pattern))):
+            pattern2 = pattern[:]
+            pattern2[y] = pattern[y][:x] + UNSMUDGE[pattern[y][x]] + pattern[y][x + 1 :]
+            if mirror := find_reflection_line(pattern2, ignore=prior_reflection):
+                lines2.append(mirror)
                 break
-            if (v_mirror := find_v_mirror(new_pattern, original_v_mirror)) is not None:
-                v_mirrors.append(v_mirror)
-                break
-        else:
-            raise RuntimeError(f"No mirror found for pattern: {pattern}")
-    print(f"Part 2: {sum(v_mirrors) + 100 * sum(h_mirrors)}\n")
+
+    print(f"Part 1: {sum(m[0] for m in lines1) + 100 * sum(m[1] for m in lines1)}")
+    print(f"Part 2: {sum(m[0] for m in lines2) + 100 * sum(m[1] for m in lines2)}\n")
 
 
 solve(sample_input)
