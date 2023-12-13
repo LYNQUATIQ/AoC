916363180cd37f714a201ab537f27e2b6cd6d72d
Gid <gsmith.jemba2000@gmail.com>
2023-12-13 16:01:59 +0000

    Add type hints to day generator

--- 2023/day_generator.py
+++ 2023/day_generator.py
@@ -18,7 +18,7 @@ for i in range(1, 26):
         )
         f.write("    actual_input = f.read()\n\n\n")
         f.write('sample_input = """xxx"""\n\n\n')
-        f.write("def solve(inputs):\n")
+        f.write("def solve(inputs: str):\n")
         f.write("    values = tuple(map(int, inputs.splitlines()))\n\n")
         f.write('    print(f"Part 1: {False}")\n')
         f.write('    print(f"Part 2: {False}\n")\n\n\n')
