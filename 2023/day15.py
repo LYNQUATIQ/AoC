0abe3882c1d0a45740fb836300a71025e28adec8
Gid <gsmith.jemba2000@gmail.com>
2023-12-15 07:22:55 +0000

    Tidied day 15

--- 2023/day15.py
+++ 2023/day15.py
@@ -1,6 +1,7 @@
 """https://adventofcode.com/2023/day/15"""
 import os
 from collections import defaultdict
+from functools import reduce
 
 with open(os.path.join(os.path.dirname(__file__), "inputs/day15_input.txt")) as f:
     actual_input = f.read()
@@ -10,39 +11,26 @@ sample_input = """rn=1,cm-,qp=3,cm=2,qp-,pc=4,ot=9,ab=5,pc-,pc=6,ot=7"""
 
 
 def hash_value(token: str):
-    value = 0
-    for c in token:
-        value += ord(c)
-        value *= 17
-        value %= 256
-    return value
+    return reduce(lambda value, x: (value + ord(x)) * 17 % 256, token, 0)
 
 
 def solve(inputs: str):
     tokens = inputs.split(",")
-
-    value = 0
-    for token in tokens:
-        h = hash_value(token)
-        # print(token, h)
-        value += h
-    print(f"Part 1: {value}")
+    print(f"Part 1: {sum(hash_value(token) for token in tokens)}")
 
     boxes = defaultdict(dict)
     for token in tokens:
         if token.endswith("-"):
             label = token[:-1]
-            box = hash_value(label)
-            boxes[box].pop(label, None)
+            boxes[hash_value(label)].pop(label, None)
         else:
             label, lens = token.split("=")
-            box = hash_value(label)
-            boxes[box][label] = int(lens)
-    value = 0
+            boxes[hash_value(label)][label] = int(lens)
+    focusing_power = 0
     for box, lenses in boxes.items():
         for i, lens in enumerate(lenses.values(), 1):
-            value += (box + 1) * i * lens
-    print(f"Part 2: {value}\n")
+            focusing_power += (box + 1) * i * lens
+    print(f"Part 2: {focusing_power}\n")
 
 
 solve(sample_input)
