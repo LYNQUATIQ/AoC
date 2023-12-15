bb648506d72dbef0e89b6b63078800aa598d3e54
Gid <gsmith.jemba2000@gmail.com>
2023-12-15 07:47:01 +0000

    Rename variables

--- 2023/day15.py
+++ 2023/day15.py
@@ -11,7 +11,7 @@ sample_input = """rn=1,cm-,qp=3,cm=2,qp-,pc=4,ot=9,ab=5,pc-,pc=6,ot=7"""
 
 
 def hash_value(token: str):
-    return reduce(lambda value, x: (value + ord(x)) * 17 % 256, token, 0)
+    return reduce(lambda value, c: (value + ord(c)) * 17 % 256, token, 0)
 
 
 def solve(inputs: str):
