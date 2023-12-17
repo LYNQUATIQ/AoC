61a6d4016f3567d1e686e15cd96b4e8b4fb0394b
Gid <gsmith.jemba2000@gmail.com>
2023-12-17 16:01:27 +0000

    Store *next* directions in state

--- 2023/day17.py
+++ 2023/day17.py
@@ -25,9 +25,9 @@ sample_input = """2413432311323
 4322674655533"""
 
 
-#  State is a location, last direction
+#  State is a location, next direction
 Xy = tuple[int, int]
-State = tuple[Xy, int, Xy]
+State = tuple[Xy, Xy]
 
 
 class Grid:
@@ -45,24 +45,20 @@ class Grid:
 def possible_next_states(
     this_state: State, grid: Grid, min_steps: int, max_steps: int
 ) -> Generator[Iterable[tuple[State, int]]]:
-    xy, current_direction = this_state
-    if current_direction == (0, 0):
-        turn_left, turn_right = (1, 0), (0, 1)
-    else:
-        turn_left = (current_direction[1], current_direction[0])
-        turn_right = (current_direction[1] * -1, current_direction[0] * -1)
-    for direction in (turn_left, turn_right):
-        additional_heat_loss = 0
-        new_xy = xy
-        for n_steps in range(max_steps):
-            new_xy = (new_xy[0] + direction[0], new_xy[1] + direction[1])
-            if not grid.is_valid_location(new_xy):
-                break
-            additional_heat_loss += grid.get(new_xy)
-            if n_steps < min_steps - 1:
-                continue
-            next_state = (new_xy, direction)
-            yield (next_state, additional_heat_loss)
+    xy, direction = this_state
+    turn_right = (direction[1], direction[0])
+    turn_left = (direction[1] * -1, direction[0] * -1)
+    additional_heat_loss = 0
+    next_xy = xy
+    for n_steps in range(max_steps):
+        next_xy = (next_xy[0] + direction[0], next_xy[1] + direction[1])
+        if not grid.is_valid_location(next_xy):
+            break
+        additional_heat_loss += grid.get(next_xy)
+        if n_steps < min_steps - 1:
+            continue
+        yield ((next_xy, turn_right), additional_heat_loss)
+        yield ((next_xy, turn_left), additional_heat_loss)
 
 
 def manhattan_distance(a: Xy, b: Xy) -> int:
@@ -71,21 +67,19 @@ def manhattan_distance(a: Xy, b: Xy) -> int:
 
 def best_path(grid: Grid, max_steps: int, min_steps: int = 1) -> int:
     target = (grid.width - 1, grid.height - 1)
-    initial_state = ((0, 0), (0, 0))
-    to_visit = [(0, initial_state)]
-    heat_losses = {initial_state: 0}
+    start, initial_directions = (0, 0), ((0, 1), (1, 0))
+    initial_states = [(start, direction) for direction in initial_directions]
+    to_visit = [(0, initial_state) for initial_state in initial_states]
+    heat_losses = {initial_state: 0 for initial_state in initial_states}
     while to_visit:
         _, this_state = heappop(to_visit)
-        if this_state[0] in ((7, 0), (7, 4)):
-            pass
         if this_state[0] == target:
             return heat_losses[this_state]
         next_states = possible_next_states(this_state, grid, min_steps, max_steps)
         for next_state, additional_heat_loss in next_states:
             new_location = next_state[0]
             heat_loss = heat_losses[this_state] + additional_heat_loss
-            best_heat_loss_so_far = heat_losses.get(next_state, heat_loss + 1)
-            if heat_loss < best_heat_loss_so_far:
+            if heat_loss < heat_losses.get(next_state, heat_loss + 1):
                 heat_losses[next_state] = heat_loss
                 f_score = heat_loss + manhattan_distance(new_location, target)
                 heappush(to_visit, (f_score, next_state))
