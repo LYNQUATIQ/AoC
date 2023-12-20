8f5290cc9145403f859f8595e18473499760284f
Gid <gsmith.jemba2000@gmail.com>
2023-12-20 23:18:04 +0000

    Day 20 - part 2

--- 2023/day20.py
+++ 2023/day20.py
@@ -1,17 +1,11 @@
-"""https://adventofcode.com/2023/day/20"""
 import os
 
 from collections import defaultdict, deque
+import math
 
 with open(os.path.join(os.path.dirname(__file__), "inputs/day20_input.txt")) as f:
     actual_input = f.read()
 
-
-sample_input = """broadcaster -> a, b, c
-%a -> b
-%b -> c
-%c -> inv
-&inv -> a"""
 
 sample_input = """broadcaster -> a
 %a -> inv, con
@@ -25,6 +19,15 @@ BROADCASTER, OUTPUT = "broadcaster", "output"
 
 HIGH, LOW = True, False
 ON, OFF = True, False
+
+BIT_ORDER = {
+    "sn": {"ng": 1, "vp": 2, "vf": 4, "bt": 256, "xd": 512, "tn": 1024, "tv": 2048},
+    "tf": {"gr": 1, "td": 16, "vr": 64, "hq": 128, "mq": 512, "fl": 1024, "gz": 2048},
+    "lr": {"js": 1, "xv": 8, "hj": 32, "hm": 256, "rd": 512, "xl": 1024, "gx": 2048},
+    "hl": {"lb": 1, "qk": 16, "hs": 32, "xb": 128, "kg": 512, "px": 1024, "tm": 2048},
+}
+
+BLOCKS = {True: "\u2588", False: "\u25af"}
 
 
 def button_press(module_destinations, module_inputs, conjunctions, flipflops, counts):
@@ -57,7 +60,7 @@ def button_press(module_destinations, module_inputs, conjunctions, flipflops, co
     return True
 
 
-def solve(inputs: str, test_rx: bool = False):
+def solve(inputs: str, analyse_inputs: bool = False):
     module_destinations: dict[list[str]] = {OUTPUT: []}
     module_inputs: dict[str, set[str]] = defaultdict(set)
     conjunctions: dict[dict[str, bool]] = {}
@@ -83,21 +86,25 @@ def solve(inputs: str, test_rx: bool = False):
         )
     print(f"\nPart 1: {counts[LOW] * counts[HIGH]}")
 
-    if not test_rx:
+    if not analyse_inputs:
         return
 
     flipflops = {f: LOW for f in flipflops}
     conjunctions = {c: defaultdict(bool) for c in conjunctions}
-    button_presses = 1
-    while button_press(
-        module_destinations, module_inputs, conjunctions, flipflops, counts
-    ):
-        button_presses += 1
-        if button_presses % 1000 == 0:
-            print(button_presses)
+    presses = 0
+    while presses < 2_500:
+        presses += 1
+        button_press(
+            module_destinations, module_inputs, conjunctions, flipflops, counts
+        )
+        m = "lr"
+        print(
+            presses,
+            ", ".join(f"{k}-{BLOCKS[conjunctions[m].get(k, 0)]}" for k in BIT_ORDER[m]),
+        )
 
-    print(f"Part 2: {button_presses}\n")
+    print(f"\nPart 2: {math.lcm(*[sum(BIT_ORDER[m].values()) for m in BIT_ORDER])}\n")
 
 
 solve(sample_input)
-solve(actual_input, test_rx=True)
+solve(actual_input, analyse_inputs=True)
