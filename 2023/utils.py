ae62b98eb163ad0b68b49bde9b0817ec2911f5b4
Gid <gsmith.jemba2000@gmail.com>
2023-12-22 07:07:17 +0000

    Add utils

---
+++ 2023/utils.py
@@ -0,0 +1,43 @@
+import functools
+import time
+from itertools import chain, combinations, zip_longest
+from typing import Callable, Iterable, Optional, TypeVar
+
+T = TypeVar("T")
+
+
+def print_time_taken(func) -> Callable:
+    @functools.wraps(func)
+    def _wrapped_func(*args, **kwargs):
+        start_time = time.time()
+        retval = func(*args, **kwargs)
+        time_taken = time.time() - start_time
+        if time_taken > 1.0:
+            time_str = f"{time_taken:.2f}s"
+        elif time_taken > 0.001:
+            time_str = f"{time_taken*1_000:.2f}ms"
+        elif time_taken > 0.000_001:
+            time_str = f"{time_taken*1_000_000:.2f}µs"
+        else:
+            time_str = f"{time_taken*1_000_000_000:.2f}ns"
+        print(f"Time taken: {time_str}")
+        return retval
+
+    return _wrapped_func
+
+
+def flatten(list_of_lists: list[list[T]]):
+    # flatten([ [1, 2, 3], [4, 5, 6] ]) --> 1, 2, 3, 4, 5, 6
+    return chain.from_iterable(list_of_lists)
+
+
+def grouper(iterable: Iterable[T], n: int, fillvalue: Optional[T] = None):
+    # grouper('ABCDEFG', 3, 'x') --> ABC DEF Gxx
+    args = [iter(iterable)] * n
+    return zip_longest(*args, fillvalue=fillvalue)
+
+
+def powerset(iterable: Iterable[T]):
+    # powerset([1,2,3]) --> () (1,) (2,) (3,) (1,2) (1,3) (2,3) (1,2,3)
+    s = list(iterable)
+    return chain.from_iterable(combinations(s, r) for r in range(len(s) + 1))
