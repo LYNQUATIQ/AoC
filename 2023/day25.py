980d073a52a2a5f061df255f158014c4d69803d1
Gid <gsmith.jemba2000@gmail.com>
2023-12-26 16:02:23 +0000

    Better variable names

--- 2023/day25.py
+++ 2023/day25.py
@@ -24,7 +24,7 @@ frs: qnr lhk lsr"""
 
 
 def apply_karger(node_edges: dict[str, set[tuple[str, str]]]) -> tuple[int, int, int]:
-    # Take a copy of the graoh and counbters to keep track of nodes/edges merged
+    # Take a copy of the graoh and create counbters to keep track of nodes/edges merged
     graph, node_count, edge_count = {}, defaultdict(int), defaultdict(int)
     for node, edges in node_edges.items():
         graph[node] = set(edges)
@@ -33,25 +33,23 @@ def apply_karger(node_edges: dict[str, set[tuple[str, str]]]) -> tuple[int, int,
             edge_count[tuple(sorted([node, edge]))] = 1
 
     while len(graph) > 2:
-        node1 = random.choice(list(graph))
-        node2 = random.choice(list(graph[node1]))
-        node_count[node1] += node_count[node2]
-        graph[node1].remove(node2)
-        graph[node2].remove(node1)
-        for other_node in graph[node2]:
-            graph[other_node].remove(node2)
-            graph[other_node].add(node1)
-            graph[node1].add(other_node)
-            old_edge = tuple(sorted([node2, other_node]))
-            new_edge = tuple(sorted([node1, other_node]))
+        target = random.choice(list(graph))
+        to_merge = random.choice(list(graph[target]))
+        node_count[target] += node_count[to_merge]
+        graph[target].remove(to_merge)
+        graph[to_merge].remove(target)
+        for other_node in (n for n in graph[to_merge] if n != target):
+            graph[other_node].remove(to_merge)
+            graph[other_node].add(target)
+            graph[target].add(other_node)
+            old_edge = tuple(sorted([to_merge, other_node]))
+            new_edge = tuple(sorted([target, other_node]))
             edge_count[new_edge] += edge_count[old_edge]
-            del edge_count[old_edge]
-        del graph[node2]
-        del node_count[node2]
+        del graph[to_merge]
 
     cluster1, cluster2 = graph.keys()
-    edge = tuple(sorted([cluster1, cluster2]))
-    return edge_count[edge], node_count[cluster1], node_count[cluster2]
+    min_cut = tuple(sorted([cluster1, cluster2]))
+    return edge_count[min_cut], node_count[cluster1], node_count[cluster2]
 
 
 def solve(inputs: str):
@@ -63,11 +61,11 @@ def solve(inputs: str):
             node_edges[connected_node].add(this_node)
 
     while True:
-        min_cut, cluster1_size, cluster2_size = apply_karger(node_edges)
+        min_cut, cluster1, cluster2 = apply_karger(node_edges)
         if min_cut == 3:
             break
 
-    print(f"Answer: {cluster1_size* cluster2_size }\n")
+    print(f"Answer: {cluster1* cluster2 }\n")
 
 
 solve(sample_input)
