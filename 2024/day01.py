88def3795e1e74bd2e7cf3d4773e28b5b3c5f8a5
Gid <gsmith.jemba2000@gmail.com>
2024-12-01 05:12:14 +0000

    Day 1

--- 2024/day01.py
+++ 2024/day01.py
@@ -1,20 +1,37 @@
 """https://adventofcode.com/2024/day/1"""
 
 import os
+from collections import Counter
 
 with open(os.path.join(os.path.dirname(__file__), "inputs/day01_input.txt")) as f:
     actual_input = f.read()
 
 
-sample_input = """xxx"""
+sample_input = """3   4
+4   3
+2   5
+1   3
+3   9
+3   3
+"""
 
 
 def solve(inputs: str):
-    values = tuple(map(int, inputs.splitlines()))
+    list_a, list_b = [], []
+    for line in inputs.splitlines():
+        a, b = map(int, line.split())
+        list_a.append(a)
+        list_b.append(b)
+    list_a, list_b = sorted(list_a), sorted(list_b)
+    counts = Counter(list_b)
+    distance, similarity = 0, 0
+    for a, b in zip(list_a, list_b):
+        distance += abs(a - b)
+        similarity += a * counts[a]
 
-    print(f"Part 1: {False}")
-    print(f"Part 2: {False}\n")
+    print(f"Part 1: {distance}")
+    print(f"Part 2: {similarity}\n")
 
 
 solve(sample_input)
-# solve(actual_input)
+solve(actual_input)
