faf54c80ff634a277f45f2f96c46607587f762e5
Gid <gsmith.jemba2000@gmail.com>
2024-12-04 05:33:26 +0000

    Tidied day 4

--- src/aoc/2024/day04.py
+++ src/aoc/2024/day04.py
@@ -1,5 +1,7 @@
 """https://adventofcode.com/2024/day/4"""
 
+from itertools import product
+
 from aoc_utils import get_input_data
 
 actual_input = get_input_data(2024, 4)
@@ -23,32 +25,31 @@ def solve(inputs: str):
     grid = inputs.splitlines()
     width, height = len(grid[0]), len(grid)
 
-    def get_c(x, y):
+    def c_at_xy(x: int, y: int) -> str:
         if 0 <= x < width and 0 <= y < height:
             return grid[y][x]
         return ""
 
     xmas_count = 0
     mas_count = 0
-    for x in range(width):
-        for y in range(height):
-            for dx, dy in DIRECTIONS:
-                found_xmas = True
-                for c, i in zip("XMAS", range(4)):
-                    if get_c(y + i * dy, x + i * dx) != c:
-                        found_xmas = False
-                        break
-                if found_xmas:
-                    xmas_count += 1
-            if get_c(x, y) == "A":
-                if (
-                    (get_c(x - 1, y - 1) == "M" and get_c(x + 1, y + 1) == "S")
-                    or (get_c(x - 1, y - 1) == "S" and get_c(x + 1, y + 1) == "M")
-                ) and (
-                    (get_c(x - 1, y + 1) == "M" and get_c(x + 1, y - 1) == "S")
-                    or (get_c(x - 1, y + 1) == "S" and get_c(x + 1, y - 1) == "M")
-                ):
-                    mas_count += 1
+    for x, y in product(range(width), range(height)):
+        for dx, dy in DIRECTIONS:
+            if all(
+                [
+                    c_at_xy(y + d * dy, x + d * dx) == c_to_match
+                    for c_to_match, d in zip("XMAS", range(4))
+                ]
+            ):
+                xmas_count += 1
+        if c_at_xy(x, y) == "A":
+            if (
+                (c_at_xy(x - 1, y - 1) == "M" and c_at_xy(x + 1, y + 1) == "S")
+                or (c_at_xy(x - 1, y - 1) == "S" and c_at_xy(x + 1, y + 1) == "M")
+            ) and (
+                (c_at_xy(x - 1, y + 1) == "M" and c_at_xy(x + 1, y - 1) == "S")
+                or (c_at_xy(x - 1, y + 1) == "S" and c_at_xy(x + 1, y - 1) == "M")
+            ):
+                mas_count += 1
 
     print(f"Part 1: {xmas_count}")
     print(f"Part 2: {mas_count}\n")
