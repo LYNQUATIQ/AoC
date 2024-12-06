6fe478135aee7ae69a5363ee69949e2a7d53d1a9
Gid <gsmith.jemba2000@gmail.com>
2024-12-06 07:03:03 +0000

    Day 5 part 2 attempt 1

--- src/aoc/2024/day06.py
+++ src/aoc/2024/day06.py
@@ -40,20 +40,45 @@ def solve(inputs: str):
 
     guard_xys = {guard_xy}
     direction = NORTH
-    obstacles_hit = set()
+    obstacles_hit = []
+    potential_loop_candidate = None
+    loop_candidates = set()
     while True:
         next_step = (guard_xy[0] + direction[0], guard_xy[1] + direction[1])
         if next_step in obstacles:
+            obstacles_hit.append(next_step)
             direction = RIGHT_TURN[direction]
-            obstacles_hit.add(next_step)
+            if len(obstacles_hit) >= 3:
+                loop_far_corner = obstacles_hit[-3]
+                if direction == NORTH:
+                    x, y = guard_xy[0], loop_far_corner[1] - 1
+                    if y >= 0:
+                        potential_loop_candidate = (x, y)
+                elif direction == EAST:
+                    x, y = loop_far_corner[0] + 1, guard_xy[1]
+                    if x < width:
+                        potential_loop_candidate = (x, y)
+                elif direction == SOUTH:
+                    x, y = guard_xy[0], loop_far_corner[1] + 1
+                    if y < height:
+                        potential_loop_candidate = (x, y)
+                elif direction == WEST:
+                    x, y = loop_far_corner[0] - 1, guard_xy[1]
+                    if x >= 0:
+                        potential_loop_candidate = (x, y)
             continue
         if not (0 <= next_step[0] < height and 0 <= next_step[1] < width):
             break
+
         guard_xy = next_step
+        if guard_xy == potential_loop_candidate:
+            loop_candidates.add(potential_loop_candidate)
+            potential_loop_candidate = None
         guard_xys.add(guard_xy)
 
     print(f"Part 1: {len(guard_xys)}")
-    print(f"Part 2: {False}\n")
+    print(f"Part 2: {len(loop_candidates)}\n")
+    print(loop_candidates)
 
 
 solve(example_input)
