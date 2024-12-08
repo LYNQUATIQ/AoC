5258c01d1be28fbe4538da452483ee34b59d4ade
Gid <gsmith.jemba2000@gmail.com>
2024-12-08 05:55:13 +0000

    Tidied

--- src/aoc/2024/day08.py
+++ src/aoc/2024/day08.py
@@ -32,20 +32,18 @@ def solve(inputs: str):
     height, width = y + 1, x + 1
 
     antinodes_part1, antinodes_part2 = set(), set()
-    for positions in antennas.values():
-        for a, b in combinations(positions, 2):
-            dxy = b - a
-            for ant_xy, dxy in ((a, a - b), (b, b - a)):
-                i, in_range = 0, True
-                while in_range:
-                    antinode_xy = ant_xy + (dxy * i)
-                    in_range = False
-                    if 0 <= antinode_xy.real < width and 0 <= antinode_xy.imag < height:
-                        antinodes_part2.add(antinode_xy)
-                        if i == 1:
-                            antinodes_part1.add(antinode_xy)
-                        in_range = True
-                    i += 1
+    for antenna_positions in antennas.values():
+        for a, b in combinations(antenna_positions, 2):
+            for antenna_xy, dxy in ((a, a - b), (b, b - a)):
+                multiplier, in_grid = 0, True
+                while in_grid:
+                    antinode = antenna_xy + (dxy * multiplier)
+                    in_grid = 0 <= antinode.real < width and 0 <= antinode.imag < height
+                    if in_grid:
+                        antinodes_part2.add(antinode)
+                        if multiplier == 1:
+                            antinodes_part1.add(antinode)
+                    multiplier += 1
 
     print(f"Part 1: {len(antinodes_part1)}")
     print(f"Part 2: {len(antinodes_part2)}\n")
