9b81297df50acffa700b1136d60f057c55bc50ea
Gid <gsmith.jemba2000@gmail.com>
2024-12-08 05:59:13 +0000

    Tidied

--- src/aoc/2024/day08.py
+++ src/aoc/2024/day08.py
@@ -6,8 +6,6 @@ from itertools import combinations
 from aoc_utils import get_input_data
 
 actual_input = get_input_data(2024, 8)
-
-
 example_input = """............
 ........0...
 .....0......
@@ -26,20 +24,19 @@ def solve(inputs: str):
     antennas = defaultdict(list)
     for y, row in enumerate(inputs.splitlines()):
         for x, c in enumerate(row):
-            xy = complex(x, y)
             if c != ".":
-                antennas[c].append(xy)
-    height, width = y + 1, x + 1
+                antennas[c].append(complex(x, y))
+    max_x, max_y = x + 1, y + 1
 
     antinodes_part1, antinodes_part2 = set(), set()
     for antenna_positions in antennas.values():
         for a, b in combinations(antenna_positions, 2):
             for antenna_xy, dxy in ((a, a - b), (b, b - a)):
-                multiplier, in_grid = 0, True
-                while in_grid:
+                multiplier, in_range = 0, True
+                while in_range:
                     antinode = antenna_xy + (dxy * multiplier)
-                    in_grid = 0 <= antinode.real < width and 0 <= antinode.imag < height
-                    if in_grid:
+                    in_range = 0 <= antinode.real < max_x and 0 <= antinode.imag < max_y
+                    if in_range:
                         antinodes_part2.add(antinode)
                         if multiplier == 1:
                             antinodes_part1.add(antinode)
