86621c7aef60d3256aad3c469c7faf4d91848d59
Gideon Smith <gideon.smith@orbis.com>
2024-12-09 10:31:01 +0000

    Tidied

--- src/aoc/2024/day09.py
+++ src/aoc/2024/day09.py
@@ -3,38 +3,29 @@
 from aoc_utils import get_input_data
 
 actual_input = get_input_data(2024, 9)
-
-
 example_input = """2333133121414131402"""
 
 
 def solve(inputs: str):
-    i = 0
+    inputs += "0"
     pointer = 0
     file_id = 0
     file_sizes, file_positions, free_spaces = {}, {}, {}
     spaces = []
-    while True:
-        file_size = int(inputs[i])
+    for i in range(0, len(inputs), 2):
+        file_size, space_size = int(inputs[i]), int(inputs[i + 1])
         file_sizes[file_id] = file_size
         file_positions[file_id] = pointer
         pointer += file_size
         file_id += 1
-        try:
-            n_spaces = int(inputs[i + 1])
-            free_spaces[pointer] = (n_spaces, 0)
-            spaces.append(n_spaces)
-            pointer += n_spaces
-        except IndexError:
-            spaces.append(0)
-            break
-        i += 2
-    max_file_id = file_id - 1
+        free_spaces[pointer] = (space_size, 0)
+        spaces.append(space_size)
+        pointer += space_size
+
     disk = []
     for (file_id, file_size), n_spaces in zip(file_sizes.items(), spaces):
         disk += ([file_id] * file_size) + ([None] * n_spaces)
     start_index, end_index = 0, len(disk) - 1
-
     check_sum = 0
     while True:
         while disk[end_index] is None:  # Find the next file block at the end
@@ -46,14 +37,12 @@ def solve(inputs: str):
             start_index += 1
         if end_index < start_index:
             break
-        # Move file block at end to current position from start
-        check_sum += disk[end_index] * start_index
+        check_sum += disk[end_index] * start_index  # Move end file block up
         end_index -= 1
         start_index += 1
-
     print(f"Part 1: {check_sum}")
 
-    for file_to_move in range(max_file_id, 0, -1):
+    for file_to_move in range(len(file_positions) - 1, 0, -1):
         file_size = file_sizes[file_to_move]
         for pointer, (space, offset) in free_spaces.items():
             if pointer > file_positions[file_to_move]:
