cbffd17c939c1c1601a0b941eb7948ea841bec4a
Gideon Smith <gideon.smith@orbis.com>
2024-12-09 09:20:10 +0000

    Day 9 - part 1

--- src/aoc/2024/day09.py
+++ src/aoc/2024/day09.py
@@ -12,48 +12,48 @@ def solve(inputs: str):
     i = 0
     pointer = 0
     file_id = 0
-    files = {}
-    free_space = {}
+    file_sizes = {}
+    file_positions = {}
+    free_space = []
 
     disk = ""
     while True:
-        files[file_id] = int(inputs[i])
-        pointer += files[file_id]
+        file_size = int(inputs[i])
+        file_sizes[file_id] = file_size
+        file_positions[file_id] = pointer
+        pointer += file_size
         file_id += 1
         try:
-            free_space[pointer] = int(inputs[i + 1])
-            pointer += free_space[pointer]
+            spaces = int(inputs[i + 1])
+            free_space.append(spaces)
+            pointer += spaces
         except IndexError:
             break
         i += 2
-    free_space[pointer] = 0
+    free_space.append(0)
+    disk = []
+    for (file_id, file_size), spaces in zip(file_sizes.items(), free_space):
+        disk += ([file_id] * file_size) + ([None] * spaces)
+    start_index, end_index = 0, len(disk) - 1
 
-    disk = ""
-    for (file_id, file_size), spaces in zip(files.items(), free_space.values()):
-        disk += str(file_id) * file_size + "." * spaces
-
-    end_index, start_index = len(disk) - 1, 0
-    sorted_disk = ""
     check_sum = 0
-    file_id = 0
     while True:
-        # Find next file character from the end
-        while disk[end_index] == ".":
+        # Find the next file block at the end
+        while disk[end_index] is None:
             end_index -= 1
-        # Find next space character from the start
-        while disk[start_index] != ".":
+
+        # Find next free space from the start
+        while disk[start_index] is not None:
             if start_index >= end_index:
                 break
-            sorted_disk += disk[start_index]
-            check_sum += int(disk[start_index]) * start_index
+            check_sum += disk[start_index] * start_index
             start_index += 1
 
         if end_index < start_index:
             break
-        sorted_disk += disk[end_index]
-        start_index += 1
-        check_sum += int(disk[end_index]) * start_index
+        check_sum += disk[end_index] * start_index
         end_index -= 1
+        start_index += 1
 
     print(f"Part 1: {check_sum}")
     print(f"Part 2: {False}\n")
