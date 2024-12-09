645344b576322767670bed5eee445b66db0b700e
Gid <gsmith.jemba2000@gmail.com>
2024-12-09 07:25:44 +0000

    Start of day 9

--- src/aoc/2024/day09.py
+++ src/aoc/2024/day09.py
@@ -5,14 +5,51 @@ from aoc_utils import get_input_data
 actual_input = get_input_data(2024, 9)
 
 
-example_input = """xxx"""
+example_input = """2333133121414131402"""
 
 
 def solve(inputs: str):
-    values = tuple(map(int, inputs.splitlines()))
+    i = 0
+    pointer = 0
+    file_id = 0
+    files = {}
+    free_space = {}
 
-    print(f"Part 1: {False}")
-    print(f"Part 2: {False}\n")
+    while True:
+        files[file_id] = int(inputs[i])
+        pointer += files[file_id]
+        file_id += 1
+        try:
+            free_space[pointer] = int(inputs[i + 1])
+            pointer += free_space[pointer]
+        except IndexError:
+            break
+        i += 2
+    free_space[pointer] = 0
+
+    disk = ""
+    for (file_id, file_size), spaces in zip(files.items(), free_space.values()):
+        disk += str(file_id) * file_size + "." * spaces
+
+    end_index, start_index = len(disk) - 1, 0
+    sorted_disk = ""
+    while True:
+        while disk[end_index] == ".":
+            end_index -= 1
+            if end_index < start_index:
+                break
+        while disk[start_index] != ".":
+            sorted_disk += disk[start_index]
+            start_index += 1
+            if end_index < start_index:
+                break
+        sorted_disk += disk[end_index]
+        start_index += 1
+        end_index -= 1
+        input(sorted_disk)
+
+    print(f"Part 1: {disk}")
+    print(f"Part 2: {sorted_disk}\n")
 
 
 solve(example_input)
