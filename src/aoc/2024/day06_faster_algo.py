bc1179e4aa5388e65c0dbced03df136d8257989a
Gideon Smith <gideon.smith@orbis.com>
2024-12-09 15:59:53 +0000

    Tidied up, sped up, and better variable names

--- src/aoc/2024/day06_faster_algo.py
+++ src/aoc/2024/day06_faster_algo.py
@@ -1,6 +1,7 @@
 """https://adventofcode.com/2024/day/6"""
 
 from collections import defaultdict
+from itertools import product
 
 from aoc_utils import get_input_data, print_time_taken
 
@@ -22,6 +23,7 @@ EAST = +1
 WEST = -1
 
 RIGHT_TURN = {NORTH: EAST, EAST: SOUTH, SOUTH: WEST, WEST: NORTH}
+LEFT_TURN = {NORTH: WEST, WEST: SOUTH, SOUTH: EAST, EAST: NORTH}
 REVERSE = {NORTH: SOUTH, SOUTH: NORTH, EAST: WEST, WEST: EAST}
 OFF_MAP = complex(-1, -1)
 
@@ -38,14 +40,14 @@ def solve(inputs: str):
                 obstacles.add(xy)
             elif c == "^":
                 start_xy = xy
-    height, width = y + 1, x + 1
+    max_y, max_w = y + 1, x + 1
 
-    # Walk the guard path for part 1 and remember the positions visited
+    # Walk the steps the guard takes for part 1 and remember the positions visited
     guard_positions = []
     xy, heading = start_xy, NORTH
     while True:
         next_xy = xy + heading
-        if not (0 <= xy.real < width and 0 <= xy.imag < height):
+        if not (0 <= xy.real < max_w and 0 <= xy.imag < max_y):
             break
         guard_positions.append((xy, heading))
         if next_xy in obstacles:
@@ -55,26 +57,24 @@ def solve(inputs: str):
 
     print(f"Part 1: {len({p for p,_ in guard_positions})}")
 
-    # Precompute all the edges that lead away from the obstacles
+    # For part 2... precompute all the edges that lead away from the obstacles
     def get_edge_end(origin_xy, heading):
-        """Given a starting point/heading work out endpoint (i.e. obstacle or off map)"""
+        """Given an origin point/heading work out end point (i.e. obstacle or off map)"""
         xy = origin_xy
         while True:
             next_xy = xy + heading
             if next_xy in obstacles:
                 return xy
-            if not (0 <= xy.real < width and 0 <= xy.imag < height):
+            if not (0 <= xy.real < max_w and 0 <= xy.imag < max_y):
                 return OFF_MAP
             xy = next_xy
 
     edges = defaultdict(dict)
-    edges[NORTH][start_xy] = get_edge_end(start_xy, NORTH)
-    for obstacle in obstacles:
-        for neighbour_direction in (NORTH, SOUTH, EAST, WEST):
-            neighour_xy = obstacle + neighbour_direction
-            heading = REVERSE[neighbour_direction]
-            heading = RIGHT_TURN[heading]
-            edges[heading][neighour_xy] = get_edge_end(neighour_xy, heading)
+    for obstacle, neighbour_direction in product(obstacles, (NORTH, SOUTH, EAST, WEST)):
+        turn_xy = obstacle + neighbour_direction
+        new_heading = LEFT_TURN[neighbour_direction]
+        edges[new_heading][turn_xy] = get_edge_end(turn_xy, new_heading)
+    edges[NORTH][start_xy] = get_edge_end(start_xy, NORTH)  # Add the initial 'edge'
 
     # Loop over the steps in the path and test adding an extra obstacle ahead of us
     obstacle_candidates = set()
@@ -82,40 +82,36 @@ def solve(inputs: str):
         extra_obstacle = path_xy + path_heading
         if extra_obstacle in obstacles or extra_obstacle == start_xy:
             continue
-        # Find edges that need to be overridden, and add extra ones due to new obstacle
-        obstacle_x, obstacle_y = extra_obstacle.real, extra_obstacle.imag
+        # Find edges that should be shortened due to new obstacle and add extra 'turn'
+        extra_x, extra_y = extra_obstacle.real, extra_obstacle.imag
         extra_edges = defaultdict(dict)
-        for edge_direction in (NORTH, SOUTH, EAST, WEST):
-            for start, end in edges[edge_direction].items():
-                x0, y0 = start.real, start.imag
-                x1, y1 = end.real, end.imag
-                if edge_direction == NORTH:
-                    if (obstacle_x != x0) or not (y0 > obstacle_y >= y1):
+        for edge_heading, directional_edges in edges.items():
+            for edge_start, edge_end in directional_edges.items():
+                x0, y0 = edge_start.real, edge_start.imag
+                x1, y1 = edge_end.real, edge_end.imag
+                if edge_heading == NORTH:
+                    if extra_x != x0 or not y0 > extra_y >= y1:
                         continue
-                    xy, heading_out = extra_obstacle + SOUTH, RIGHT_TURN[edge_direction]
-                elif edge_direction == SOUTH:
-                    y1 = height if y1 == -1 else y1
-                    if (obstacle_x != x0) or not (y0 < obstacle_y <= y1):
+                elif edge_heading == SOUTH:
+                    if extra_x != x0 or not y0 < extra_y <= (max_y if y1 == -1 else y1):
                         continue
-                    xy, heading_out = extra_obstacle + NORTH, RIGHT_TURN[edge_direction]
-                elif edge_direction == EAST:
-                    x1 = width if x1 == -1 else x1
-                    if (obstacle_y != y0) or not (x0 < obstacle_x <= x1):
+                elif edge_heading == EAST:
+                    if extra_y != y0 or not x0 < extra_x <= (max_w if x1 == -1 else x1):
                         continue
-                    xy, heading_out = extra_obstacle + WEST, RIGHT_TURN[edge_direction]
-                elif edge_direction == WEST:
-                    if (obstacle_y != y0) or not (x0 > obstacle_x >= x1):
+                elif edge_heading == WEST:
+                    if extra_y != y0 or not x0 > extra_x >= x1:
                         continue
-                    xy, heading_out = extra_obstacle + EAST, RIGHT_TURN[edge_direction]
-                # Found edge hitting the extra obstacle; shorten it and add new one
-                extra_edges[edge_direction][start] = xy
-                extra_edges[heading_out][xy] = get_edge_end(xy, heading_out)
+                # Found edge hitting the extra obstacle; shorten it and add new edge
+                turn_xy = extra_obstacle + REVERSE[edge_heading]
+                new_heading = RIGHT_TURN[edge_heading]
+                extra_edges[edge_heading][edge_start] = turn_xy
+                extra_edges[new_heading][turn_xy] = get_edge_end(turn_xy, new_heading)
 
-        # Test if the extra obstacle (i.e. extra edges) causes a loop
+        # Test if the extra obstacle (extra edges) now causes a loop
         xy, heading, visited = start_xy, NORTH, set()
         while xy != OFF_MAP:
-            if (xy, heading) in visited:
-                obstacle_candidates.add(extra_obstacle)  # Found a loop
+            if (xy, heading) in visited:  # Found a loop
+                obstacle_candidates.add(extra_obstacle)
                 break
             visited.add((xy, heading))
             try:
