f6b6e0540b28443a32d2b0a04cf86460e38d7fe6
Gideon Smith <gideon.smith@orbis.com>
2024-12-11 10:10:41 +0000

    Simplify day 11

--- src/aoc/2024/day11.py
+++ src/aoc/2024/day11.py
@@ -8,28 +8,33 @@ actual_input = get_input_data(2024, 11)
 example_input = "125 17"
 
 
-def iterate_stone(value: int) -> list[int]:
-    if value == 0:
-        return [1]
-    if len(str(value)) % 2 == 0:
-        return [
-            int(str(value)[: len(str(value)) // 2]),
-            int(str(value)[len(str(value)) // 2 :]),
-        ]
-    return [value * 2024]
-
-
 @cache
-def stone_count(stone_value: int, iterations_left: int) -> int:
-    if iterations_left == 0:
+def stone_count(stone_value: int, iterations_to_do: int) -> int:
+    if iterations_to_do == 0:
         return 1
-    return sum(stone_count(v, iterations_left - 1) for v in iterate_stone(stone_value))
+
+    # Determine the stone(s) after we iterate (i.e. blink at) this stone
+    if stone_value == 0:
+        stones_after_iteration = [1]
+    elif len(str(stone_value)) % 2 == 0:
+        stones_after_iteration = [
+            int(str(stone_value)[: len(str(stone_value)) // 2]),
+            int(str(stone_value)[len(str(stone_value)) // 2 :]),
+        ]
+    else:
+        stones_after_iteration = [stone_value * 2024]
+
+    return total_stone_count(stones_after_iteration, iterations_to_do - 1)
+
+
+def total_stone_count(stones: list[int], iterations_to_do: int) -> int:
+    return sum(stone_count(stone, iterations_to_do) for stone in stones)
 
 
 def solve(inputs: str):
-    stones = list(map(int, inputs.split()))
-    print(f"Part 1: {sum(stone_count(stone, 25) for stone in stones)}")
-    print(f"Part 2: {sum(stone_count(stone, 75) for stone in stones)}\n")
+    inital_stones = list(map(int, inputs.split()))
+    print(f"Part 1: {total_stone_count(inital_stones, 25)}")
+    print(f"Part 2: {total_stone_count(inital_stones, 75)}\n")
 
 
 solve(example_input)
