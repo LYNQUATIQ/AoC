bf9f874dcbe3134577f44158c6dcb15a01c3d6e9
Gid <gsmith.jemba2000@gmail.com>
2024-12-11 05:58:46 +0000

    Day 11 part 1

--- src/aoc/2024/day11.py
+++ src/aoc/2024/day11.py
@@ -1,19 +1,41 @@
 """https://adventofcode.com/2024/day/11"""
 
-from aoc_utils import get_input_data
+from functools import cache
 
-actual_input = get_input_data(2024, 11)
+actual_input = "9694820 93 54276 1304 314 664481 0 4"
+example_input = "125 17"
 
 
-example_input = """xxx"""
+@cache
+def process_value(value: int) -> list[int]:
+    if value == 0:
+        return [1]
+    if len(str(value)) % 2 == 0:
+        lhs = int(str(value)[: len(str(value)) // 2])
+        rhs = int(str(value)[len(str(value)) // 2 :])
+        return [lhs, rhs]
+    return [value * 2024]
+
+
+def stone_count(
+    value: int, total_iterations: int, current_iteration: int = 0
+) -> list[int]:
+    if current_iteration == total_iterations:
+        return 1
+    values = process_value(value)
+    return sum(stone_count(v, total_iterations, current_iteration + 1) for v in values)
+
+
+def total_stone_count(initial_stones: list[int], total_iterations: int) -> list[int]:
+    return sum(stone_count(stone, total_iterations) for stone in initial_stones)
 
 
 def solve(inputs: str):
-    values = tuple(map(int, inputs.splitlines()))
+    stones = list(map(int, inputs.split()))
 
-    print(f"Part 1: {False}")
-    print(f"Part 2: {False}\n")
+    print(f"Part 1: {total_stone_count(stones, 25)}")
+    # print(f"Part 2: {total_stone_count(stones, 75)}\n")
 
 
 solve(example_input)
-# solve(actual_input)
+solve(actual_input)
