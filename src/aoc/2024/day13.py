2bb30f2fd5c7be2c43c42c0e9875384132e76754
Gid <gsmith.jemba2000@gmail.com>
2024-12-13 08:10:21 +0000

    Day 13 wip

--- src/aoc/2024/day13.py
+++ src/aoc/2024/day13.py
@@ -1,17 +1,50 @@
 """https://adventofcode.com/2024/day/13"""
 
+import re
+
 from aoc_utils import get_input_data
 
 actual_input = get_input_data(2024, 13)
 
 
-example_input = """xxx"""
+example_input = """Button A: X+94, Y+34
+Button B: X+22, Y+67
+Prize: X=8400, Y=5400
+
+Button A: X+26, Y+66
+Button B: X+67, Y+21
+Prize: X=12748, Y=12176
+
+Button A: X+17, Y+86
+Button B: X+84, Y+37
+Prize: X=7870, Y=6450
+
+Button A: X+69, Y+23
+Button B: X+27, Y+71
+Prize: X=18641, Y=10279"""
 
 
 def solve(inputs: str):
-    values = tuple(map(int, inputs.splitlines()))
+    machine_inputs = inputs.split("\n\n")
+    machines = []
+    for machine_input in machine_inputs:
+        button_a_line, button_b_line, prize_line = machine_input.splitlines()
+        a_xy = tuple(map(int, re.findall(r"-?\d+", button_a_line)))
+        b_xy = tuple(map(int, re.findall(r"-?\d+", button_b_line)))
+        prize_xy = tuple(map(int, re.findall(r"-?\d+", prize_line)))
+        machines.append(
+            {
+                "x": (a_xy[0], b_xy[0], prize_xy[0]),
+                "y": (a_xy[1], b_xy[1], prize_xy[1]),
+            }
+        )
 
-    print(f"Part 1: {False}")
+    total_cost = 0
+    for machine in machines:
+
+        print(machine)
+
+    print(f"Part 1: {total_cost}")
     print(f"Part 2: {False}\n")
 
 
