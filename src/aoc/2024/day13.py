a83f6f97a58208c6af44fe97eec0ee3b1d4dcd89
Gideon Smith <gideon.smith@orbis.com>
2024-12-13 11:21:01 +0000

    Tidied day 13

--- src/aoc/2024/day13.py
+++ src/aoc/2024/day13.py
@@ -1,6 +1,4 @@
-"""https://adventofcode.com/2024/day/13
-    https://en.wikipedia.org/wiki/Diophantine_equation
-"""
+"""https://adventofcode.com/2024/day/13"""
 
 import math
 import re
@@ -8,8 +6,6 @@ import re
 from aoc_utils import get_input_data
 
 actual_input = get_input_data(2024, 13)
-
-
 example_input = """Button A: X+94, Y+34
 Button B: X+22, Y+67
 Prize: X=8400, Y=5400
@@ -27,44 +23,44 @@ Button B: X+27, Y+71
 Prize: X=18641, Y=10279"""
 
 
-def find_total_cost(machines: dict, extra_distance: int = 0):
-    total_cost = 0
-    for machine in machines:
+def find_total_cost(
+    machines: list[tuple[int, int, int, int, int, int]], extra_distance: int = 0
+):
+    """See https://en.wikipedia.org/wiki/Diophantine_equation
 
-        ax, bx, prize_x = machine["x"]
-        ay, by, prize_y = machine["y"]
+    Assuming A,B are the number of times we press the buttons A and B, then:
+        ax * A  +  bx * B  =  prize_x
+        ay * A  +  by * B  =  prize_y
+
+    Find the lcm of ax,ay and then multiply through by lcm/ax and -lcm/ay so that:
+       lcm * A  + bx' * B  =  prize_x'
+      -lcm * A  + by' * B  =  prize_y'
+
+    Add the equations together to give a single equation with one unknown, B.
+    Rearrange to find B, and then A (checking they're integers, i.e. a solution exists)
+        B = (prize_x' + prize_y') / (bx' + by')
+        A = (prize_x' - B * bx') / ax
+    """
+    total_cost = 0
+    for ax, ay, bx, by, prize_x, prize_y in machines:
         prize_x += extra_distance
         prize_y += extra_distance
 
-        # Find the lcm of ax and bx
         lcm = math.lcm(ax, ay)
-        ax, bx, prize_x = ax * (lcm // ax), bx * (lcm // ax), prize_x * (lcm // ax)
-        ay, by, prize_y = ay * (-lcm // ay), by * (-lcm // ay), prize_y * (-lcm // ay)
+        ax, bx, prize_x = ax * (lcm / ax), bx * (lcm / ax), prize_x * (lcm / ax)
+        ay, by, prize_y = ay * (-lcm / ay), by * (-lcm / ay), prize_y * (-lcm / ay)
 
-        b_presses = (prize_x + prize_y) / (by + bx)
+        b_presses = (prize_x + prize_y) / (bx + by)
         a_presses = (prize_x - b_presses * bx) / ax
-        if b_presses != int(b_presses) or a_presses != int(a_presses):
-            continue
-        total_cost += int(a_presses * 3 + b_presses)
+
+        if b_presses == int(b_presses) and a_presses == int(a_presses):
+            total_cost += int(a_presses * 3 + b_presses)
 
     return total_cost
 
 
 def solve(inputs: str):
-    machine_inputs = inputs.split("\n\n")
-    machines = []
-    for machine_input in machine_inputs:
-        button_a_line, button_b_line, prize_line = machine_input.splitlines()
-        a_xy = tuple(map(int, re.findall(r"-?\d+", button_a_line)))
-        b_xy = tuple(map(int, re.findall(r"-?\d+", button_b_line)))
-        prize_xy = tuple(map(int, re.findall(r"-?\d+", prize_line)))
-        machines.append(
-            {
-                "x": (a_xy[0], b_xy[0], prize_xy[0]),
-                "y": (a_xy[1], b_xy[1], prize_xy[1]),
-            }
-        )
-
+    machines = [tuple(map(int, re.findall(r"\d+", m))) for m in inputs.split("\n\n")]
     print(f"Part 1: {find_total_cost(machines)}")
     print(f"Part 2: {find_total_cost(machines, 10_000_000_000_000)}\n")
 
