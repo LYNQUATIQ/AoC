62666472274fcbda90480c4237bf0638437785a3
Gideon Smith <gideon.smith@orbis.com>
2024-12-14 14:44:18 +0000

    Faster search heuristic for xmas tree

--- src/aoc/2024/day14.py
+++ src/aoc/2024/day14.py
@@ -1,6 +1,9 @@
 """https://adventofcode.com/2024/day/14"""
 
+import math
 import re
+import statistics
+
 from collections import defaultdict
 
 from aoc_utils import get_input_data
@@ -20,53 +23,68 @@ p=2,4 v=2,-3
 p=9,5 v=-3,-3"""
 
 
-def solve(inputs: str, width: int, height: int):
+def find_xmas_tree(
+    initial_robots: list[tuple[int, int, int, int]], width: int, height: int
+):
+    # Run through the robot positions unitl they cycle again and note the x/y variances
+    robots = initial_robots[:]
+    x_variances, y_variances = {}, {}
+    for i in range(max(width, height)):
+        robots = move_robots(robots, width, height)
+        x, y, _, _ = zip(*robots)
+        x_variances[i] = statistics.stdev(x)
+        y_variances[i] = statistics.stdev(y)
+
+    # Find the iteration with the lowest variance for the x and y positions respectively
+    # (the assumption is that the robots will will cluster when they form the xmas tree)
+    x_iteration = min(x_variances, key=x_variances.get)
+    y_iteration = min(y_variances, key=y_variances.get)
+
+    # Find the subsequent iterations where they hit their 'tree positions' and find the
+    # iteration where they hit in both the x and y dimension simultaneously
+    x_hits = {x_iteration + width * i for i in range(height)}
+    y_hits = {y_iteration + height * i for i in range(width)}
+    iterations = (x_hits & y_hits).pop() + 1
+
+    # Draw the xmas tree
+    robots = initial_robots
+    grid = {(x, y) for x, y, _, _ in move_robots(robots, width, height, iterations)}
+    for y in range(height):
+        for x in range(width):
+            print("*" if (x, y) in grid else " ", end="")
+        print()
+
+    print(f"Part 2: {iterations}\n")
+
+
+def move_robots(
+    robots: list[tuple[int, int, int, int]],
+    width: int,
+    height: int,
+    iterations: int = 1,
+) -> list[tuple[int, int, int, int]]:
+    return [
+        ((px + vx * iterations) % width, (py + vy * iterations) % height, vx, vy)
+        for px, py, vx, vy in robots
+    ]
+
+
+def solve(inputs: str, width: int, height: int, find_easter_egg: bool = False):
     robots = [tuple(map(int, re.findall(r"-?\d+", m))) for m in inputs.splitlines()]
 
-    final_positions = []
-    for px, py, vx, vy in robots:
-        x = (px + vx * 100) % width
-        y = (py + vy * 100) % height
-        final_positions.append((x, y))
-
+    final_positions = move_robots(robots, width, height, 100)
     mid_x, mid_y = width // 2, height // 2
     quadrants = defaultdict(int)
-    for x, y in final_positions:
+    for x, y, _, _ in final_positions:
         if x == mid_x or y == mid_y:
             continue
         quadrants[(y < mid_y, x < mid_x)] += 1
 
-    safety_factor = 1
-    for value in quadrants.values():
-        safety_factor *= value
-    print(f"Part 1: {safety_factor}")
+    print(f"Part 1: {math.prod(quadrants.values())}")
 
-
-def find_xmas_tree(inputs: str, width: int, height: int):
-    robots = [tuple(map(int, re.findall(r"-?\d+", m))) for m in inputs.splitlines()]
-    iteration, interesting = 0, False
-    while not interesting:
-        iteration += 1
-        new_robots = []
-        grid = defaultdict(lambda: False)
-        rows, columns = defaultdict(int), defaultdict(int)
-        for px, py, vx, vy in robots:
-            x = (px + vx) % width
-            y = (py + vy) % height
-            grid[(x, y)] = True
-            columns[x] += 1
-            rows[y] += 1
-            new_robots.append((x, y, vx, vy))
-        robots = new_robots
-        interesting = max(rows.values()) > 30 and max(columns.values()) > 30
-
-    for y in range(height):
-        for x in range(width):
-            print("*" if grid[(x, y)] else " ", end="")
-        print()
-    print(f"Part 2: {iteration}\n")
+    if find_easter_egg:
+        find_xmas_tree(robots, width, height)
 
 
 solve(example_input, 11, 7)
-solve(actual_input, 101, 103)
-find_xmas_tree(actual_input, 101, 103)
+solve(actual_input, 101, 103, find_easter_egg=True)
