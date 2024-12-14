6b89b5f95620da44b8f3a43979a7ae8a37daf664
Gid <gsmith.jemba2000@gmail.com>
2024-12-14 06:23:52 +0000

    Tidied Day 14

--- src/aoc/2024/day14.py
+++ src/aoc/2024/day14.py
@@ -6,8 +6,6 @@ from collections import defaultdict
 from aoc_utils import get_input_data
 
 actual_input = get_input_data(2024, 14)
-
-
 example_input = """p=0,4 v=3,-3
 p=6,3 v=-1,-3
 p=10,3 v=-1,2
@@ -25,24 +23,23 @@ p=9,5 v=-3,-3"""
 def solve(inputs: str, width: int, height: int):
     robots = [tuple(map(int, re.findall(r"-?\d+", m))) for m in inputs.splitlines()]
 
-    positions = []
+    final_positions = []
     for px, py, vx, vy in robots:
         x = (px + vx * 100) % width
         y = (py + vy * 100) % height
-        positions.append((x, y))
+        final_positions.append((x, y))
 
     mid_x, mid_y = width // 2, height // 2
     quadrants = defaultdict(int)
-    for x, y in positions:
+    for x, y in final_positions:
         if x == mid_x or y == mid_y:
             continue
-        in_top, in_left = y < mid_y, x < mid_x
-        quadrants[(in_top, in_left)] += 1
+        quadrants[(y < mid_y, x < mid_x)] += 1
 
-    rating = 1
+    safety_factor = 1
     for value in quadrants.values():
-        rating *= value
-    print(f"Part 1: {rating}")
+        safety_factor *= value
+    print(f"Part 1: {safety_factor}")
 
 
 def find_xmas_tree(inputs: str, width: int, height: int):
@@ -51,21 +48,21 @@ def find_xmas_tree(inputs: str, width: int, height: int):
     while not interesting:
         iteration += 1
         new_robots = []
-        grid = defaultdict(int)
+        grid = defaultdict(lambda: False)
         rows, columns = defaultdict(int), defaultdict(int)
         for px, py, vx, vy in robots:
             x = (px + vx) % width
             y = (py + vy) % height
-            grid[(x, y)] += 1
-            rows[x] += 1
-            columns[y] += 1
+            grid[(x, y)] = True
+            columns[x] += 1
+            rows[y] += 1
             new_robots.append((x, y, vx, vy))
         robots = new_robots
         interesting = max(rows.values()) > 30 and max(columns.values()) > 30
 
     for y in range(height):
         for x in range(width):
-            print(" " if grid[(x, y)] == 0 else "*", end="")
+            print("*" if grid[(x, y)] else " ", end="")
         print()
     print(f"Part 2: {iteration}\n")
 
