feddf519753ab05e93720a3fc9f3efbc6358841a
Gid <gsmith.jemba2000@gmail.com>
2024-12-15 07:37:28 +0000

    Day 15 wip

--- src/aoc/2024/day15.py
+++ src/aoc/2024/day15.py
@@ -27,19 +27,37 @@ vvv<<^>^v^^><<>>><>^<<><^vv^^<>vvv<>><^^v>^>vv<>v<<<<v<^v>^<^^>>>^<v<v
 ^^>vv<^v^v<vv>^<><v<^v>^^^>>>^^vvv^>vvv<>>>^<^>>>>>^<<^v>^vvv<>^<><<v>
 v^^>>><<^^<>>^v^<v^vv<>v^<<>^<^v^v><^<<<><<^<v><v<>vv>>v><v^<vv<>v^<<^"""
 
+# example_input = """#######
+# #...#.#
+# #.....#
+# #..OO@#
+# #..O..#
+# #.....#
+# #######
+
+# <vv<<^^<<^^"""
+
 WALL, BOX, ROBOT = "#", "O", "@"
+BOX_LHS, BOX_RHS, BIG_BOX = "[", "]", "[]"
+
 UP, DOWN, LEFT, RIGHT = -1j, 1j, -1, 1
 INSTRUCTIONS = {"^": UP, ">": RIGHT, "v": DOWN, "<": LEFT, "\n": 0}
 
 
-def draw_map(robot_xy, walls, boxes, width, height):
+def draw_map(robot_xy, walls, boxes, width, height, big_boxes: bool = False):
     for y in range(height):
-        for x in range(width):
+        x = 0
+        while x < width:
+            x += 1
             xy = complex(x, y)
             if xy == robot_xy:
                 print(ROBOT, end="")
             elif xy in boxes:
-                print(BOX, end="")
+                if big_boxes:
+                    print(BIG_BOX, end="")
+                    x += 1
+                else:
+                    print(BOX, end="")
             elif xy in walls:
                 print(WALL, end="")
             else:
@@ -50,43 +68,104 @@ def draw_map(robot_xy, walls, boxes, width, height):
 def solve(inputs: str):
     grid, instructions = inputs.split("\n\n")
 
-    walls, boxes = set(), set()
+    # walls, boxes = set(), set()
+    # robot_xy = None
+    # for y, row in enumerate(grid.split("\n")):
+    #     for x, cell in enumerate(row):
+    #         xy = complex(x, y)
+    #         if cell == ROBOT:
+    #             robot_xy = xy
+    #         elif cell == WALL:
+    #             walls.add(xy)
+    #         elif cell == BOX:
+    #             boxes.add(xy)
+    # assert robot_xy is not None
+
+    # for instruction in instructions:
+    #     move = INSTRUCTIONS[instruction]
+    #     xy = robot_xy + move
+    #     boxes_to_move = []
+    #     while xy in boxes:
+    #         boxes_to_move.append(xy)
+    #         xy += move
+    #     if xy in walls:
+    #         continue
+    #     for box in boxes_to_move[::-1]:
+    #         boxes.discard(box)
+    #         boxes.add(box + move)
+    #     robot_xy += move
+
+    # gps_sum = 0
+    # for box in boxes:
+    #     gps_sum += abs(box.real) + abs(box.imag) * 100
+    # print(f"Part 1: {int(gps_sum)}")
+
+    walls, boxes, connected_boxes, boxes_lhs = set(), set(), {}, set()
     robot_xy = None
     for y, row in enumerate(grid.split("\n")):
-        for x, cell in enumerate(row):
+        x = 0
+        for _, cell in enumerate(row):
             xy = complex(x, y)
+            next_xy = complex(x + 1, y)
+            x += 2
             if cell == ROBOT:
                 robot_xy = xy
             elif cell == WALL:
                 walls.add(xy)
+                walls.add(next_xy)
             elif cell == BOX:
+                boxes_lhs.add(xy)
                 boxes.add(xy)
-    assert robot_xy is not None
+                boxes.add(next_xy)
+                connected_boxes[xy] = next_xy
+                connected_boxes[next_xy] = xy
     width, height = x + 1, y + 1
-
     for instruction in instructions:
-        # draw_map(robot_xy, walls, boxes, width, height)
-        # print("\nMove:", instruction, f"(robot at {robot_xy})")
-
         move = INSTRUCTIONS[instruction]
         xy = robot_xy + move
-        boxes_to_move = []
-        while xy in boxes:
-            boxes_to_move.append(xy)
-            xy += move
-        if xy in walls:
-            continue
-        for box in boxes_to_move[::-1]:
-            boxes.remove(box)
-            boxes.add(box + move)
-        robot_xy += move
+        hit_wall = False
+        if move in (LEFT, RIGHT):
+            boxes_to_move = set()
+            while xy in boxes:
+                boxes_to_move.add(xy)
+                xy += move
+            if xy in walls:
+                hit_wall = True
+                continue
+        elif move in (UP, DOWN):
+            boxes_to_move, front_edge = set(), {xy}
+            hit_wall = False
+            while front_edge:
+                if any(edge in walls for edge in front_edge):
+                    hit_wall = True
+                    break
+                front_edge = {edge for edge in front_edge if edge in boxes}
+                front_edge |= {connected_boxes[edge] for edge in front_edge}
+                boxes_to_move |= front_edge
+                front_edge = {edge + move for edge in front_edge}
+
+        if not hit_wall:
+            robot_xy += move
+            boxes -= boxes_to_move
+            boxes |= {box + move for box in boxes_to_move}
+            new_connected_boxes = {
+                box + move: connected_boxes[box] + move for box in boxes_to_move
+            }
+            for box in boxes_to_move:
+                connected_boxes.pop(box)
+            connected_boxes |= new_connected_boxes
+            new_lhs = {box + move for box in boxes_to_move if box in boxes_lhs}
+            boxes_lhs -= boxes_to_move
+            boxes_lhs |= new_lhs
+
+        print("Move", instruction)
+        draw_map(robot_xy, walls, boxes, width, height, big_boxes=True)
 
     gps_sum = 0
-    for box in boxes:
+    for box in boxes_lhs:
         gps_sum += abs(box.real) + abs(box.imag) * 100
-    print(f"Part 1: {int(gps_sum)}")
-    print(f"Part 2: {False}\n")
+    print(f"Part 2: {gps_sum}\n")
 
 
 solve(example_input)
-solve(actual_input)
+# solve(actual_input)
