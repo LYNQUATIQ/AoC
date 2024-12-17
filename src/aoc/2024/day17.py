4f367372e4d8fbe2927c0b45c1f3107ecb354159
Gideon Smith <gideon.smith@orbis.com>
2024-12-17 11:13:33 +0000

    Better commenting

--- src/aoc/2024/day17.py
+++ src/aoc/2024/day17.py
@@ -13,54 +13,50 @@ REG_A, REG_B, REG_C = "A", "B", "C"
 ADV, BXL, BST, JNZ, BXC, OUT, BDV, CDV = 0, 1, 2, 3, 4, 5, 6, 7
 
 
-def combo_op(combo_operand: int, registers: dict[str, int]) -> int:
-    return (
-        combo_operand
-        if combo_operand <= 3
-        else registers[{4: REG_A, 5: REG_B, 6: REG_C}[combo_operand]]
-    )
+def combo_operand(operand: int, regs: dict[str, int]) -> int:
+    return operand if operand <= 3 else regs[{4: REG_A, 5: REG_B, 6: REG_C}[operand]]
 
 
-def run_program(program: list[int], register_a: int) -> list[int]:
+def run_program(program: list[int], initial_a: int) -> list[int]:
     """Run the program and return the output using the following opcodes:
     adv/bdv/cdv - operations that take right hand bits from A and put them into A/B/C
-    bxl/bxc     - operations that flip bits in B using XOR
+    bxl/bxc     - operations that flip bits in B
     bst/out     - operations that puts the last 3 bits of something into B or output
     jnz         - operation that controls the program flow (jumps if A!=0)
     """
-    registers = {REG_A: register_a, REG_B: 0, REG_C: 0}
+    regs = {REG_A: initial_a, REG_B: 0, REG_C: 0}
     pointer, output = 0, []
     while pointer < len(program):
         opcode, operand = program[pointer], program[pointer + 1]
         pointer += 2
         if opcode == ADV:
-            registers[REG_A] = registers[REG_A] // (2 ** combo_op(operand, registers))
+            regs[REG_A] = regs[REG_A] // (2 ** combo_operand(operand, regs))
         elif opcode == BDV:
-            registers[REG_B] = registers[REG_A] // (2 ** combo_op(operand, registers))
+            regs[REG_B] = regs[REG_A] // (2 ** combo_operand(operand, regs))
         elif opcode == CDV:
-            registers[REG_C] = registers[REG_A] // (2 ** combo_op(operand, registers))
+            regs[REG_C] = regs[REG_A] // (2 ** combo_operand(operand, regs))
         elif opcode == BXL:
-            registers[REG_B] = registers[REG_B] ^ operand
+            regs[REG_B] = regs[REG_B] ^ operand
         elif opcode == BXC:
-            registers[REG_B] = registers[REG_B] ^ registers[REG_C]
+            regs[REG_B] = regs[REG_B] ^ regs[REG_C]
         elif opcode == BST:
-            registers[REG_B] = combo_op(operand, registers) % 8
+            regs[REG_B] = combo_operand(operand, regs) % 8
         elif opcode == OUT:
-            output.append(combo_op(operand, registers) % 8)
+            output.append(combo_operand(operand, regs) % 8)
         elif opcode == JNZ:
-            pointer = pointer if registers[REG_A] == 0 else operand
+            pointer = pointer if regs[REG_A] == 0 else operand
     return output
 
 
-def reverse_engineer(opcodes: list[int], register_a: int, loop: list[int]) -> int:
-    if not opcodes:
-        return register_a
-    for next_3_bits in range(8):
-        candidate_a = register_a * 8 + next_3_bits
-        output_a = run_program(loop, candidate_a)[0]
-        if output_a == opcodes[-1]:
+def reverse_engineer(loop: list[int], target: list[int], a_so_far: int = 0) -> int:
+    """Find the value of register A that will output the specified target"""
+    if target == []:
+        return a_so_far
+    for candidate_a in (a_so_far * 8 + next_3_bits for next_3_bits in range(8)):
+        # Extend register A by 3 bits and see if we output the last token in the target
+        if run_program(loop, candidate_a).pop() == target[-1]:
             try:
-                return reverse_engineer(opcodes[:-1], candidate_a, loop)
+                return reverse_engineer(loop, target[:-1], candidate_a)
             except StopIteration:
                 continue
     raise StopIteration
@@ -69,15 +65,15 @@ def reverse_engineer(opcodes: list[int], register_a: int, loop: list[int]) -> in
 def solve(inputs: str):
     registers_input, program_input = inputs.split("\n\n")
     program = list(map(int, program_input[8:].split(",")))
-    register_a = int(registers_input.splitlines()[0][12:])
+    initial_a = int(registers_input.splitlines()[0][12:])
 
-    print(f"Part 1: {','.join(map(str, run_program(program, register_a)))}")
+    print(f"Part 1: {','.join(map(str, run_program(program, initial_a)))}")
 
-    assert program[-2:] == [3, 0]
+    assert program[-2:] == [JNZ, 0]  # i.e. this is a program that loops until A is 0
     loop = program[:-2]
-    register_a = reverse_engineer(program, 0, loop)
-    assert run_program(program, register_a) == program
-    print(f"Part 2: {register_a}\n")
+    initial_a = reverse_engineer(loop, program)
+    assert run_program(program, initial_a) == program
+    print(f"Part 2: {initial_a}\n")
 
 
 solve(example_input)
