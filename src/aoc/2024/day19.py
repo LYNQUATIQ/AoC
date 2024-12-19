0ce88ddf52d4444bd0ccb0e498c475301e0e3862
Gideon Smith <gideon.smith@orbis.com>
2024-12-19 12:13:17 +0000

    Finished Day 19

--- src/aoc/2024/day19.py
+++ src/aoc/2024/day19.py
@@ -1,13 +1,8 @@
 """https://adventofcode.com/2024/day/19"""
 
-from collections import defaultdict
-from itertools import product
-
 from aoc_utils import get_input_data
 
 actual_input = get_input_data(2024, 19)
-
-
 example_input = """r, wr, b, g, bwu, rb, gb, br
 
 brwrr
@@ -19,45 +14,35 @@ bwurrg
 brgr
 bbrgwb"""
 
-ways_to_make_design: dict[str, int] = {}
-ways_to_make_pattern: dict[str, int] = {}
-
-
-def possible_ways(design: str, ignore_this: bool = False) -> int:
-    if not design:
-        return 1
-    if design in ways_to_make_design and not ignore_this:
-        return ways_to_make_design[design]
-    ways = 0
-    possible_starts = [p for p in ways_to_make_pattern if design.startswith(p)]
-    for start in possible_starts:
-        # ways += ways_to_make_design[start] * possible_ways(design[len(start) :])
-        ways += possible_ways(design[len(start) :])
-    if ways:
-        ways_to_make_design[design] = ways
-    return ways
-
 
 def solve(inputs: str):
+
     patterns_input, designs_input = inputs.split("\n\n")
     patterns = patterns_input.split(", ")
     designs = designs_input.splitlines()
 
+    def ways_to_design(design: str) -> int:
+        if not design:
+            return 1
+        if design in ways_to_make_design:
+            return ways_to_make_design[design]
+        ways_to_make_design[design] = 0
+        for start in [p for p in ways_to_make_pattern if design.startswith(p)]:
+            ways_to_make_design[design] += ways_to_design(design[len(start) :])
+        return ways_to_make_design[design]
+
+    # Cache to store designs and the number of ways to make them
+    ways_to_make_design: dict[str, int] = {}
+
+    # Initialise ways_to_make_pattern with the known patterns (one way to make each)
+    # and then add all the possible ways to build known patterns from others
+    ways_to_make_pattern: dict[str, int] = {pattern: 1 for pattern in patterns}
     for pattern in patterns:
-        ways_to_make_pattern[pattern] = 1
+        ways_to_make_pattern[pattern] = ways_to_design(pattern)
 
-    # Need to add all the possible ways to build known patterns from others
-    for pattern in patterns:
-        ways_to_make_pattern[pattern] = possible_ways(pattern, ignore_this=True)
-
-    # Now add all the possible ways to build the requested designs
-    for design in designs:
-        ways_to_make_design[design] = possible_ways(design)
-
-    print(f"Part 1: {sum([(ways_to_make_design[design]>0) for design in designs])}")
-    print(f"Part 2: {sum([ways_to_make_design[design] for design in designs])}\n")
+    print(f"Part 1: {sum([(ways_to_design(design)>0) for design in designs])}")
+    print(f"Part 2: {sum([ways_to_design(design) for design in designs])}\n")
 
 
 solve(example_input)
 solve(actual_input)
-# 41271664854551032 too high
