d8ce698aba4ceaa6a1dc195bd2e6c834851d62c8
Gideon Smith <gideon.smith@orbis.com>
2024-12-22 07:34:03 +0000

    Day 22 part 1

--- src/aoc/2024/day22.py
+++ src/aoc/2024/day22.py
@@ -1,19 +1,44 @@
 """https://adventofcode.com/2024/day/22"""
 
-from aoc_utils import get_input_data
+from aoc_utils import get_input_data, print_time_taken
 
 actual_input = get_input_data(2024, 22)
 
 
-example_input = """xxx"""
+example_input = """1
+10
+100
+2024"""
+
+MODULO = 16777216
 
 
+def evolve_secret(secret: int) -> int:
+    # xor secret with 64 * secret
+    secret ^= 64 * secret
+    secret %= MODULO
+    secret ^= secret // 32
+    secret %= MODULO
+    secret ^= 2048 * secret
+    secret %= MODULO
+    return secret
+
+
+@print_time_taken
 def solve(inputs: str):
-    values = tuple(map(int, inputs.splitlines()))
+    initial_secrets = tuple(map(int, inputs.splitlines()))
 
-    print(f"Part 1: {False}")
+    total = 0
+    for secret in initial_secrets:
+        # original_secret = secret
+        for _ in range(2000):
+            secret = evolve_secret(secret)
+        # print(f"{original_secret}: {secret}")
+        total += secret
+
+    print(f"Part 1: {total}")
     print(f"Part 2: {False}\n")
 
 
 solve(example_input)
-# solve(actual_input)
+solve(actual_input)
