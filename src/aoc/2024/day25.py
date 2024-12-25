85c8acbd7aabf0db8ba47ce94fb2fbb243ea09b0
Gideon Smith <gideon.smith@orbis.com>
2024-12-25 06:25:08 +0000

    Day 25 - done

--- src/aoc/2024/day25.py
+++ src/aoc/2024/day25.py
@@ -1,19 +1,67 @@
 """https://adventofcode.com/2024/day/25"""
 
+from itertools import product
+
 from aoc_utils import get_input_data
 
 actual_input = get_input_data(2024, 25)
+example_input = """#####
+.####
+.####
+.####
+.#.#.
+.#...
+.....
 
+#####
+##.##
+.#.##
+...##
+...#.
+...#.
+.....
 
-example_input = """xxx"""
+.....
+#....
+#....
+#...#
+#.#.#
+#.###
+#####
+
+.....
+.....
+#.#..
+###..
+###.#
+###.#
+#####
+
+.....
+.....
+.....
+#....
+#.#..
+#.#.#
+#####"""
 
 
 def solve(inputs: str):
-    values = tuple(map(int, inputs.splitlines()))
+    locks, keys = [], []
+    for schematic in inputs.split("\n\n"):
+        lines = schematic.split("\n")
+        heights = [sum(c == "#" for c in column) for column in list(zip(*lines))]
+        if lines[0].startswith("#"):
+            locks.append(heights)
+        else:
+            keys.append(heights)
 
-    print(f"Part 1: {False}")
-    print(f"Part 2: {False}\n")
+    fit_count = 0
+    for lock, key in product(keys, locks):
+        if all(key[pin] + lock[pin] <= 7 for pin in range(5)):
+            fit_count += 1
+    print(f"Part 1: {fit_count}\n")
 
 
 solve(example_input)
-# solve(actual_input)
+solve(actual_input)
