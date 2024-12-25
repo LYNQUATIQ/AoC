a3f285dcb78c5e45ec07ff02ee7d93b2f8420902
Gideon Smith <gideon.smith@orbis.com>
2024-12-25 06:31:37 +0000

    Tidied

--- src/aoc/2024/day25.py
+++ src/aoc/2024/day25.py
@@ -49,16 +49,16 @@ example_input = """#####
 def solve(inputs: str):
     locks, keys = [], []
     for schematic in inputs.split("\n\n"):
-        lines = schematic.split("\n")
-        heights = [sum(c == "#" for c in column) for column in list(zip(*lines))]
-        if lines[0].startswith("#"):
+        rows = schematic.split("\n")
+        heights = [sum(c == "#" for c in column) - 1 for column in zip(*rows)]
+        if rows[0].startswith("#"):
             locks.append(heights)
         else:
             keys.append(heights)
 
     fit_count = 0
     for lock, key in product(keys, locks):
-        if all(key[pin] + lock[pin] <= 7 for pin in range(5)):
+        if all(key[pin] + lock[pin] <= 5 for pin in range(5)):
             fit_count += 1
     print(f"Part 1: {fit_count}\n")
 
